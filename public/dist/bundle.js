43b7e530d81d071a4db1a74dbd11fc7f463ddfa4
francois <francoisrvespa@gmail.com>
2014-10-01 18:32:55 +0200

    creator

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -3465,7 +3465,55 @@ module.exports = function () {
 
     // d3 charts
 
-    synappUtilCharts          :       require('./directives/util/charts')
+    synappUtilCharts          :       require('./directives/util/charts'),
+
+    syNavItemDescription      :       function () {
+      return {
+        restrict: 'C',
+        link: function ($scope, $elem, $attr) {
+
+          $attr.$observe('ngBind', function (n, o) {
+            if ( n && n !== o ) {
+              var des = $elem.text();
+
+              var limit = 100;
+
+              if ( des.length > limit ) {
+                $elem.text(des.substr(0, limit));
+
+                var more = $('<a href="#">more</a>');
+                var less = $('<a href="#">less</a>');
+
+                function _more () {
+                  more.on('click', function (e) {
+                    e.preventDefault();
+                    $elem.text(des);
+                    $elem.append($('<span> </span>'), less);
+                    _less();
+                    return false;
+                  });
+                }
+
+                function _less () {
+                  less.on('click', function (e) {
+                    e.preventDefault();
+                    $elem.text(des.substr(0, limit));
+                    $elem.append($('<span> </span>'), more);
+                    _more();
+                    return false;
+                  });
+                }
+
+                _more();
+
+                $elem.append($('<span> </span>'), more);
+              }
+            }
+          });
+
+        }
+      };
+    }
   });
 
   // CONTROLLERS
@@ -3478,6 +3526,16 @@ module.exports = function () {
     // Accordion Controller
     NavigatorCtrl             :       function ($scope, ItemFactory, $timeout) {
       $scope.navigator = {};
+
+      $timeout(function () {
+        $('.more').on('click', function (e) {
+          var item = $(this).data('item');
+          item.$maxChars = 1000;
+          angular.element($(this)).scope().$digest();
+          e.preventDefault();
+          return false;
+        });
+      }, 250);
 
       $scope.moreLess = function (is) {
         $timeout(function () {
