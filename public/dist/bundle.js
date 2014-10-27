ddeb5566fdfc260586cee63ac6f97d1adc1cebe0
francois <francoisrvespa@gmail.com>
2014-10-27 22:55:33 +0100

    issue 46

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -251,6 +251,8 @@ module.exports = function ($scope, DataFactory, $timeout) {
     else {
       obj.image = getImage();
 
+      console.log('obj', obj);
+
       setTimeout(function () {
         DataFactory.model('Item').post(obj)
 
