e9465b7f65d9b9692bb014cc62dd2067445b08a7
francois <francoisrvespa@gmail.com>
2014-11-27 18:01:46 +0100

    Pushing to Heroku

--- app/web/angular/synapp/directives/details.js
+++ app/web/angular/synapp/directives/details.js
@@ -6,6 +6,7 @@
     return {
       restrict: 'C',
       link: function ($scope, $elem) {
+        $scope.emailBody = "I'm helping to bring synergy to democracy by voicing my opinion. I'm participating in the topic: " + '"' + $scope.item.subject + '". Please join me at Synaccord ' + location.href;
       }
     };
   }
