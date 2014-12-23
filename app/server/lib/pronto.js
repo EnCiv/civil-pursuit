db5f68d203336bccdf2c1eb99dd1f81e4915a05b
francois <francoisrvespa@gmail.com>
2014-12-23 19:42:23 +0100

    app/web/views/

--- app/server/lib/pronto.js
+++ app/server/lib/pronto.js
@@ -14,17 +14,23 @@
 
     exportConfig.user = { name: 'Roger' };
 
+    var monson = require('monson')(process.env.MONGOHQ_URL, {
+      base: require('path').join(process.cwd(), 'app/business')
+    });
+
     return pronto ()
 
       .inject('synapp', config)
 
-      .open('app/business/models/', { with: 'monson' }, when('/models' ))
+      .opener('monson', monson.pronto)
+
+      .open('app/business/models/', { with: 'monson', 'append extension': 'js' }, when('/models' ))
 
       .open('app/web/views/pages/index.jade', when.home)
 
       .open('app/web/views/pages', when.prefix('/page/'))
 
-      .open('app/web/views/', when('/partials/'))
+      .open('app/web/views/partials', { 'append extension': 'jade' }, when.prefix('/partial/'))
 
       .open('app/web/bower_components/', when.prefix('/bower/'))
 
