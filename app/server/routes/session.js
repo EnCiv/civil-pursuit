e87017f8586e883e68573a8bbd273a07abc9a035
francois <francoisrvespa@gmail.com>
2015-01-06 20:34:43 +0100

    editor layout

--- app/server/routes/session.js
+++ app/server/routes/session.js
@@ -2,21 +2,7 @@
 
   'use strict';
 
-  var cookieParser  =   require('cookie-parser');
-
-  var bodyParser    =   require('body-parser');
-
-  var session       =   require('express-session');
-
-  var config        =   require('../../business/config.json');
-
   function sessionMiddleware (req, res, next) {
-    cookieParser(config.secret)(req, res, next);
-    session({
-        secret: config.secret,
-        resave: true,
-        saveUninitialized: true
-      })(req, res, next);
 
     next();
   }
