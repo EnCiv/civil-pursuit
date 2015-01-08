06d08a6aff16a96e2ccd4a3b53d23cd431db634a
francois <francoisrvespa@gmail.com>
2015-01-08 02:02:13 +0100

    some advance in file upload

--- app/web/js/true-story/controllers/upload.js
+++ app/web/js/true-story/controllers/upload.js
@@ -9,25 +9,33 @@
 
     for (var i = 0, f; f = files[i]; i++) {
       parse(f);
+      upload(f);
     }
   }
 
   function hover (e) {
     e.stopPropagation();
     e.preventDefault();
-    e.target.className = (e.type == "dragover" ? "hover" : "");
+    // e.target.className = (e.type == "dragover" ? "hover" : "");
   }
 
   function parse (file) {
     console.warn('file parsed', file);
   }
 
-  function upload (dropbox) {
+  function upload (file) {
+
+  }
+
+  function init (dropbox) {
     if ( window.File ) {
       dropbox.find('input').on('change', handler);
+      dropbox.on('dragover', hover);
+      dropbox.on('dragleave', hover);
+      dropbox.on('drop', handler);
     }
   }
 
-  module.exports = upload;
+  module.exports = init;
 
 } ();
