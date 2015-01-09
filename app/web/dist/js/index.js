7081bcc8bffe5e0c1434b447d238949261a9db68
francois <francoisrvespa@gmail.com>
2015-01-09 17:47:16 +0100

    timeout charts

--- app/web/dist/js/index.js
+++ app/web/dist/js/index.js
@@ -1666,77 +1666,80 @@ $$$$$$$    $$$$$$$  $$    $$   $$$$$$$  $$$$$$$   $$$$$$$      $$  $$$$$$$
 
   module.exports = {
     template: '.votes-by-criteria',
+
     controller: function (view, details) {
-      details.criterias.forEach(function (criteria) {
-        view.find('h4').text(criteria.name);
+      setTimeout(function () {
+        details.criterias.forEach(function (criteria) {
+          view.find('h4').text(criteria.name);
 
-        var vote = details.votes[criteria._id];
+          var vote = details.votes[criteria._id];
 
-        var svg = $('<svg class="chart"></svg>');
+          var svg = $('<svg class="chart"></svg>');
 
-        svg.attr('id', 'chart-' + details.item._id + '-' + criteria._id);
+          svg.attr('id', 'chart-' + details.item._id + '-' + criteria._id);
 
-        console.log('svg!', svg.attr('id'))
+          console.log('svg!', svg.attr('id'))
 
-        view.find('.chart').append(svg);
+          view.find('.chart').append(svg);
 
-        var data = [];
+          var data = [];
 
-        for ( var number in vote.values ) {
-          data.push({
-            label: 'number',
-            value: vote.values[number] * 100 / vote.total
+          for ( var number in vote.values ) {
+            data.push({
+              label: 'number',
+              value: vote.values[number] * 100 / vote.total
+            });
+          }
+
+          var columns = ['votes'];
+
+          data.forEach(function (d) {
+            columns.push(d.value);
           });
-        }
 
-        var columns = ['votes'];
+          var chart = c3.generate({
+            bindto: '#' + svg.attr('id'),
 
-        data.forEach(function (d) {
-          columns.push(d.value);
-        });
+            data: {
+              x: 'x',
+              columns: [['x', -1, 0, 1], columns],
+              type: 'bar'
+            },
 
-        var chart = c3.generate({
-          bindto: '#' + svg.attr('id'),
-
-          data: {
-            x: 'x',
-            columns: [['x', -1, 0, 1], columns],
-            type: 'bar'
-          },
-
-          grid: {
-            x: {
-              lines: 3
-            }
-          },
-          
-          axis: {
-            x: {},
+            grid: {
+              x: {
+                lines: 3
+              }
+            },
             
-            y: {
-              max: 90,
+            axis: {
+              x: {},
+              
+              y: {
+                max: 90,
 
-              show: false,
+                show: false,
 
-              tick: {
-                count: 5,
+                tick: {
+                  count: 5,
 
-                format: function (y) {
-                  return y;
+                  format: function (y) {
+                    return y;
+                  }
                 }
               }
+            },
+
+            size: {
+              height: 80
+            },
+
+            bar: {
+              width: $(window).width() / 5
             }
-          },
-
-          size: {
-            height: 80
-          },
-
-          bar: {
-            width: $(window).width() / 5
-          }
+          });
         });
-      });
+      }, 1500);
     }
   };
 
