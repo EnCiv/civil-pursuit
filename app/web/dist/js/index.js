62e5e980a3b3c3899a3d13aa46557c0a2c586c22
francois <francoisrvespa@gmail.com>
2015-01-28 18:11:18 +0100

    fixes

--- app/web/dist/js/index.js
+++ app/web/dist/js/index.js
@@ -1400,10 +1400,11 @@
 
     // DOM elements
 
-    var $panel    =   $(trigger).closest('.panel');
-    var $item     =   $(trigger).closest('.item');
-    var $details  =   $item.find('>.collapsers >.details');
-    var $editor  =   $item.find('>.collapsers >.editor');
+    var $panel      =   $(trigger).closest('.panel');
+    var $item       =   $(trigger).closest('.item');
+    var $details    =   $item.find('>.collapsers >.details');
+    var $editor     =   $item.find('>.collapsers >.editor');
+    var $promote    =   $item.find('>.collapsers >.evaluator');
 
     // helpers
 
@@ -1414,45 +1415,66 @@
 
     var reveal = Panel.controller('reveal').bind(Panel);
 
+    // On reveal
+
+    function onReveal () {
+      div.controller('progress bar')($details, item);
+
+      div.controller('invite people in')($details, item);
+
+      if ( ! $details.hasClass('is-loaded') ) {
+        div.controller('get item details')($details, item);
+
+        $details
+          .find('.edit-and-go-again-toggler')
+          .eq(0)
+          .on('click', function () {
+
+            luigi('tpl-creator')
+
+              .controller(function (view) {
+                $editor.empty().append(view);
+                Panel.controller('reveal')($editor, $item);
+              });
+
+            // div.render('edit and go again', item, function (editView) {
+            //   console.log(editView)
+            //   $editor
+            //     .empty()
+            //     .append(editView);
+            // });
+            // Panel.controller('reveal')($editor, $item);
+          });
+      }
+    }
+
+    // If details shown, then hide
+
     if ( $details.hasClass('is-shown') ) {
       scrollToPOA($item, function ()  {
         hide($details);
       });
     }
+
+    else if ( $details.hasClass('is-showing') || $details.hasClass('is-hidding') ) {
+      return;
+    }
     
     else {
-      reveal($details, $item, function () {
 
-        div.controller('progress bar')($details, item);
+      /** If promote is shown, hide it first */
 
-        div.controller('invite people in')($details, item);
+      if ( $promote.hasClass('is-shown') ) {
+        scrollToPOA($item, function ()  {
+          hide($promote, function () {
+            reveal($details, $item, onReveal);
+          });
+        });
+      }
 
-        if ( ! $details.hasClass('is-loaded') ) {
-          div.controller('get item details')($details, item);
-
-          $details
-            .find('.edit-and-go-again-toggler')
-            .eq(0)
-            .on('click', function () {
-
-              luigi('tpl-creator')
-
-                .controller(function (view) {
-                  $editor.empty().append(view);
-                  Panel.controller('reveal')($editor, $item);
-                });
-
-              // div.render('edit and go again', item, function (editView) {
-              //   console.log(editView)
-              //   $editor
-              //     .empty()
-              //     .append(editView);
-              // });
-              // Panel.controller('reveal')($editor, $item);
-            });
-        }
-
-      });
+      else {
+        reveal($details, $item, onReveal);
+      }
     }
   }
 
