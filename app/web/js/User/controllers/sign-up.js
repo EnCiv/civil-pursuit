c5cb066ccec1cc2e7a827ccac2566f9712c37c2b
francois <francoisrvespa@gmail.com>
2015-01-29 20:01:46 +0100

    sign-up

---
+++ app/web/js/User/controllers/sign-up.js
@@ -0,0 +1,103 @@
+! function () {
+
+  'use strict';
+
+  function signUp () {
+    var app = this;
+
+    $('#join').find('.i-agree').on('click', function () {
+      var agreed = $('#join').find('.agreed');
+
+      if ( agreed.hasClass('fa-square-o') ) {
+        agreed.removeClass('fa-square-o').addClass('fa-check-square-o');
+      }
+      else {
+        agreed.removeClass('fa-check-square-o').addClass('fa-square-o');
+      }
+    });
+
+    $('#join').find('form').on('submit', function () {
+      
+      var email = $(this).find('[name="email"]');
+      var password = $(this).find('[name="password"]');
+      var confirm = $(this).find('[name="confirm"]');
+
+      email.removeClass('error');
+      password.removeClass('error');
+      confirm.removeClass('error');
+
+      $('#join').find('.alert')
+          .css('display', 'none');
+
+      if ( ! email.val() ) {
+        email.addClass('error').focus();
+        $('#join').find('.alert')
+          .css('display', 'block')
+          .find('.alert-message').text('Please enter an email address');
+      }
+
+      else if ( ! password.val() ) {
+        password.addClass('error').focus();
+        $('#join').find('.alert')
+          .css('display', 'block')
+          .find('.alert-message').text('Please enter a password');
+      }
+
+      else if ( ! confirm.val() ) {
+        confirm.addClass('error').focus();
+        $('#join').find('.alert')
+          .css('display', 'block')
+          .find('.alert-message').text('Please confirm password');
+      }
+
+      else if ( password.val() !== confirm.val() ) {
+        confirm.addClass('error').focus();
+        $('#join').find('.alert')
+          .css('display', 'block')
+          .find('.alert-message').text('Passwords do not match');
+      }
+
+      else {
+        $.ajax({
+          url: '/sign/up',
+          type: 'POST',
+          data: {
+            email: email.val(),
+            password: password.val()
+          }
+        })
+          
+          .error(function (response, state, code) {
+            if ( response.status === 401 ) {
+              $('#join').find('.alert')
+                .css('display', 'block')
+                .find('.alert-message').text('This email address is already in use');
+            }
+          })
+          
+          .success(function (response) {
+            synapp.user = response.user;
+            
+            $('.is-in').css('visibility', 'visible');
+
+            $('#join').modal('hide');
+
+            $('#signer').find('section').hide(2000);
+
+            $('#signer').find('.sign-success')
+              .show(function () {
+                setTimeout(function () {
+                  $('#signer').hide(2500);
+                }, 5000);
+              })
+              .text('Welcome to Synaccord!');
+          });
+      }
+
+      return false;
+    })
+  }
+
+  module.exports = signUp;
+
+} ();
