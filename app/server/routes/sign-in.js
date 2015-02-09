3d0f1359925a473f0fc013659d09a48e8a0a30e9
francois <francoisrvespa@gmail.com>
2015-02-09 19:50:47 +0100

    sign bar

--- app/server/routes/sign-in.js
+++ app/server/routes/sign-in.js
@@ -3,38 +3,49 @@
   'use strict';
 
   function signIn (req, res, next) {
-    var monson    =   require('monson')(process.env.MONGOHQ_URL, {
-      base: require('path').join(process.cwd(), 'app/business')
+
+    // require('mongoose').connect(process.env.MONGOHQ_URL);
+
+    var domain = require('domain').create();
+    
+    domain.on('error', function (error) {
+      next(error);
     });
+    
+    domain.run(function () {
 
-    var url = 'models/User.identify/' + req.body.email + '/' + req.body.password;
+      var User = require('../../business/models/User');
 
-    monson.get(url)
-
-      .on('error', function (error) {
-        if ( /^User not found/.test(error.message) ) {
-          res.statusCode = 401;
-          res.json({ 'user not found': req.body.email });
+      User.identify(req.body.email, req.body.password, domain.bind(function (error, user) {
+        if ( error ) {
+          if ( /^User not found/.test(error.message) ) {
+            res.statusCode = 404;
+            res.json({ 'user not found': req.body.email });
+          }
+          else if ( /^Wrong password/.test(error.message) ) {
+            res.statusCode = 401;
+            res.json({ 'user not found': req.body.email });
+          }
+          else {
+            throw error;
+          }
         }
         else {
-          throw error;
+          res.cookie('synuser',
+            {
+              email: user.email,
+              id: user._id,
+              prefs: user.preferences
+            },
+            require('../../business/config.json').cookie);
+          
+          res.json({
+            in: true,
+            user: user._id
+          });
         }
-      })
-
-      .on('success', function (user) {
-        res.cookie('synuser',
-          {
-            email: user.email,
-            id: user._id,
-            prefs: user.preferences
-          },
-          require('../../business/config.json').cookie);
-        
-        res.json({
-          in: true,
-          user: user._id
-        });
-      });
+      }));
+    });
   }
 
   module.exports = signIn;
