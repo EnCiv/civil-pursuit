5145d59f9c471bf35b07afae1bf368dcd8c496cc
francois <francoisrvespa@gmail.com>
2015-02-27 19:24:50 +0100

    youtube

--- app/web/dist/js/profile.js
+++ app/web/dist/js/profile.js
@@ -4509,7 +4509,7 @@
 
       var button = $('<button></button>');
 
-      button.addClass('icon-play danger');
+      button.addClass('icon-play shy');
 
       var i = $('<i></i>');
 
@@ -4529,16 +4529,36 @@
     }
   }
 
+  function resize (elem) {
+    var img   =   elem.find('img');
+
+    var icon  =   elem.find('.icon-play');
+
+    var h = icon.height();
+
+    icon.css({
+      'top': (img.offset().top + (img.height() / 2) - (h / 2)) + 'px'
+    });
+
+    icon.width(width);
+  }
+
   function Play (elem) {
 
     var img   =   elem.find('img');
 
     var icon  =   elem.find('.icon-play');
 
-    icon.removeClass('hide');
+    $(window).on('resize', function () {
+      resize(elem);
+    });
 
     img.on('load', function () {
+
+      resize(elem);
+
       icon.find('.fa').on('click', function () {
+
         var video_container = $('<div class="video-container"></div>');
 
         var preview = $(this).closest('.youtube-preview');
@@ -4554,48 +4574,6 @@
             + preview.data('video') + '?autoplay=1'); 
       });
     });
-
-    setTimeout(function () {
-
-
-      return;
-
-      icon.css('width', img.width() + 'px');
-
-      icon.css('height', img.height() + 'px');
-
-      img.css('margin-bottom', '-' + img.height() + 'px');
-
-      $(window).on('resize', function () {
-
-        icon.css('width', img.width() + 'px');
-
-        img.css('margin-bottom', '-' + img.height() + 'px');
-      });
-
-      icon.find('.fa').on('click', function () {
-        var video_container = $('<div class="video-container"></div>');
-
-        var preview = $(this).closest('.youtube-preview');
-
-        preview
-          .empty()
-          .append(video_container);
-
-        video_container.append($('<iframe frameborder="0" width="300" height="175" allowfullscreen></iframe>'));
-
-        video_container.find('iframe')
-          .attr('src', 'http://www.youtube.com/embed/'
-            + preview.data('video') + '?autoplay=1'); 
-      });
-
-      icon.show();
-
-      icon.css('padding-top',
-        ( ( img.height() / 2 ) - ( icon.find('.fa').height() / 2 ) )
-          + 'px');
-        
-    }, 800);
   }
 
   module.exports = YouTube;
