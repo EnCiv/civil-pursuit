b75c1b6a1e5068c0072059f191fcf66c55109c13
francois <francoisrvespa@gmail.com>
2015-03-28 12:29:02 +0100

    "deploy"

--- app/web/test/reference-opens-in-a-new-window.js
+++ app/web/test/reference-opens-in-a-new-window.js
@@ -65,7 +65,7 @@
         
       browser.waitForElementVisible(            'body', 1000)
 
-        .waitForElementVisible(__(              'item'), 2500, "The first item should be the test item")
+        .waitForElementVisible(__(              'item'), 3000, "The first item should be the test item")
         .assert.visible(__(                     'reference'), "Test item should have a reference")
         .click(__(                              'reference'))
 
