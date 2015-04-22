aadf69c59af3bcb019776bc1af02f47c6d9755af
francois <francoisrvespa@gmail.com>
2015-04-22 12:40:04 +0200

    "Test-models/User/statics/save-image.js,-test/lib/util/cloudinary.js"

--- app/models/User/statics/set-birthdate.js
+++ app/models/User/statics/set-birthdate.js
@@ -3,9 +3,19 @@
   'use strict';
 
   function setBirthdate (user_id, dob, cb) {
-    require('syn/lib/domain/next-tick')(cb, function () {
-      require('syn/models/User').update({ _id: user_id }, { dob: dob }, cb);
-    });
+
+    var domain = require('domain').create();
+    
+    domain
+      
+      .on('error', cb)
+    
+      .run(function () {
+        var User = require('syn/models/User');
+
+        process.nextTick(
+          User.update.bind(User, { _id: user_id }, { dob: dob }, cb));
+      });
   }
 
   module.exports = setBirthdate;
