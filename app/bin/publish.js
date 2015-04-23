452face6c0abdc393ef8f8ea216dfb5c067ec50a
francois <francoisrvespa@gmail.com>
2015-04-23 18:41:51 +0200

    "test/models/Item/methods/get-popularity"

--- app/bin/publish.js
+++ app/bin/publish.js
@@ -21,8 +21,7 @@
 
     { name: 'mocha test/lib/util/arguments-to-array' },
     { name: 'mocha test/models/Item/methods/' },
-    { name: 'mocha test/models/Item/statics/generate-short-id' },
-    { name: 'mocha test/models/Item/statics/generate-panel-items' },
+    { name: 'mocha test/models/Item/statics/' },
 
     // { name: [test, 'test/lib'].join(' ') },
 
