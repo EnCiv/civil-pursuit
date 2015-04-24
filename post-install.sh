dfe8c112bb6b600fc2d99b33f1e6ea353b415650
francois <francoisrvespa@gmail.com>
2015-04-24 18:24:46 +0200

    symbolic link

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,64 @@
 #!/bin/bash
 
+if [ -d "/app" ]; then
+  # Heroku
+  symbolic_link="/app/node_modules/syn";
+else
+  symbolic_link="$PWD/node_modules/syn";
+fi
+
+symbolic_link="$PWD/node_modules/syn";
+
+echo
+echo '###'
+echo '    CREATE SYMBOLIC LINK'
+echo '###'
+echo
+
+echo $symbolic_link
+
+echo
+echo '###'
+echo '    SYMBOLIC LINK EXISTS?'
+echo '###'
+echo
+
+ls $symbolic_link
+
+echo
+echo '###'
+echo '    LIST SYMBOLIC LINK'
+echo '###'
+echo
+
+ls $symbolic_link
+
+echo
+echo '###'
+echo '    CREATE SYMBOLIC LINK'
+echo '###'
+echo
+
+echo ln -s $PWD/app/ $symbolic_link  || exit 1;
+ln -s $PWD/app/ $symbolic_link  || exit 1;
+
+echo
+echo '###'
+echo '    CREATE SYMBOLIC PACKAGE.JSON'
+echo '###'
+echo
+
+ln -s $PWD/package.json $symbolic_link/package.json  || exit 1;
+
+echo
+echo '###'
+echo '    LIST SYMBOLIC LINK'
+echo '###'
+echo
+
+ls $symbolic_link
+ls /app
+
 echo
 echo '###'
 echo '    MIGRATE TO v2'
@@ -25,6 +84,14 @@ echo
 cd app/dist
 ../../node_modules/.bin/bower install  || exit 1;
 cd ../..;
+
+echo
+echo '###'
+echo '    REMOVE SYMBOLIC LINK'
+echo '###'
+echo
+
+unlink $symbolic_link || exit 1;
 
 echo
 echo '###'
