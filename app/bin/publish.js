27a6ddd335654a63b1f463ae0b13a21d4806076e
francois <francoisrvespa@gmail.com>
2015-04-24 15:28:45 +0200

    "Test-Item"

--- app/bin/publish.js
+++ app/bin/publish.js
@@ -24,7 +24,7 @@
     { name: 'mocha test/lib/util/di' },
     { name: 'mocha test/lib/util/encrypt' },
     { name: 'mocha test/lib/util/get-url-title' },
-    { name: 'mocha test/models/Item/*' },
+    { name: 'mocha test/models/Item/Item test/models/Item/methods test/models/Item/statics test/models/Item/pre' },
 
     // { name: [test, 'test/lib'].join(' ') },
 
