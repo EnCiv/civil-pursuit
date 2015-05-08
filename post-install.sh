44860ff413a5a4aef9b09d7c01acb60c47e8454f
francois <francoisrvespa@gmail.com>
2015-05-08 20:11:56 +0200

    "start-+-migrate---post-install"

--- post-install.sh
+++ post-install.sh
@@ -1,50 +1,50 @@
 #!/bin/bash
 
-if [ -d "/app" ]; then
-  # Heroku
-  echo Heroku detected
-  isHeroku=y
-  symbolic_link="/app/node_modules/syn";
-else
-  symbolic_link="$PWD/node_modules/syn";
-fi
+# if [ -d "/app" ]; then
+#   # Heroku
+#   echo Heroku detected
+#   isHeroku=y
+#   symbolic_link="/app/node_modules/syn";
+# else
+#   symbolic_link="$PWD/node_modules/syn";
+# fi
 
-if [ -L "$symbolic_link" ]; then
-  echo 'Symbolic link already exists - skipping'
-else
-  echo
-  echo '###'
-  echo '    CREATE SYMBOLIC LINK '$symbolic_link
-  echo '###'
-  echo
+# if [ -L "$symbolic_link" ]; then
+#   echo 'Symbolic link already exists - skipping'
+# else
+#   echo
+#   echo '###'
+#   echo '    CREATE SYMBOLIC LINK '$symbolic_link
+#   echo '###'
+#   echo
 
-  ln -s $PWD/app/ $symbolic_link  || exit 6;
-  echo ln -s $PWD/app/ $symbolic_link  || exit 6;
-fi
+#   ln -s $PWD/app/ $symbolic_link  || exit 6;
+#   echo ln -s $PWD/app/ $symbolic_link  || exit 6;
+# fi
 
-echo
-echo '###'
-echo '    MIGRATE TO v2'
-echo '###'
-echo
+# echo
+# echo '###'
+# echo '    MIGRATE TO v2'
+# echo '###'
+# echo
 
-node app/models/migrations/v2.js  || exit 9;
+# node app/models/migrations/v2.js  || exit 9;
 
-echo
-echo '###'
-echo '    MIGRATE TO v3'
-echo '###'
-echo
+# echo
+# echo '###'
+# echo '    MIGRATE TO v3'
+# echo '###'
+# echo
 
-node app/models/migrations/v3.js  || exit 10;
+# node app/models/migrations/v3.js  || exit 10;
 
-echo
-echo '###'
-echo '    MIGRATE TO v4'
-echo '###'
-echo
+# echo
+# echo '###'
+# echo '    MIGRATE TO v4'
+# echo '###'
+# echo
 
-node app/models/migrations/v4.js  || exit 11;
+# node app/models/migrations/v4.js  || exit 11;
 
 echo
 echo '###'
@@ -58,13 +58,13 @@ cd ../..;
 
 # if [ $isHeroku = 'y' ]; then
 
-echo
-echo '###'
-echo '    REMOVE SYMBOLIC LINK' 
-echo '###'
-echo
+# echo
+# echo '###'
+# echo '    REMOVE SYMBOLIC LINK' 
+# echo '###'
+# echo
 
-unlink $symbolic_link  || exit 13;
+# unlink $symbolic_link  || exit 13;
 
 echo
 echo '###'
