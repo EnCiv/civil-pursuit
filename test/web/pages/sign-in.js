b5b5c1685dd784a01fff98c562d99270119493ea
francois <francoisrvespa@gmail.com>
2015-05-25 18:09:25 +0200

    cinco version in package.json

--- test/web/pages/sign-in.js
+++ test/web/pages/sign-in.js
@@ -1,26 +1,3 @@
 'use strict';
 
-import should from 'should';
-import Describe from 'beatpoints/lib/app/Describe';
-import config from 'syn/config.json';
 
-let describe = new Describe('Web / Sign in Page')
-  
-  .use('mongoose')
-
-  .use('web driver')
-
-  .define('user', () => Describe.disposableUser())
-
-  .assert('document has the right title', { document: 'title' },
-    title => { title.should.be.exactly(config.signIn) }
-  )
-
-  .assert('document\'s encoding is UTF-8', { attribute: 'meta[charset]' },
-    charset => { charset.should.be.exactly('utf-8') })
-
-  .assert(new TopBar())
-
-  .assert(new SignIn())
-
-  .assert(new Footer());
