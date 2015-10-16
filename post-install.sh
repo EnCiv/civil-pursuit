950c6da5da0ec26ccd167d67e5d80a698c6a3d5f
francois <francoisrvespa@gmail.com>
2015-10-16 14:37:30 +0700

    'build dist in post-install'

--- post-install.sh
+++ post-install.sh
@@ -2,12 +2,6 @@
 
 # node app/models/migrations/v4.js  || exit 11;
 
-echo
-echo '###'
-echo '    BOWER INSTALL'
-echo '###'
-echo
-
 cd assets
 ../node_modules/.bin/bower install  || exit 12;
 cd ..;
