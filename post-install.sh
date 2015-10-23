d23153ecc7cb3ed484d08aee34e5d35760b44873
francois <francoisrvespa@gmail.com>
2015-10-23 22:01:16 +0700

    heroku mung

--- post-install.sh
+++ post-install.sh
@@ -3,12 +3,11 @@
 # node app/models/migrations/v4.js  || exit 11;
 
 cd assets &&
-../node_modules/.bin/bower install &&
+bower install &&
 cd .. &&
 npm run transpile &&
 npm run build &&
 npm run less &&
 npm run min-css &&
 npm run uglify &&
-npm run uglify-assets &&
-npm run migrate
+npm run uglify-assets
