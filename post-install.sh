c7482314bccaab8280266afbf73b92c00c3038d5
francois <francoisrvespa@gmail.com>
2015-10-23 21:49:11 +0700

    heroku mung

--- post-install.sh
+++ post-install.sh
@@ -2,12 +2,10 @@
 
 # node app/models/migrations/v4.js  || exit 11;
 
-cd assets
-../node_modules/.bin/bower install  || exit 12;
-
-cd ..;
-
-# npm run transpile &&
+cd assets &&
+../node_modules/.bin/bower install &&
+cd .. &&
+npm run transpile
 # npm run build &&
 # npm run less &&
 # npm run min-css &&
