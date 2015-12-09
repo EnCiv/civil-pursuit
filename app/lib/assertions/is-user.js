9ed5023c44be3243cfba280b3d4b185bbb40ac45
francois <francoisrvespa@gmail.com>
2015-12-10 00:33:55 +0700

    Test

--- app/lib/assertions/is-user.js
+++ app/lib/assertions/is-user.js
@@ -19,19 +19,45 @@ function isUser (user, compare = {}, serialized = false) {
         ok();
       });
     }
-    
-    //
-    // it('should have a name', (ok, ko) => {
-    //   type.should.have.property('name').which.is.a.String();
-    //   ok();
-    // });
-    //
-    // if ( 'name' in compare ) {
-    //   it('name should match compare', (ok, ko) => {
-    //     type.name.should.be.exactly(compare.name);
-    //     ok();
-    //   });
-    // }
+
+    it('should have a email', (ok, ko) => {
+      user.should.have.property('email').which.is.a.String();
+      ok();
+    });
+
+    if ( 'email' in compare ) {
+      it('email should match compare', (ok, ko) => {
+        user.email.should.be.exactly(compare.email);
+        ok();
+      });
+    }
+
+    it('should have a password', (ok, ko) => {
+      user.should.have.property('password').which.is.a.String();
+      ok();
+    });
+
+    if ( 'image' in user ) {
+      it('should have an image', (ok, ko) => {
+        user.should.have.property('image').which.is.a.String();
+        ok();
+      });
+    }
+
+    if ( 'preferences' in user ) {
+      it('preferences', [ it => {
+        it('should have preferences', (ok, ko) => {
+          user.should.have.property('preferences').which.is.an.Array();
+          ok();
+        });
+        user.preferences.forEach(preference => it('should be a preference', [ it  => {
+          it('should be an object', (ok, ko) => {
+            preference.should.be.an.Object();
+            ok();
+          });
+        }]));
+      }]);
+    }
   };
 
 }
---
+++ app/test/5-models/app-error.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import Mungo                from 'mungo';
+import should               from 'should';
+import describe             from 'redtea';
+import isAppError           from '../../lib/assertions/is-app-error';
+import AppError             from '../../models/app-error';
+
+function test () {
+  const locals = {};
+
+  return describe ( 'Models/AppError', [
+
+    {
+      'Throw error' : [
+        {
+          'should create a new error in DB' : (ok, ko) => {
+            try {
+              throwError();
+            }
+            catch ( error ) {
+              AppError.throwError(error).then(
+                error => {
+                  locals.error = error;
+                  ok();
+                },
+                ko
+              );
+            }
+          }
+        },
+        {
+          'should be an error' : describe.use(() => isAppError(locals.error))
+        }
+      ]
+    }
+
+  ]);
+}
+
+export default test;
