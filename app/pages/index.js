fc8bf1a7133cb8a5119d8ea4f4f2c560d29a32e6
francois <francoisrvespa@gmail.com>
2015-12-10 01:20:43 +0700

    Google Analytics

--- app/pages/index.js
+++ app/pages/index.js
@@ -38,7 +38,9 @@ class Script extends Element {
 
 class GoogleAnalytics extends Element {
   constructor () {
-    super('script').text(`(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', "${publicConfig['google analytics'].key}", 'auto'); ga('send', 'pageview');`);
+    super('script');
+
+    this.text(`(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', "${publicConfig['google analytics'].key}", 'auto'); ga('send', 'pageview');`);
   }
 }
 
