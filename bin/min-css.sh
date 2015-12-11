1ee9eff3a24f8602f1d6837d35e45153991da3d1
francois <francoisrvespa@gmail.com>
2015-12-12 01:12:52 +0700

    debug heroku dependency issues

--- bin/min-css.sh
+++ bin/min-css.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-echo hello
+node dist/lib/app/minify-css.js \
+  assets/css/index.css \
+  assets/css/index.min.css || {
+  echo Could not minfiy index;
+  exit 1
+}
 
-node dist/lib/build/minify-css.js assets/css/index.css assets/css/index.min.css &&
-node dist/lib/build/minify-css.js assets/css/training.css assets/css/training.min.css &&
+node dist/lib/app/minify-css.js \
+  assets/css/training.css \
+  assets/css/training.min.css || {
+  echo Could not minfiy training;
+  exit 1
+}
 
-cat assets/css/normalize.min.css assets/bower_components/c3/c3.min.css  > assets/css/assets.min.css
+cat assets/css/normalize.min.css assets/bower_components/c3/c3.min.css  > assets/css/assets.min.css || {
+  echo could not cat assets;
+  exit 1;
+}
 
 echo "min css ok"
