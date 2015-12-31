9cce82accba22d0b971ee0941e36cd17c11be170
francois <francoisrvespa@gmail.com>
2015-12-31 16:49:52 +0700

    227 - offcanvas

--- app/models/sidebar-link/index.js
+++ app/models/sidebar-link/index.js
@@ -2,4 +2,13 @@
 
 import Mungo          from 'mungo';
 
-class SidebarLink
+class SidebarLink extends Mungo.Model {
+  static schema () {
+    return {};
+  }
+}
+
+SidebarLink.collection = 'sidebar-links';
+SidebarLink.version = 1;
+
+export default SidebarLink;
