c7056ccc39bbb763d72d56d4a599ceb138374d34
Hans Hennis <hanshennis@hotmail.com>
2016-02-25 22:05:36 +0100

    test

--- app/components/promote-item-description.jsx
+++ app/components/promote-item-description.jsx
@@ -4,16 +4,12 @@ import React from 'react';
 
 class Description extends React.Component {
   static propTypes = {
-    description1 : React.PropTypes.string,
-    description2 : React.PropTypes.string
+    description : React.PropTypes.string
   };
 
   render () {
     return (
-      <section className="promote-description item-description">
-      	<div className="column1">{ this.props.description1 }</div>
-      	<div className="column2">{ this.props.description2 }</div>
-      </section>
+      <section className="promote-description item-description">{ this.props.description }</section>
     );
   }
 }
