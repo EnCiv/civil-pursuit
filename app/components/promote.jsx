0af9e93b24d815e5a13b30d2f1ae812d7df4f5f8
Hans Hennis <hanshennis@hotmail.com>
2016-02-26 18:29:12 +0100

    no message

--- app/components/promote.jsx
+++ app/components/promote.jsx
@@ -19,11 +19,11 @@ componentDidUpdate () {
         var ly = document.getElementById('left_description').offsetHeight;
         var ry = document.getElementById('right_description').offsetHeight;
         if (ly < ry) {
-          alert("right big");
+          alert("right big" + ry);
           document.getElementById('left_description').style.height = ry + "px";
         }
         else {
-          alert("left big");
+          alert("left big" + ly);
           document.getElementById('right_description').style.height = ly + "px'";
         }
     }  
