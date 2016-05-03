858fd917306e94a5d453be912d52429e9b779045
David Fridley <david@synaccord.com>
2016-05-03 15:04:32 -0700

    just info

--- app/components/panel-items.jsx
+++ app/components/panel-items.jsx
@@ -66,7 +66,7 @@ class PanelItems extends React.Component {
 
   toggle (itemId, section) {
 
-    console.info("toggle", itemId, section, this.mountedItems);
+    console.info("toggle", itemId, section, "mountedItems", this.mountedItems.length);
 
     if (
       this.state.active &&
