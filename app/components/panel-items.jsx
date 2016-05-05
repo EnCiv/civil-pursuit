26f6786e78594cbb08f407a438b98ed65357886c
David Fridley <david@synaccord.com>
2016-05-04 18:38:07 -0700

    collapseAround with .map

--- app/components/panel-items.jsx
+++ app/components/panel-items.jsx
@@ -106,7 +106,7 @@ class PanelItems extends React.Component {
       for (let itm in this.props.items) {
         this.state.itemhide[this.props.items[itm]._id] =
             (itemId) ? 
-              ((item._id == itemId) ? ( { hid: false } ) : ( { hid: true } ) )
+              ((itm._id == itemId) ? ( { hid: false } ) : ( { hid: true } ) )
             : ( { hid: false } );
         }
     } else {
