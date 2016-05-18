c72e4bbc514e1f393b015a18dd02ab07b5308131
David Fridley <david@synaccord.com>
2016-05-17 23:26:30 -0700

    about lower case and syntax

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,8 +13,8 @@ class About extends React.Component {
   render() {
     return (
       <section>
-        <h1 About />
-        <h2 "What's Wrong"/>
+        <h1>About</h1>
+        <h2> "What's Wrong"</h2>
         <p> "You’re rational, good-natured, and you genuinely want to find the best solution,
          but politics has become too hostile, deceitful, and uncivilized that you about it. You care, but you’re disengaging because it’s futile and unhealthy."
          </p>
