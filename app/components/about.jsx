e3f45011e819b46e09fc049e4251b582d54eca48
David Fridley <david@synaccord.com>
2016-05-19 17:55:22 -0700

    section and structure

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -5,7 +5,12 @@ import React from 'react';
 class About extends React.Component {
 
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ constructor (props) {
+    super(props);
 
+    this.state = { about: true };
+
+  }
 
 
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
