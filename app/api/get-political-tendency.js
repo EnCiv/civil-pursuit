5e79a5510e8b89222c8983dd7c33642dd9cae08e
David Fridley <david@synaccord.com>
2016-05-21 21:22:02 -0700

    console.info

--- app/api/get-political-tendency.js
+++ app/api/get-political-tendency.js
@@ -3,6 +3,7 @@
 import PoliticalTendency from '../models/political-tendency';
 
 function getPoliticalTendency (cb) {
+	console.info("getPoliticalTendency:", cb, this);
   PoliticalTendency.find().then(cb).catch(this.error.bind(this));
 }
 
