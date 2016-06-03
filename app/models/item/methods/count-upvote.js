fdff14d81d558ec9fc5446bc6b28b242f94ffc56
David Fridley <david@synaccord.com>
2016-06-02 23:53:50 -0700

    query item

--- app/models/item/methods/count-upvote.js
+++ app/models/item/methods/count-upvote.js
@@ -3,8 +3,9 @@
 import Upvote from '../../upvote';
 
 function countUpvote (userId) {
-	console.info("Item.countUpvote", this);
-  return Upvote.getAccumulation( this.Item._id , userId );
+	console.info("Item.countUpvote", this, this.Item, this._id);
+	let itemId = this.Item._id;
+  return Upvote.getAccumulation( itemId , userId );
 }
 
 export default countUpvote;
