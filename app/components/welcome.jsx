fae58515e5fe4ed55a431c3dfb495c3a3b269868
David Fridley <david@synaccord.com>
2016-06-06 14:29:16 -0700

    button on headline

--- app/components/welcome.jsx
+++ app/components/welcome.jsx
@@ -20,10 +20,9 @@ class About extends React.Component {
                 <div className="civil-pursuit news-headline">
                   <div className="civil-pursuit news-headline-inner">
                     <div className="civil-pursuit news-headline-text">Us Finding Solutions to What Divides Us</div>
-                      <div className="civil-pursuit news-headline-text">
-                        <div className="civil-pursuit jump-button">
-                          <a href="#bottom-anchor">Begin</a>
-                        </div>
+                    <div className="civil-pursuit news-headline-text">
+                      <div className="civil-pursuit jump-button">
+                        <a href="#bottom-anchor">Begin</a>
                       </div>
                     </div>
                   </div>
@@ -67,13 +66,13 @@ class About extends React.Component {
                     <a name="bottom_anchor"></a>
                     <p>
                      We have the internet, and smartphones so theres a path for people to come together, but we need a way to keep the conversations productive. So we take what works in realy life meetings and we do that in an app:
+                     </p>
                      <ul>
                       <li>Divide people into small diverse groups</li>
                       <li>Facilitate the conversation in steps that lead to a conclusion</li>
                       <li>Allow people to discuss the pro's and con's of each point</li>
                       <li>Provide feedback to people so they can learn how other's react to what they say</li>
                     </ul>
-                    </p>
                     <h3>Technical:</h3>
                     <p> It’s a web app that works on your phone, tablet and desktop for structured text or video based discussion in small diverse groups.
                     </p>
