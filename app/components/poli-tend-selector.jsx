6213b8ae8747025db85a2b3930baa39501f391fd
David Fridley <david@synaccord.com>
2016-06-07 18:25:58 -0700

    stripped down politendselector

--- app/components/poli-tend-selector.jsx
+++ app/components/poli-tend-selector.jsx
@@ -11,21 +11,14 @@ class PoliTendSelector extends React.Component {
 
   render () {
     console.info("PoliTendSelector.render:",this);
-    let { valueDefault } = this.props;
-
+    
     if(!(window.Synapp && window.Synapp.tendencyChoice)) {console.info("PoliTendSelector: tendencyChoice not ready yet"); return ({}); }
 
-    const tendencyChoice = window.Synapp.tendencyChoice;
-
-    let tendency = tendencyChoice.map(tendency => (
-      <option value={ tendency._id } key={ tendency._id }>{ tendency.name }</option>
-    ));
 
     return (
       <section>
         <Select block medium ref="tendency">
             <option value=''>Choose one</option>
-            { tendency }
         </Select>
       </section>
     );
