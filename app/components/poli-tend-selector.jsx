3817150bb92833a089bc0fcd18e7b7913245849c
David Fridley <david@synaccord.com>
2016-06-07 22:14:46 -0700

    more protection around tendency choice

--- app/components/poli-tend-selector.jsx
+++ app/components/poli-tend-selector.jsx
@@ -9,13 +9,26 @@ class PoliTendSelector extends React.Component {
 
    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  render () {
-    console.info("PoliTendSelector.render:",this);
-   
+  render () 
+    console.info("PoliTendSelector.render:",this.props);
+    let tendencyChoice;
+    const { valueDefault } = this.props;
+
+    
+    if ( typeof window !== 'undefined' ) && window.Synapp && window.Synapp.tendencyChoice ) {
+      tendencyChoice= window.Synapp.tendencyChoice;
+      tendency = tendencyChoice.map(tendency => (
+        <option value={ tendency._id } key={ tendency._id }>{ tendency.name }</option>
+        ));
+    } else {
+      console.info("PoliTendSelector: tendencyChoice not ready yet");
+    }
+
     return (
       <section>
-        <Select block medium ref="tendency">
+        <Select block medium ref="tendency" defaultValue={ valueDefault }>
             <option value=''>Choose one</option>
+            { tendency }
         </Select>
       </section>
     );
