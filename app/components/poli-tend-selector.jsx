a20844d8cb3e20df1edb6df0b520cc98414bffdb
David Fridley <david@synaccord.com>
2016-06-07 17:56:06 -0700

    stripped down politendselector

--- app/components/poli-tend-selector.jsx
+++ app/components/poli-tend-selector.jsx
@@ -7,26 +7,12 @@ import Select                         from './util/select';
 class PoliTendSelector extends React.Component {
 
 
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  setTendency () {
-    console.info("PoliTendSelector.setTendency");
-    const tendency = React.findDOMNode(this.refs.tendency).value;
-
-    if ( tendency ) {
-
-      if(this.props.changeHandler) {
-        this.props.changeHandler(tendency);
-      } else {
-        window.socket.emit('set user info', { tendency });
-      }
-    }
-  }
    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   render () {
     console.info("PoliTendSelector.render:",this);
     let { valueDefault } = this.props;
+
     if(!(window.Synapp && window.Synapp.tendencyChoice)) {console.info("PoliTendSelector: tendencyChoice not ready yet"); return ({}); }
 
     const tendencyChoice = window.Synapp.tendencyChoice;
@@ -37,7 +23,7 @@ class PoliTendSelector extends React.Component {
 
     return (
       <section>
-        <Select block medium ref="tendency" defaultValue={ valueDefault } onChange={ this.setTendency.bind(this) }>
+        <Select block medium ref="tendency" }>
             <option value=''>Choose one</option>
             { tendency }
         </Select>
