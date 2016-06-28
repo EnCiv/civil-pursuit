f7d038d1048ecf1f0737b6c338289fd254e1cffd
David Fridley <david@synaccord.com>
2016-06-27 17:31:43 -0700

    criteria should not be visible on IE, button top still not calc right

--- app/components/vote.jsx
+++ app/components/vote.jsx
@@ -25,7 +25,7 @@ class Vote extends React.Component {
 
   toggleDescription (e) {
     let description = React.findDOMNode(this.refs.description);
-    description.classList.toggle('syn-visible');
+    description.classList.toggle('syn-hide');
   }
 
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -126,7 +126,7 @@ class Vote extends React.Component {
         <Row>
           <Column span="40">
             <h4 onClick={ this.toggleDescription.bind(this) }>{ criteria.name }</h4>
-            <div className="syn-votes-criteria-description" ref="description">
+            <div className="syn-votes-criteria-description syn-hide" ref="description">
               <h5>{ criteria.description }</h5>
             </div>
           </Column>
