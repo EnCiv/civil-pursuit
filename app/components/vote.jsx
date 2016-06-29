09cc67db215bc8862eaeb14d1387d8f91740932e
David Fridley <david@synaccord.com>
2016-06-29 15:49:03 -0700

    implement charts in react

--- app/components/vote.jsx
+++ app/components/vote.jsx
@@ -11,17 +11,18 @@ import Legend                         from './util/legend';
 
 class Vote extends React.Component {
 
+  state = { 
+    itemId: null,
+    total: null,
+    data: [],
+    series: ['-1', '0', '1'],
+    colors: ['#43A19E', '#7B43A1', '#F2317A']
+  };
 
 
   constructor (props) {
     super(props);
-      state = { 
-      itemId: null,
-      total: null,
-      data: [],
-      series: ['-1', '0', '1'],
-      colors: ['#43A19E', '#7B43A1', '#F2317A']
-    };
+
   };
 
 
