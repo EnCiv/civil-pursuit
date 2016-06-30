d8f0fda63d13294844e95768cdd00bde9e472619
David Fridley <david@synaccord.com>
2016-06-30 11:45:28 -0700

    chars on small screens, criteria on bottom with line

--- app/components/util/modal.jsx
+++ app/components/util/modal.jsx
@@ -29,20 +29,20 @@ class Modal extends React.Component {
 
     return (
       <section className={ Component.classList(this, 'syn-modal') } ref="modal">
-        <div className="syn-modal-cover" onClick={ this.hide.bind(this) }></div>
-        <div className="syn-modal-container">
-          <header className="syn-modal-header">
-            <h1>{ this.props.title }</h1>
-          </header>
+        <div className="syn-modal-cover" onClick={ this.hide.bind(this) }>
+          <div className="syn-modal-container">
+            <header className="syn-modal-header">
+              <h1>{ this.props.title }</h1>
+            </header>
 
-          <section>
-            { this.props.children }
-          </section>
+            <section>
+              { this.props.children }
+            </section>
 
-          <footer>
-            <a href="" onClick={ this.hide.bind(this) }>X CLOSE</a>
-          </footer>
-
+            <footer>
+              <a href="" onClick={ this.hide.bind(this) }>X CLOSE</a>
+            </footer>
+          </div>
         </div>
       </section>
     );
