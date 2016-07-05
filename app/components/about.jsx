a781010a1483fee270de89f8e9c35248235420bd
David Fridley <david@synaccord.com>
2016-07-05 14:08:43 -0700

    make mailchimp work

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -6,16 +6,6 @@ class About extends React.Component {
 
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  componentWillMount() {
-        const script = document.createElement("script");
-
-        script.src = "//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js";
-        script.async = true;
-
-        // document.body.appendChild(script);
-        console.info("about: mailchimp script appended.", script);
-  }
-
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   render() {
