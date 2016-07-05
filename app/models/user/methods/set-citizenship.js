8d28b9f679a9af6c1551a844484864bede1757db
David Fridley <david@synaccord.com>
2016-07-05 16:59:07 -0700

    info on citizenship

--- app/models/user/methods/set-citizenship.js
+++ app/models/user/methods/set-citizenship.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function setCitizenship (position, country) {
+	console.info("user.setCiticenship", this, position, country);
   return this.setByIndex('citizenship', { [position] : country });
 }
 
