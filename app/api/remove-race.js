fa6e172ebebc99503ef27ac60c4a4b3ddda5e93d
David Fridley <david@synaccord.com>
2016-07-06 16:05:52 -0700

    is the issue that we are not working on the right user?

--- app/api/remove-race.js
+++ app/api/remove-race.js
@@ -4,10 +4,11 @@ import User from '../models/user';
 
 function removeRace (raceId, cb) {
   try {
-    User
-      .findById(this.synuser.id)
-      .removeRace(raceId)
-      .save()
+  let userdoc =  User.findById(this.synuser.id);
+  console.info("removeRace 1:", userdoc);
+  let moduserdoc =userdoc.removeRace(raceId);
+  console.info("removeRace 2:", userdoc, moduserdoc);
+  User.save(moduserdoc)
       .then(cb, this.error.bind(this));
   }
   catch ( error ) {
