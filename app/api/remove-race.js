2a559b87d59e0171340a2349300c451c7df88220
David Fridley <david@synaccord.com>
2016-07-06 14:05:21 -0700

    expand user into steps

--- app/api/remove-race.js
+++ app/api/remove-race.js
@@ -12,9 +12,10 @@ function removeRace (raceId, cb) {
             if ( ! user ) {
               throw new Error(`No such user with id ${this.synuser.id}`);
             }
-            user
-              .removeRace(raceId)
-              .save()
+            console.info("removeRace 1:", user);
+            let user2= user.removeRace(raceId);
+            console.info("removeRace 2:", user2);            
+            user2.save()
               .then(cb, this.error.bind(this));
               console.info("removerRace after", user);
           }
