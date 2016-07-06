3484d2ca18fe7e77928240ffde0c02f087c00b67
David Fridley <david@synaccord.com>
2016-07-06 11:51:02 -0700

    results after filter

--- app/models/user/methods/remove-race.js
+++ app/models/user/methods/remove-race.js
@@ -3,8 +3,8 @@
 function removeRace (raceId) {
   console.info("user.remove-race:", raceId, this);
 
-  this.race.filter(r => { 
-  		console.info("user.remove-race infilter", r, raceId);
+  this.race = this.race.filter(r => { 
+  		console.info("user.remove-race infilter", r, raceId, r != raceId);
   		return(r != raceId) 
   	});
 
