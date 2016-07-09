6a48ffdcc92ff1ca80c2f3a18ba91d167e91e445
David Fridley <david@synaccord.com>
2016-07-08 18:44:44 -0700

    more info on send status

--- app/api/send-contact-us.js
+++ app/api/send-contact-us.js
@@ -6,19 +6,17 @@ import sendEmail          from '../lib/app/send-email';
 import secret             from '../../secret.json';
 
 function sendContactUs (email, fname, lname, subject, message, cb) {
-  const { host } = this.handshake.headers;
-  console.info("sendContatUs",host, email, fname, lname, subject, message);
+  
+  console.info("sendContatUs", email, fname, lname, subject, message);
 
-  new Promise((pass, fail) => {
+    return(
             sendEmail({
               from      :   secret.email.user,
               to        :   "david@synaccord.com",
               subject   :   subject ,
               text      :   message
-            });
-  })
-    .then(cb)
-    .catch(error => cb({ error : error.message }));
+            })
+            );
 }
 
 export default sendContactUs;
