b749c665d3d861482b3e203310b7b8752eabba41
David Fridley <david@synaccord.com>
2016-07-13 13:26:54 -0700

    mungo verposity and info

--- app/lib/app/model.js
+++ app/lib/app/model.js
@@ -7,6 +7,7 @@ class Model extends Mungo.Model {
   static emit (event, document) {
     return new Promise((ok, ko) => {
       try {
+        console.info("Model", event, this.collection);
         emitter.emit(event, this.collection, document);
         ok();
       }
