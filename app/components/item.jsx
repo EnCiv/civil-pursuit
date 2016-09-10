d864307dafcebbb6e61d1908195369d244cff88b
David Fridley <david@synaccord.com>
2016-09-09 22:16:24 -0700

    item-touch: no stopPropogation

--- app/components/item.jsx
+++ app/components/item.jsx
@@ -174,8 +174,6 @@ class Item extends React.Component {
 
   readMore (e) {
     e.preventDefault();
-    e.stopPropogation();
-    e.nativeEvent.stopImmediatePropagation();
 
     let item = React.findDOMNode(this.refs.item);
 
