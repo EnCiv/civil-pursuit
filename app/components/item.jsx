1ca90a45053ce8563918d5db404de644d19e59c5
David Fridley <david@synaccord.com>
2016-10-30 17:00:35 -0700

    item-column: info

--- app/components/item.jsx
+++ app/components/item.jsx
@@ -141,8 +141,8 @@ class Item extends React.Component {
 
   smoothOpen(target) {
     // set an interval to update scrollTop attribute every 25 ms
-    console.info("item.smoothOpen", target.style.maxHeight, target.style.height);
-    console.info("item.smoothOpen2");
+    console.info("item.smoothOpen", target.style);
+    console.info("item.smoothOpen2", target.style.maxHeight);
     const timer = setInterval( () => {
       console.log(".");
       if(target.style.maxHeight <= target.style.height){
