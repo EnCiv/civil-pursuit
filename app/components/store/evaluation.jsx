39b4016f3ac71901c6380bc19c78773ae9f7424f
David Fridley <david@synaccord.com>
2016-11-01 17:51:09 -0700

    item-column: left/rigth_description ids depricated

--- app/components/store/evaluation.jsx
+++ app/components/store/evaluation.jsx
@@ -55,6 +55,7 @@ class EvaluationStore extends React.Component {
         this.okGetEvaluation.bind(this)
       );
     }
+
     if (document.getElementById('left_description') != null && document.getElementById('right_description') != null) {
         var ly = document.getElementById('left_description').offsetHeight;
         var ry = document.getElementById('right_description').offsetHeight;
@@ -147,8 +148,8 @@ class EvaluationStore extends React.Component {
     }
 
     if ( cursor <= this.state.limit ) {
-      document.getElementById('left_description').style.height = "auto";
-      document.getElementById('right_description').style.height = "auto";
+ //     document.getElementById('left_description').style.height = "auto";
+ //     document.getElementById('right_description').style.height = "auto";
 
       left = this.state.evaluation.items[regular ? cursor - 1 : cursor];
       right = this.state.evaluation.items[regular ? cursor : cursor + 1];
