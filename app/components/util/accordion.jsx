37ecadb20d9375195e97883b2cd17a664bebfe6a
David Fridley <david@synaccord.com>
2016-11-03 21:27:41 -0700

    item-column: smooth accordion

--- app/components/util/accordion.jsx
+++ app/components/util/accordion.jsx
@@ -23,6 +23,7 @@ class Accordion extends React.Component {
 
     if ( this.props.active === true ) {
       this.state.attr = 'show';
+      this.smoothOpen();
     }
     else if ( this.props.active === false ) {
       this.state.attr = 'hide';
@@ -32,17 +33,98 @@ class Accordion extends React.Component {
   componentWillReceiveProps (props = {}) {
     if ( props.active === true ) {
       this.setState({ attr : 'show' });
+      this.smoothOpen();
     }
     else if ( props.active === false ) {
-      this.setState({ attr : 'hide' });
+      smoothClose();
     }
+  }
+
+  smoothOpen() {
+    // set an interval to update scrollTop attribute every 25 ms
+
+    let accordion = React.findDOMNode(this.refs.accordion);
+    let shadow = React.findDOMNode(this.refs.shadow);
+
+    let timerMax=1000;
+
+    let maxHeight = parseInt(accordion.style.maxHeight,10) || 0;
+    let height= accordion.clientHeight;
+    if (maxHeight < height) {
+      accordion.style.maxHeight= height + 'px';
+    }
+
+    shadow.style.width = accordion.offsetWidth + 'px';
+    let rect=shadow.getBoundingClientRect();
+    shadow.style.minHeight= (window.innerHeight - rect.top) + 'px';
+    accordion.style.position='relative';
+    accordion.style.zIndex= -2;
+    accordion.style.overflow= 'visible';
+
+    const timer = setInterval( () => {
+      if(--timerMax == 0 ){ clearInterval(timer); console.error("item.smoothOpen timer overflow");}
+      let lmaxHeight = parseInt(accordion.style.maxHeight,10) || 0;
+      let lheight= accordion.clientHeight;
+      if( lmaxHeight <= lheight ){
+        accordion.style.maxHeight = (lmaxHeight + 7) + 'px';
+        shadow.style.minHeight = Math.max((parseInt(shadow.style.minHeight) - 7), 0) + 'px';
+      } else {
+      // end interval if the scroll is completed
+        clearInterval(timer);
+        accordion.style.overflow="visible";
+        //item.style.position='static';
+        accordion.style.zIndex= 1;
+        shadow.style.minHeight= 0;
+      }
+    }, 25);
+  }
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  smoothClose() {
+    // set an interval to update scrollTop attribute every 25 ms
+    let accordion = React.findDOMNode(this.refs.accordion);
+    let shadow = React.findDOMNode(this.refs.shadow);
+
+    let maxHeight = parseInt(accordion.style.maxHeight,10) || 0;
+    let height= accordion.clientHeight;
+
+    shadow.style.width = item.offsetWidth + 'px';
+    let rect=shadow.getBoundingClientRect();
+    shadow.style.minHeight= (window.innerHeight  - rect.top -7) + 'px';
+    console.info("smoothClose", window.innerHeight - rect.top);
+    accordion.style.position='relative';
+    accordion.style.zIndex= -2;
+    accordion.style.overflow= 'visible';
+    let timerMax=1000;
+
+
+    const timer = setInterval( () => {
+      if(--timerMax == 0 ){ clearInterval(timer); console.error("item.smoothOpen timer overflow");}
+      let lmaxHeight = parseInt(accordion.style.maxHeight,10) || 0;
+      let lheight= accordion.clientHeight;
+      if( lmaxHeight >= lheight ){ //it's still shrinking
+        accordion.style.maxHeight =  (((lmaxHeight - 7) > 0) ? (lmaxHeight - 7) : 0 ) + 'px';
+        shadow.style.minHeight = (window.innerHeight - shadow.getBoundingClientRect().top) + 'px'; 
+      } else {
+      // end interval if the scroll is completed
+        clearInterval(timer);
+        this.setState({ attr : 'hide' });
+        accordion.style.overflow= 'hidden';
+        accordion.style.minHeight= 0;
+        accordion.style.position='static';
+        accordion.style.zIndex= 'auto';
+        shadow.style.minHeight= 0;
+      }
+    }, 25);
   }
 
   render () {
     return (
-      <section className={ `syn-accordion ${this.state.attr}` } ref="wrapper">
+      <section className={ `syn-accordion } ref="accordion">
             { this.props.children }
       </section>
+      <div className="syn-accordion-shadow" ref="shadow">''</div>
     );
   }
 }
