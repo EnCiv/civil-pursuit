584210575e981874ecabad456a05796c46871135
David Fridley <david@synaccord.com>
2016-11-09 23:52:13 -0800

    react-collapse: info

--- app/components/uploader.jsx
+++ app/components/uploader.jsx
@@ -30,7 +30,7 @@ class Uploader extends React.Component {
 
 
     this.upload = new Upload(dropbox, input, bucket, replace);
-
+    console.info("uploader.componentDidMount",input);
     this.upload.init();
 
     this.upload.on('uploaded', this.stream.bind(this));
