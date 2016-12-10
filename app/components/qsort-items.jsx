f02457e59b993b3a13e38503c608bbfc91034f6b
David Fridley <david@synaccord.com>
2016-12-09 18:16:41 -0800

    flipmove: smooth scroll up, relative to current position

--- app/components/qsort-items.jsx
+++ app/components/qsort-items.jsx
@@ -74,20 +74,27 @@ class QSortItems extends React.Component {
   smoothScroll(target, time) {
     // time when scroll starts
     const start = new Date().getTime();
+        const interval=25; //25 mSec
+    
 
         // set an interval to update scrollTop attribute every 25 ms
         const timer = setInterval( () => {
 
+            var doc = document.documentElement;
+            var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+            var distance = target - top; // negative if currently below target
+
             // calculate the step, i.e the degree of completion of the smooth scroll 
-            var step = Math.min(1, (new Date().getTime() - start) / time);
+            let step = Math.min(1, (new Date().getTime() - start) / time);
+
 
             // calculate the scroll distance and update the scrollTop
-            document.body['scrollTop'] = (step * target);
-            document.documentElement.scrollTop=(step * target); //this if for IE!!
+            document.body['scrollTop'] = top + step*distance;
+            document.documentElement.scrollTop= top + step*distance; //this if for IE!!
 
             // end interval if the scroll is completed
             if (step == 1) clearInterval(timer);
-        }, 25);
+        }, interval);
   }
 
 
