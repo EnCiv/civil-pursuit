d88a7416ade22ee5d32350199100a3a36920ef5b
David Fridley <david@synaccord.com>
2016-12-16 13:49:15 -0800

    instruction: simplify id object to find

--- app/api/get-listo-type.js
+++ app/api/get-listo-type.js
@@ -6,7 +6,7 @@ import Config       from '../models/config';
 // import {ObjectId}   from 'mongo';
 
 function getListoType (typeIdList, cb) {
-  var ids = typeIdList.map(function(id) { return ({ "_id": {"$oid": id}}); });
+  var ids = typeIdList.map(function(id) { return ({ "_id": id}); });
   console.info("getListoType", ids);
   Type.find({ "_id": {$in: ids} })
   .then(typeList => cb(typeList.toJSON()))
