431bfdaabe76ef367fefcb11618ac71315ef3f15
David Fridley <david@synaccord.com>
2016-12-16 13:18:47 -0800

    instruction: don't pull in mongo

--- app/api/get-listo-type.js
+++ app/api/get-listo-type.js
@@ -3,12 +3,12 @@
 import sequencer    from 'promise-sequencer';
 import Type         from '../models/type';
 import Config       from '../models/config';
-import {ObjectId}   from 'mongo';
+// import {ObjectId}   from 'mongo';
 
 function getListoType (typeIdList, cb) {
-  var ids = typeIdList.map(function(id) { return ObjectId(id); });
+  var ids = typeIdList.map(function(id) { return ({ "_id": {"$oid": id}}); });
   console.info("getListoType", ids);
-  Type.find({ _id :{$in: ids} })
+  Type.find({ "_id": {$in: ids} })
   .then(typeList => cb(typeList.toJSON()))
   .catch(this.error.bind(this));
 }
