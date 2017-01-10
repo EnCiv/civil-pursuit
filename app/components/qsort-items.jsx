0204696a24bbb39ec0a588b314df4c1def8908cf
David Fridley <david@synaccord.com>
2017-01-10 13:18:41 -0800

    qvote-method: no render sections that don't exist

--- app/components/qsort-items.jsx
+++ app/components/qsort-items.jsx
@@ -122,7 +122,7 @@ class QSortItems extends React.Component {
                 );
             } else {
                 if (this.props.sections['unsorted'].length) { issues++ }
-                Object.keys(this.QSortButtonList).forEach((name) => {
+                Object.keys(this.state.sections).forEach((name) => {
                     let qb = this.QSortButtonList[name];
                     if (qb.max) {
                         if (this.props.sections[name].length > qb.max) {
@@ -167,7 +167,6 @@ class QSortItems extends React.Component {
                     )
                 }
             }
-
         }
 
 
