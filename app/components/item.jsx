fa61b22b79a8e9e36755e52a58a8f262ef23a484
David Fridley <david@synaccord.com>
2017-01-12 15:38:48 -0800

    qsort-refine: info

--- app/components/item.jsx
+++ app/components/item.jsx
@@ -46,7 +46,7 @@ truncateState=0;
 
   transparentEventListener= {};
   transparent(e){
-    e.preventDefault(); console.info("itemTrunkable event", e.type);
+    e.preventDefault(); console.info("itemTrunkable event", e.type, e.target);
   }
 
 
