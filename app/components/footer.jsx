09759c39cd8187b279c7e5ea74e95bd92305836b
David Fridley <david@synaccord.com>
2017-01-19 10:14:37 -0800

    visual-state: link to synaccord

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -7,7 +7,7 @@ class Footer extends React.Component {
     return (
       <footer className="syn-footer">
         <p>
-          Copyright © 2014 - { new Date().getFullYear() } by Synaccord, LLC.
+          Copyright © 2014 - { new Date().getFullYear() } by <a href="http://www.synaccord.com">Synaccord, LLC.</a>
         </p>
         <p>
           <a href="/page/terms-of-service">Terms of Service and Privacy Policy</a>
