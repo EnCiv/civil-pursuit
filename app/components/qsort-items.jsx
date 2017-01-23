f8291f27f077c9b2c71aeeda186af650e85046cf
David Fridley <david@synaccord.com>
2017-01-23 15:09:00 -0800

    visual-state: import

--- app/components/qsort-items.jsx
+++ app/components/qsort-items.jsx
@@ -14,6 +14,7 @@ import Button           from './util/button';
 import QSortButtonList from './qsort-button-list';
 import Creator            from './creator';
 import Accordion          from './util/accordion';
+import Icon               from './util/icon';
 
 
 class QSortItems extends React.Component {
