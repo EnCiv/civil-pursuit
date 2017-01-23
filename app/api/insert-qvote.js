7066e28534ecb33f473b91a1aeff5b04e60b5e80
David Fridley <david@synaccord.com>
2017-01-23 15:39:09 -0800

    visual-state: no votes if not logged in

--- app/api/insert-qvote.js
+++ app/api/insert-qvote.js
@@ -4,6 +4,7 @@ import QVote from '../models/qvote';
 
 function insertQVote (vote) {
  var theVote=vote;
+ if(!this.synuser  && this.synuser.id) return; // can't vote if your not a users (and don't cause an error if you try)
  theVote.user=this.synuser.id; // on the server side we add the userId.
   QVote
     .create(theVote)
