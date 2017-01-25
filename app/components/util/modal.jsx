eadbad187f3e37b90e81b4f238c660df34582e47
David Fridley <david@synaccord.com>
2017-01-25 00:03:48 -0800

    modal: modal: text-align for horizontal, MutationObserver to keep vertical up to date

--- app/components/util/modal.jsx
+++ app/components/util/modal.jsx
@@ -5,6 +5,8 @@ import Component from '../../lib/app/component';
 
 class Modal extends React.Component {
 
+  observer=null;
+
   hide (e) {
 
     e.preventDefault();
@@ -15,22 +17,32 @@ class Modal extends React.Component {
     modal.classList.remove('syn--visible');
   }
 
+  mutations(mutations){
+      console.info("modal mutation");
+      this.hvcenter();
+  }
+
   hvcenter(){
       let modalR=this.refs.modal.getBoundingClientRect();
       let container=this.refs.container;
       let containerR=container.getBoundingClientRect();
-      let left=(modalR.width - containerR.width)/2; // center horizontally
       let top=(modalR.height - containerR.height)/2; // center vertically
-      container.style.left=left + 'px'; 
       container.style.top=top + 'px';
+      // text-align: center in parent div is centering horizontally
   }
 
   componentDidMount(){
     this.hvcenter();
+    this.observer = new MutationObserver( this.mutations.bind(this) );
+    this.observer.observe(target,{attributes: true});
   }
 
   componentDidUpdate(){
     this.hvcenter();
+  }
+
+  componentWillUnmount() {
+    this.observer.disconnect();
   }
 
   render () {
