cb33ceb3943c36c810206482807e1d491d07d45c
David Fridley <david@synaccord.com>
2017-01-25 17:56:13 -0800

    modal: getlistotype

--- app/components/login-panel.jsx
+++ app/components/login-panel.jsx
@@ -1,41 +1,57 @@
 'use strict';
 
-import React                      from 'react';
-import {JoinForm}                 from './join';
-import Panel                      from './panel';
-import config                     from 'syn/../../public.json';
-import TypeComponent              from './type-component';
+import React from 'react';
+import { JoinForm } from './join';
+import Panel from './panel';
+import config from 'syn/../../public.json';
+import TypeComponent from './type-component';
 
 class LoginPanel extends React.Component {
 
-  render () {
-    const { panel, user, active } = this.props;
-
-    if(user) {
-            const newPanel={
-                parent: panel.parent,
-                type: panel.type.harmony[0],
-                skip      :   panel.skip || 0,
-                limit     :   panel.limit || config['navigator batch size'],
-              };
-        return (
-            <TypeComponent component={panel.type.component} { ...this.props } panel={newPanel} />
-        )
+    state = {
+        typeList: []
     }
 
-    let title = panel.type.name  || "User Registration Required";
-    let instruction=panel.type.instruction || "This discussion requsts that all users be registered.";
+    componentDidMount() {
+        console.info("LoginPanel.cDM", this.props)
+        if (typeof window !== 'undefined' && this.props.panel.type.harmony) {
+            window.socket.emit('get listo type', this.props.panel.type.harmony, this.okGetListoType.bind(this))
+        }
+    }
+    okGetListoType(typeList) {
+        this.setState({ typeList: typeList });
+    }
 
-    return (
-        <Panel
-          ref         =   "panel"
-          heading     =   {[<h4>{ title }</h4>]}
-        >
-            <span>{instruction}</span>
-            <JoinForm />
-        </Panel>
-      );
-  }
+    render() {
+        const { panel, user, active } = this.props;
+        console.info("LoginPanel:",this.props, this.state);
+
+        if (user) {
+            if(!this.state.typelist) return(null);
+            const newPanel = {
+                parent: panel.parent,
+                type: this.state.typelist[0],
+                skip: panel.skip || 0,
+                limit: panel.limit || config['navigator batch size'],
+            };
+            return (
+                <TypeComponent  { ...this.props } component={typelist.type.component} panel={newPanel} />
+            )
+        }
+
+        let title = panel.type.name || "User Registration Required";
+        let instruction = panel.type.instruction || "This discussion requsts that all users be registered.";
+
+        return (
+            <Panel
+                ref="panel"
+                heading={[<h4>{title}</h4>]}
+                >
+                <span>{instruction}</span>
+                <JoinForm />
+            </Panel>
+        );
+    }
 }
 
 export default LoginPanel;
