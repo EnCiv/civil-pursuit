a2eb0abc0bc407f7a6f08a0578929279bcb5d390
David Fridley <david@synaccord.com>
2017-01-31 12:10:34 -0800

    syntax

--- app/models/qvote/statics/get-accumulation.js
+++ app/models/qvote/statics/get-accumulation.js
@@ -12,7 +12,7 @@ function getAccumulation (itemId, userId) {
     itemId.forEach(item=> typeof item !== 'object' ? itemA.push({"$oid": item}) : itemA.push(item) );
     // when using mungo aggregate the filter object is passed directly to mongo. It is necessary that all id's are objects in the ObjectId from and not strings
     var query={item: {$in: itemA }};
-    if(userId){ query.user = {"$oid": userId} // get a specific user's accumulation
+    if(userId){ query.user = {"$oid": userId}} // get a specific user's accumulation
     try {
       let accumulation = [];
 
