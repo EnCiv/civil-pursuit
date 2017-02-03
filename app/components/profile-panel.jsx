474643bd05ed5435436487882eaccada2b637ed5
David Fridley <david@synaccord.com>
2017-02-03 11:53:00 -0800

    firstPass and done

--- app/components/profile-panel.jsx
+++ app/components/profile-panel.jsx
@@ -1,7 +1,6 @@
 'use strict';
 
 import React from 'react';
-import { JoinForm } from './join';
 import Panel from './panel';
 import config from 'syn/../../public.json';
 import TypeComponent from './type-component';
@@ -16,24 +15,23 @@ class ProfilePanel extends React.Component {
     state = {
         typeList: [],
         ready: false,
-        userInfo: null
+        userInfo: null,
+        done: false
     }
+
+    firstPass=true;
 
     constructor(props) {
         super(props);
-        if(this.props.user)
-            window.socket.emit('get user info', this.okGetUserInfo.bind(this));
+        if(typeof window !== 'undefined' && this.props.user)
+            window.socket.emit('get user info', this.okGetUserInfo.bind(this))
+                         .on('OK set user info', userInfo => { this.setState({ userInfo })});
+        if (typeof window !== 'undefined' && this.props.panel.type.harmony) 
+            window.socket.emit('get listo type', this.props.panel.type.harmony, this.okGetListoType.bind(this));
     }
 
     okGetUserInfo(userInfo) {
         this.setState({ ready: true, userInfo });
-    }
-
-    componentDidMount() {
-        console.info("ProfilePanel.cDM", this.props)
-        if (typeof window !== 'undefined' && this.props.panel.type.harmony) {
-            window.socket.emit('get listo type', this.props.panel.type.harmony, this.okGetListoType.bind(this))
-        }
     }
 
     okGetListoType(typeList) {
@@ -43,6 +41,7 @@ class ProfilePanel extends React.Component {
     render() {
         const { panel, user, active } = this.props;
         const {userInfo} = this.state;
+        var done=[];
         console.info("ProfilePanel:", this.props, this.state);
 
         if (!user) {
@@ -58,22 +57,36 @@ class ProfilePanel extends React.Component {
             )
         }
 
-        if (this.state.ready && userInfo.gender && userInfo.dob && userInfo.neighborhood && userInfo.member_type) {
-            if (!this.state.typeList.length) return (null);
-            const newPanel = {
-                parent: panel.parent,
-                type: this.state.typeList[1],
-                skip: panel.skip || 0,
-                limit: panel.limit || config['navigator batch size'],
-            };
-            return (
-                <TypeComponent  { ...this.props } component={this.state.typeList[1].component} panel={newPanel} />
-            )
+        if ((this.state.ready && userInfo.gender && userInfo.dob && userInfo.neighborhood && userInfo.member_type)) {
+            if(this.firstPass  || this.state.done){ // if the required data is initally there, then move forward, otherwise move forward when the user to hits done
+                if (!this.state.typeList.length) return (null);  // if we haven't received typeList yet, come back later - there will be another event when it comes in
+                const newPanel = {
+                    parent: panel.parent,
+                    type: this.state.typeList[1],
+                    skip: panel.skip || 0,
+                    limit: panel.limit || config['navigator batch size'],
+                };
+                return (
+                    <TypeComponent  { ...this.props } component={this.state.typeList[1].component} panel={newPanel} />
+                )
+            }else if(!this.firstPass){ // if all the data is there, and this is not the first pass put up the done button
+                done=[
+                    <div className='instruction-text'>
+                        Complete!
+                        <Button small shy
+                            onClick={this.setState.bind(this,{done: true})}
+                            className="profile-panel-done"
+                            style={{ backgroundColor: Color('black'), color: 'white', float: "right" }}
+                            >
+                            <span className="civil-button-text">{"next"}</span>
+                        </Button>
+                    </div>
+                ];
+            } else this.firtPass=false; // the profile data wasn't ready the first time through, so wait for the user to hit done
         }
 
-
-        let title = panel.type.name || "User Registration Required";
-        let instruction = (<div className="instruction-text">This discussion requsts that all users be registered.</div>);
+        let title = panel.type.name || "Participant Profile";
+        let instruction = (<div className="instruction-text">This discussion requsts that all users provide some profile details.</div>);
 
         if (panel.type && panel.type.instruction) {
             instruction = (
@@ -82,11 +95,12 @@ class ProfilePanel extends React.Component {
                 </Instruction>
             );
         }
+    
 
         let content = [];
         if (this.state.ready) {
             content = [
-                <div className='item-profile-panel style={{maxWidth: "150em", margin: "auto"}}'>
+                <div className='item-profile-panel style={{maxWidth: "80em", margin: "auto"}}'>
                     <Gender split={25} user={userInfo} />
                     <Birthdate split={25} user={userInfo} />
                     <Neighborhood split={25} user={userInfo} />
@@ -100,6 +114,7 @@ class ProfilePanel extends React.Component {
                 heading={[<h4>{title}</h4>]}
             >
                 {instruction}
+                {done}
                 {content}
             </Panel>
         );
