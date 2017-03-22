e59223b593080bf11359245152cbc35776e8c9db
David Fridley <david@synaccord.com>
2017-03-22 13:34:25 -0700

    login panel redirects to item.new_location immediately

--- app/components/login-panel.jsx
+++ app/components/login-panel.jsx
@@ -32,11 +32,17 @@ class LoginPanel extends React.Component {
         console.info("LoginPanel:",this.props, this.state);
         var newLocation=this.props.newLocation || null;
         if(!newLocation && panel.parent && panel.parent.new_location) newLocation=panel.parent.new_location;  // get new Location out of the parent item if there is one
+        if(user && newLocation){
+                window.onbeforeunload=null; // don't warn on redirect
+                location.href=newLocation;
+                return null;
+        }
 
-        if(!newLocation && this.state.typeList.length) newLocation="/items/"+this.state.typeList[0].id+"/"+panel.parent.id;
+        if(!newLocation && this.state.typeList.length) newLocation="/items/"+this.state.typeList[0].id+"/"+panel.parent.id;  //new location calculated from next type
         console.info("Login-Panel newlocation", newLocation);
 
         if (user) {
+
             if(!this.state.typeList.length) return(null);
             const newPanel = {
                 parent: panel.parent,
