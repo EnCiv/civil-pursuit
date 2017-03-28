98cb109bd451e07c288142d2e510d9d66e3c7156
David Fridley <david@synaccord.com>
2017-03-28 16:02:57 -0700

    no logger in browser, link to git

--- app/client/main.js
+++ app/client/main.js
@@ -4,34 +4,6 @@ import React              from 'react';
 import ReactDOM			  from 'react-dom';
 import App                from '../components/app';
 import Facebook           from '../lib/app/fb-sdk';
-//import log4js                   from 'log4js';
-//import log4js_extend            from 'log4js-extend';
-
-var log4js = require('log4js');
-
-
-//log4js.loadAppender("console");
-
-
-//log4js.loadAppender(''); 
-//log4js.addAppender(log4js.appenders.stdout()); 
-/*
-log4js.replaceConsole();
-log4js.configure({
-  appenders: [
-    { type: "stdout" }
-  ],
-  replaceConsole: true
-});
-*/
-/*log4js_extend(log4js, {
-  path: __dirname,
-  format: "{at:{n:@name,f:@file,l:@line.@column}}"
-});
-*/
-
-//if(!window.logger) window.logger = log4js.getLogger(); else console.log("window.logger already defined");
-
 
 window.socket = io();
 
