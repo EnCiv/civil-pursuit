b6a70292bedef9d5d845c2173e62f26268f25787
David Fridley <david@synaccord.com>
2017-04-06 22:53:23 -0700

    info on logger

--- app/client/main.js
+++ app/client/main.js
@@ -8,6 +8,7 @@ import Facebook           from '../lib/app/fb-sdk';
 // process has to be defined before log4js is imported on the browser side.
 if(typeof process === 'undefined') {
   window.process={stdout: {write: function (str) {console.log(str)}}};
+  process.stdout.write("stdout installed");
 }
 
 import log4js                   from 'log4js';
@@ -22,12 +23,14 @@ if(typeof global != 'undefined' && !global.logger) {
   // running on node and logger isn't defined yet
   global.logger = log4js.getLogger('node');
   global.logger.setLevel("INFO");
+  logger.info("client main running on node");
 }
 
 if(typeof window != 'undefined' && !window.logger) {
   // running on browser and logger isn't defined yet
   window.logger = log4js.getLogger('browser');
   window.logger.setLevel("INFO");
+  console.info("client main running on browser");
 }
 
 window.socket = io();
