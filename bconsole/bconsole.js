22ea6868677f126556fe9b31fba0383d25c94414
David Fridley <david@synaccord.com>
2017-04-07 15:09:14 -0700

    more info, no layout

--- bconsole/bconsole.js
+++ bconsole/bconsole.js
@@ -2,13 +2,15 @@
 var layouts = require('log4js').layouts;
 
 function bconsoleAppender (layout, timezoneOffset) {
-  layout = layout || layouts.messagePassThroughLayout;
+  console.info("bconsoleAppender",layout, timezoneOffset);
+  //layout = layout || layouts.messagePassThroughLayout;
   return function(loggingEvent) {
     console.info(loggingEvent);
   };
 }
 
 function configure(config) {
+  console.info("bconsole.configure")
   var layout;
   if (config.layout) {
     layout = layouts.layout(config.layout.type, config.layout);
