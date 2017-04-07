cd23cb155d7483bbc78872288c6eb4c12974b9d9
David Fridley <david@synaccord.com>
2017-04-06 22:35:09 -0700

    trying again to put logger on browser

--- app/client/main.js
+++ app/client/main.js
@@ -5,6 +5,31 @@ import ReactDOM			  from 'react-dom';
 import App                from '../components/app';
 import Facebook           from '../lib/app/fb-sdk';
 
+// process has to be defined before log4js is imported on the browser side.
+if(typeof process === 'undefined') {
+  window.process={stdout: {write: function (str) {console.log(str)}}};
+}
+
+import log4js                   from 'log4js';
+import log4js_extend            from 'log4js-extend';
+
+log4js_extend(log4js, {
+  path: __dirname,
+  format: "{at:{n:@name,f:@file,l:@line.@column}}"
+});
+
+if(typeof global != 'undefined' && !global.logger) {
+  // running on node and logger isn't defined yet
+  global.logger = log4js.getLogger('node');
+  global.logger.setLevel("INFO");
+}
+
+if(typeof window != 'undefined' && !window.logger) {
+  // running on browser and logger isn't defined yet
+  window.logger = log4js.getLogger('browser');
+  window.logger.setLevel("INFO");
+}
+
 window.socket = io();
 
 window.socket.on('welcome', user => {
@@ -16,6 +41,7 @@ window.socket.on('welcome', user => {
 
 function render (props) {
   console.log('Rendering app', props);
+  logger.info('Rendering app', props);
   ReactDOM.render(<App { ...props } />, document.getElementById('synapp'));
 }
 
