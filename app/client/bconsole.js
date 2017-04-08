9c0cc1d77260c1fcc1c03d6dc6c2f2789003a6f5
David Fridley <david@synaccord.com>
2017-04-07 21:11:08 -0700

    date.toString.split

--- app/client/bconsole.js
+++ app/client/bconsole.js
@@ -5,8 +5,8 @@
 // so this default layout will preserve the objects and pass them to console.log
 function bconsoleAppender (layout, timezoneOffset) {
   layout = layout || function(e,t){
-    var d=e.startTime; 
-    return [d.getFullYear()+d.toLocaleString({month: 'short'})+d.getDate()+' '+d.getHours()+':'+d.getMinutes()+':'+d.getSeconds()+'.'+d.getMilliseconds(),
+    var d=e.startTime.toString().split(' '); 
+    return [d[3]+d[1]+d[2]+' '+d[4],
      e.categoryName, ...e.data]
   }
   return function(loggingEvent) {
