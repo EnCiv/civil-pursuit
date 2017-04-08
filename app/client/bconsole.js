836d565048bc6982ce052723a19dca2c8ffde53a
David Fridley <david@synaccord.com>
2017-04-07 20:55:16 -0700

    compant date

--- app/client/bconsole.js
+++ app/client/bconsole.js
@@ -4,7 +4,11 @@
 // on the broswer, console.info display objects in a way that is compact and lets you expand them and dig down.
 // so this default layout will preserve the objects and pass them to console.log
 function bconsoleAppender (layout, timezoneOffset) {
-  layout = layout || function(e,t){var date=e.startTime.split(' '); return [date[3]+date[1]+date[2]+' '+date[4], e.categoryName, ...e.data]}
+  layout = layout || function(e,t){
+    var d=e.startTime; 
+    return [d.getFullYear()+d.toLocaleString({month: 'short'})+d.getDate()+' '+d.getHours()+':'+d.getMinutes()+':'+d.getSeconds()+'.'+d.getMilliseconds(),
+     e.categoryName, ...e.data]
+  }
   return function(loggingEvent) {
     console.log(...layout(loggingEvent, timezoneOffset));
   };
