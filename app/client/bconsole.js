3ac2361e9e91eaa5fd3465042de475dd1b1cb342
David Fridley <david@synaccord.com>
2017-04-07 20:09:20 -0700

    compacted log info, and log4js_extend

--- app/client/bconsole.js
+++ app/client/bconsole.js
@@ -1,12 +1,12 @@
 "use strict";
 
 
-
+// on the broswer, console.info display objects in a way that is compact and lets you expand them and dig down.
+// so this default layout will preserve the objects and pass them to console.log
 function bconsoleAppender (layout, timezoneOffset) {
-  layout = layout || function(e,t){return [e.startTime, e.categoryName, ...e.data]}
-
+  layout = layout || function(e,t){var date=e.startTime.split(' '); return [date[3]+date[1]+date[2]+' '+date[4], e.categoryName, ...e.data]}
   return function(loggingEvent) {
-    console.info(...layout(loggingEvent, timezoneOffset));
+    console.log(...layout(loggingEvent, timezoneOffset));
   };
 }
 
