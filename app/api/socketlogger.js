8d5c3b90cdd859161302d3cbe28492e7a1f91aaa
David Fridley <david@synaccord.com>
2017-04-11 13:50:08 -0700

    bslogger.log

--- app/api/socketlogger.js
+++ app/api/socketlogger.js
@@ -2,8 +2,8 @@
 
 function socketlogger (loggingEvent) {
     loggingEvent.data.push({socketId: this.id, userId: this.synuser.id || 'anonymous'});
-    bslogger.emit('log',loggingEvent);
 //    bslogger[loggingEvent.level.levelStr.toLowerCase()](loggingEvent.startTime, ...loggingEvent.data);
+    bslogger.log(loggingEvent);
 }
 
 export default socketlogger;
