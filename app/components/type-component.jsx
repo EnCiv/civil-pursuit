b3587ca71043fba01ffc1e0a5d94921e8281d6e4
David Fridley <david@synaccord.com>
2017-04-18 18:42:41 -0700

    type-components has manual index

--- app/components/type-component.jsx
+++ app/components/type-component.jsx
@@ -1,46 +1,15 @@
 
 'use strict';
 
-import React                            from 'react';
-import * as components                  from "./type-components/"
-
+import React                       from 'react';
+import Components                  from "./type-components/"
 
 class TypeComponent extends React.Component{
-    history=false;
-
-/**
-    static components={
-        'PanelList': PanelList,
-        'QHome': QHome,
-        'Subtype': Subtype,
-        'QSortItems': QSortItems,
-        'QSortWhy': QSortWhy,
-        'QSortRefine': QSortRefine,
-        'QSortReLook': QSortReLook,
-        'LoginPanel': LoginPanel,
-        'QSortFinale': QSortFinale,
-        'ProfilePanel': ProfilePanel,        
-    }
-
-**/
     render(){
-        console.info("TypeComponent",components);
+        const Component = this.props.component ? Components[this.props.component]
+                        : (this.props.panel && this.props.panel.type && this.props.panel.type.component) ? Components[this.props.panel.type.component]
+                        : Components.Subtype;
 
-        if(typeof window !== undefined && !this.history && this.props.panel && this.props.panel.type){
-            let typeSId=this.props.panel.type.id || 'notypeid';
-            let parentSId= this.props.panel.parent ? this.props.panel.parent.id || 'noparentid' : 'noparent';
-            window.history.pushState({},"",window.location.pathname+'items/'+typeSId+'/'+parentSId+'/');
-            this.history=true;
-        }
-  
-        var Component;
-        if(this.props.component){
-            Component=components[this.props.component];
-        } else if(this.props.panel && this.props.panel.type && this.props.panel.type.component) {
-            Component=components[this.props.panel.type.component];
-        } else {
-            Component=components.Subtype;
-        }
         logger.info("TypeComponent", this.props );
         return(<Component {...this.props} /> );
     }
--- app/components/type-components/index.js
+++ app/components/type-components/index.js
@@ -1,2 +1,30 @@
 // @create-index
+// imagine one day a program that automatically generates this (in ES5 compatible format).
+// until that day, this is manual
+'use strict';
 
+import QHome                            from './qhome';
+import PanelList                        from './panel-list';
+import Subtype                          from './subtype';
+import QSortItems                       from './qsort-items';
+import QSortWhy                         from './qsort-why';
+import QSortRefine                      from './qsort-refine';
+import QSortReLook                      from './qsort-harmony';
+import QSortFinale                      from './qsort-finale';
+import LoginPanel                       from './login-panel';
+import ProfilePanel                     from './profile-panel';
+
+var  Components={
+        'PanelList': PanelList,
+        'QHome': QHome,
+        'Subtype': Subtype,
+        'QSortItems': QSortItems,
+        'QSortWhy': QSortWhy,
+        'QSortRefine': QSortRefine,
+        'QSortReLook': QSortReLook,
+        'LoginPanel': LoginPanel,
+        'QSortFinale': QSortFinale,
+        'ProfilePanel': ProfilePanel,        
+    }
+
+export default Components;
