84f1cb69b2073796cf3b373f5fa7ec2e1404579c
David Fridley <david@synaccord.com>
2017-05-10 00:18:45 -0700

    equally

--- app/components/user-interface-manager.jsx
+++ app/components/user-interface-manager.jsx
@@ -156,13 +156,20 @@ class UserInterfaceManager extends React.Component {
     /***  don't rerender if no change in state, props don't matter if it didn't change the state. ****/
     shouldComponentUpdate(newProps, newState) {
         logger.info("UserInterfaceManager.shouldComponentUpdate",this.props.uim && this.props.uim.depth, this.state,newState);
-        //if (!Object.keys(this.state.uim).every(key=>{logger.info(key); let a=this.state.uim[key], b=newState.uim[key] || null; if(a!=b) logger.info(a,"!=",b); return a==b})) {logger.info("yes"); return true};
-        if(!isEqualWith(this.state,newState,(a,b)=>{
-            logger.info(a,b, typeof a, a==b);
+        var equaly=function(a,b){
+            logger.info(a,b);
+            if(typeof a === 'undefined') a=null;
+            if(typeof b === 'undefined') b=null;
+            if(!a && !b) return true; //false, 0, undefined, null both are falsy
             let at=typeof a;
-            if(at==='string' || at==='number') return a==b;
-            else return undefined;
-        })) {logger.info("yes"); return true};
+            if(at !== typeof b) return false; // if not falsy and types are not equal, they are not equal
+            if(at === 'object') return union(Object.keys(a),Object.keys(b)).every(key=>equaly(a[key],b[key]));
+            if(at === 'function') return true; //treat functions are equal no matter what they are
+            if(a && b) return a==b; // if both are truthy are the equal
+            return false;
+        }
+        //if (!Object.keys(this.state.uim).every(key=>{logger.info(key); let a=this.state.uim[key], b=newState.uim[key] || null; if(a!=b) logger.info(a,"!=",b); return a==b})) {logger.info("yes"); return true};
+        if(!equaly(this.state,newState)){logger.info("yes"); return true};
         logger.info("no"); return false;
     }
 
