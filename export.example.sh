26bb67ae130da697c6cef363d7c862e8a0167003
David@Synaccord <david@synaccord.com>
2017-06-16 13:25:14 -0700

    clairigied export example, works on windows finally

--- export.example.sh
+++ export.example.sh
@@ -5,14 +5,25 @@
 # Then call it like this from terminal ". export.sh"
 # Make sure git ignores the file you created.
 
+#copy databases over - path is for windows
+"C:\Program Files\MongoDB\Server\3.4\bin\mongorestore.exe" -h localhost -d synapp ../dump-dir/image-name
+
 # Mongo DB URL
-export MONGOHQ_URL="mongodb://....";
+export MONGOHQ_URL="mongodb://localhost:27017/synapp"
 
-# HTTP Port (OPTIONAL)
-export PORT="Port number (default: 3000)";
+# HTTP Port (OPTIONAL) default 3000
+export PORT="80";
 
-# App instance
-export SYNAPP_ENV="the name of the instance";
+# App instance name - used to look up things in public.json
+export SYNAPP_ENV="alpha";
 
 # Selenium Test target
 export SYNAPP_SELENIUM_TARGET="http://...";
+
+# Node environment
+export NODE_ENV=production
+
+# CDN service
+export CLOUDINARY_URL="cloudinary://642125633197983:Y_ml8LZ8WhvDTCCMtW2roxCRBlg@hcv1vvrxn"
+
+
