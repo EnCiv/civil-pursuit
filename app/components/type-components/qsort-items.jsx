6bf81c460524cc7c7579a34300ab0309d80ed4a7
David@Synaccord <david@synaccord.com>
2017-06-21 21:37:53 -0700

    QVote passes props

--- app/components/type-components/qsort-items.jsx
+++ app/components/type-components/qsort-items.jsx
@@ -30,7 +30,7 @@ class QSortItems extends React.Component {
                     type={this.props.type}
                     limit={20} >
             <QVoteStore>
-                <ReactActionStatePath {...this.props}>
+                <ReactActionStatePath >
                     <RASPQSortItems />
                 </ReactActionStatePath>
             </QVoteStore>
