a460653c01c7710751821dfeae32400f5cd9e9fa
David@Synaccord <david@synaccord.com>
2017-07-11 23:01:40 -0700

    QsortItems with PanelHead

--- app/components/type-components/qsort-items.jsx
+++ app/components/type-components/qsort-items.jsx
@@ -1,14 +1,12 @@
 'use strict';
 
 import React from 'react';
-import Panel from '../panel';
 import panelType from '../../lib/proptypes/panel';
 import QSortButtons from '../qsort-buttons';
 import ItemStore from '../store/item';
 import FlipMove from 'react-flip-move';
 import QSortFlipItem from '../qsort-flip-item'
 import smoothScroll from '../../lib/app/smooth-scroll';
-import Instruction from '../instruction';
 import Color from 'color';
 import Button           from '../util/button';
 import QSortButtonList from '../qsort-button-list';
@@ -19,6 +17,7 @@ import PanelStore from '../store/panel';
 import QVoteStore from '../store/qvote';
 import {ReactActionStatePath, ReactActionStatePathClient} from 'react-action-state-path';
 import update from 'immutability-helper';
+import PanelHead from '../panel-head';
 
 
   // 20 is hard coded, but where should this be? type or item?
@@ -31,9 +30,11 @@ class QSortItems extends React.Component {
                     type={this.props.type}
                     limit={20} >
             <QVoteStore {...this.props}>
-                <ReactActionStatePath>
-                    <RASPQSortItems />
-                </ReactActionStatePath>
+                <PanelHead  cssName={'syn-qsort-items'} >
+                    <ReactActionStatePath>
+                        <RASPQSortItems />
+                    </ReactActionStatePath>
+                </PanelHead>
             </QVoteStore>
         </PanelStore>
         );
@@ -106,96 +107,69 @@ class RASPQSortItems extends ReactActionStatePathClient {
         console.info("RASPQSortItems.render");
 
         const { panel, count, user, emitter, rasp } = this.props;
+        const {items, type, parent}=panel;
 
         const onServer = typeof window === 'undefined';
 
-        let title = 'Loading items', name, loaded = false, articles = [], loadMore, creator,
-            type, parent, items, direction = [], instruction = [], issues = 0, done = [], loading=[];
+        let articles = [], creator,
+            direction = [], instruction = [], issues = 0, done = [], loading=[];
 
-        if (panel) {
-            items = panel.items;
-            loaded = true;
-
-            type = this.props.type || panel.type;  // if a type was passed, use that one rather than the panel type. We are operating on the parents matching items not ours
-
-            parent = panel.parent;
-
-            if (type) {
-                name = `syn-panel-${type._id}`;
-            } else {
-                name = 'syn-panel-no-type';
-            }
-
-            if (parent) {
-                //name += `-${parent._id || parent}`; VSCode doesn't parse this syntax
-                name += ('-'+ (parent.id || parent));
-            }
-
-            title = type.name;
-
-            if (type && type.instruction) {
-                instruction = (
-                    <Instruction >
-                        {type.instruction}
-                    </Instruction>
-                );
-            }
-
-            if (!Object.keys(this.props.index).length) {
-                console.info("qsort-items creator")
-                loading.push(
-                    <div className="gutter text-center">
-                        <a href="#" onClick={ this.toMeFromChild.bind(this, null, {type: "TOGGLE_CREATOR"}) } className="click-to-create">
-                            Click the + to be the first to add something here
-                        </a>
-                    </div>
-                );
-            } else {
-                if (this.props.sections['unsorted'].length) { issues++ }  
-                Object.keys(this.QSortButtonList).forEach((criteria) => {  // the order of the buttons matters, this as the reference. props.sections may have a different order because what's first in db.
-                    if(!this.props.sections[criteria]){ return; }
-                    let qb = this.QSortButtonList[criteria];
-                    if (qb.max) {
-                        if (this.props.sections[criteria].length > qb.max) {
-                            direction.push(
-                                <div className='instruction-text' style={{ backgroundColor: Color(qb.color).darken(0.1) }}>
-                                    {qb.direction}
-                                </div>
-                            )
-                            issues++;
-                        }
+        
+        if (!Object.keys(this.props.index).length) {
+            console.info("qsort-items creator")
+            loading.push(
+                <div className="gutter text-center">
+                    <a href="#" onClick={ this.toMeFromChild.bind(this, null, {type: "TOGGLE_CREATOR"}) } className="click-to-create">
+                        Click the + to be the first to add something here
+                    </a>
+                </div>
+            );
+        } else {
+            if (this.props.sections['unsorted'].length) { issues++ }  
+            Object.keys(this.QSortButtonList).forEach((criteria) => {  // the order of the buttons matters, this as the reference. props.sections may have a different order because what's first in db.
+                if(!this.props.sections[criteria]){ return; }
+                let qb = this.QSortButtonList[criteria];
+                if (qb.max) {
+                    if (this.props.sections[criteria].length > qb.max) {
+                        direction.push(
+                            <div className='instruction-text' style={{ backgroundColor: Color(qb.color).darken(0.1) }}>
+                                {qb.direction}
+                            </div>
+                        )
+                        issues++;
                     }
-                    this.props.sections[criteria].forEach(itemId => {
-                        let item = items[this.props.index[itemId]];
-                        articles.push(
-                            {
-                                sectionName: criteria,
-                                qbuttons: this.QSortButtonList,
-                                user: user,
-                                item: item,
-                                id: item._id,
-                                rasp: {shape: 'truncated', depth: rasp.depth, button: criteria, toParent: this.toMeFromChild.bind(this,item._id)}
-                            }
-                        );
-                    });
+                }
+                this.props.sections[criteria].forEach(itemId => {
+                    let item = items[this.props.index[itemId]];
+                    articles.push(
+                        {
+                            sectionName: criteria,
+                            qbuttons: this.QSortButtonList,
+                            user: user,
+                            item: item,
+                            id: item._id,
+                            rasp: {shape: 'truncated', depth: rasp.depth, button: criteria, toParent: this.toMeFromChild.bind(this,item._id)}
+                        }
+                    );
                 });
-                if (!issues) {
-                    var results={index: this.props.index, sections: this.props.sections, panel: this.props.panel }
-                    done.push(
-                        <div className='instruction-text'>
-                            {this.QSortButtonList['unsorted'].direction}
-                            <Button small shy
-                                onClick={()=>this.props.rasp.toParent({type: "NEXT_PANEL", status: "done", results})}
-                                className="qsort-done"
-                                style={{ backgroundColor: Color(this.QSortButtonList['unsorted'].color).negate(), color: this.QSortButtonList['unsorted'].color, float: "right" }}
-                                >
-                                <span className="civil-button-text">{"next"}</span>
-                            </Button>
-                        </div>
-                    )
-                    this.props.rasp.toParent({type: "RESULTS", results});
-                } else this.props.rasp.toParent({type: "ISSUES"});
-            }
+            });
+            if (!issues) {
+                var results={index: this.props.index, sections: this.props.sections, panel: this.props.panel }
+                done.push(
+                    <div className='instruction-text'>
+                        {this.QSortButtonList['unsorted'].direction}
+                        <Button small shy
+                            onClick={()=>this.props.rasp.toParent({type: "NEXT_PANEL", status: "done", results})}
+                            className="qsort-done"
+                            style={{ backgroundColor: Color(this.QSortButtonList['unsorted'].color).negate(), color: this.QSortButtonList['unsorted'].color, float: "right" }}
+                            >
+                            <span className="civil-button-text">{"next"}</span>
+                        </Button>
+                    </div>
+                )
+                this.props.rasp.toParent({type: "RESULTS", results});
+            } else this.props.rasp.toParent({type: "ISSUES"});
+
             let creatorPanel;
 
             creatorPanel = (
@@ -215,44 +189,24 @@ class RASPQSortItems extends ReactActionStatePathClient {
                 { creatorPanel }
                 </Accordion>
             );
-
         }
 
 
         return (
             <section id="syn-panel-qsort">
-                <Panel
-                    className={name}
-                    ref="panel"
-                    heading={[(<h4>{title}</h4>)]}
-                    heading     =   {[
-                        ( <h4>{ title }</h4> ), !(user && user.id && parent && parent.user && parent.user._id && (user.id === parent.user._id) ) ? (null) : 
-                        (
-                        <Icon
-                            icon        =   "plus"
-                            className   =   "toggle-creator"
-                            onClick     =   { this.toMeFromChild.bind(this, null, {type: "TOGGLE_CREATOR"})}
-                        />
-                        )
-                    ]}
-
-                    type={type}
-                    >
-                    {creator}
-                    {instruction}
-                    {direction}
-                    {done}
-                    <div style={{ position: 'relative',
-                                  display: 'block',
-                    }}>
-                        <div className="qsort-flip-move-articles">
-                            <FlipMove duration={this.motionDuration} onFinishAll={this.onFlipMoveFinishAll.bind(this)} disableAllAnimations={onServer}>
-                                {articles.map(article => <QSortFlipItem {...article} key={article.id} />)}
-                            </FlipMove>
-                        </div>
+                {creator}
+                {direction}
+                {done}
+                <div style={{ position: 'relative',
+                                display: 'block',
+                }}>
+                    <div className="qsort-flip-move-articles">
+                        <FlipMove duration={this.motionDuration} onFinishAll={this.onFlipMoveFinishAll.bind(this)} disableAllAnimations={onServer}>
+                            {articles.map(article => <QSortFlipItem {...article} key={article.id} />)}
+                        </FlipMove>
                     </div>
-                    {loading}
-                </Panel>
+                </div>
+                {loading}
             </section>
         );
     }
