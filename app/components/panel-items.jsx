90cd3eaa4f8d656571a98019f555a868ca92207d
David@Synaccord <david@synaccord.com>
2017-07-20 10:10:13 -0700

    RASP with debug info, panel items info

--- app/components/panel-items.jsx
+++ app/components/panel-items.jsx
@@ -41,6 +41,7 @@ class RASPPanelItems extends ReactActionStatePathClient {
 
   actionToState(action, rasp) {
     var nextRASP = {}, delta = {};
+    console.info("PanelItems.actionToState", action, rasp);
     if (action.type === "CHILD_SHAPE_CHANGED") {
       let ash = action.shape, ush = rasp.shape;
       if (!action.shortId) logger.error("PanelItems.actionToState action without shortId", action)
