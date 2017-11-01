87e23a1beaadf9b2c9448744d6e45d85eac905cb
David@Synaccord <david@synaccord.com>
2017-10-31 18:24:54 -0700

    touchmove - wheel not on smartphones

--- app/lib/util/fixed-scroll.js
+++ app/lib/util/fixed-scroll.js
@@ -3,8 +3,7 @@
 function fixedScroll(){
     var h=document.getElementsByTagName("html")[0];
     let b=h.getBoundingClientRect();
-    h.setAttribute("style","position: fixed; width:"+b.width+'px; top:'+(-b.top)+'px;')
-    var l1=h.addEventListener("wheel",(e)=>{
+    function handler(e) {
         e.stopImmediatePropagation();
         e.preventDefault();
         e.returnValue=false;
@@ -13,7 +12,10 @@ function fixedScroll(){
         if(top<(-b.height))top= (-b.height);
         else if (top>0) top=0;
         h.style.top=top+'px'
-    })
+    }
+    h.setAttribute("style","position: fixed; width:"+b.width+'px; top:'+(-b.top)+'px;');
+    var l1=h.addEventListener("wheel",handler);
+    var l2=h.addEventListener("touchmove",handler);
 }
 
 export default fixedScroll;
