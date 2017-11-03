562444f50b6d2a06f65cc6ebe38058f78b0e3d69
David@Synaccord <david@synaccord.com>
2017-11-02 17:40:23 -0700

    scroll-swipe added

--- app/lib/util/fixed-scroll.js
+++ app/lib/util/fixed-scroll.js
@@ -1,21 +1,85 @@
 'use strict';
+var ScrollSwipe = require('../lib/ScrollSwipe'); //or just use the global window.ScrollDirect if using this on the client;
 
-function fixedScroll(){
-    var h=document.getElementsByTagName("html")[0];
-    let b=h.getBoundingClientRect();
-    function handler(e) {
-        e.stopImmediatePropagation();
-        e.preventDefault();
-        e.returnValue=false;
-        let top=(parseInt(h.style.top,10)-e.deltaY);
-        let b=h.getBoundingClientRect();
+class FixedScroll{
+	constructor() {
+		this.target=document.getElementsByTagName("html")[0];
+		this.target.style.position="fixed";
+		this.target.style.top="0px";
+		return this;
+	}
+
+	touchMoveCb(){
+		return this.touchMove.bind(this);
+	}
+
+	touchCb(){
+		return this.touch.bind(this);
+	}
+
+	touchMove(data) {
+		let {deltaX, deltaY}=data;
+        let top=parseFloat(this.target.style.top) || 0;
+        let b=this.target.getBoundingClientRect();
+        top+=deltaY;
         if(top<(-b.height))top= (-b.height);
         else if (top>0) top=0;
-        h.style.top=top+'px'
-    }
-    var l1=h.addEventListener("wheel",handler);
-    var l2=h.addEventListener("touchmove",handler, {passive: false});
-    h.setAttribute("style","position: fixed; width:"+b.width+'px; top:'+(-b.top)+'px;');
+		this.target.style.top=top+deltaY+'px';
+	}
+
+	touch(data){
+		let {deltaX, deltaY}=data;
+		var top=parseFloat(this.target.style.top) || 0;
+		if(Math.abs(deltaY)>2){
+			this.decelerate(top, deltaY);
+		}
+		ss.listen();
+	}
+
+	scrollCb(){
+		return this.scroll.bind(this);
+	}
+
+	scroll(data){
+		let deltaY=data.lastEvent.deltaY;
+        var top=parseFloat(this.target.style.top) || 0;
+        top=top-deltaY;
+        let b=this.target.getBoundingClientRect();
+        if(top<(-b.height))top= (-b.height);
+        else if (top>0) top=0;
+		this.target.style.top=(top)+'px';
+		ss.listen();
+	}
+
+	decelerate(top,deltaY){
+        top=top+deltaY;
+        
+        let b=this.target.getBoundingClientRect();
+        if(top<(-b.height))top= (-b.height);
+        else if (top>0) top=0;
+
+		this.target.style.top=top+'px';
+
+		if(Math.abs(deltaY)>2) return setTimeout(()=>this.decelerate(top,deltaY*.9),20)
+		else if(deltaY<0) return setTimeout(()=>this.target.style.top=Math.ceil(top)-1+'px',20)
+		else return setTimeout(()=>this.target.top=Math.ceil(top)+'px',20)
+	}
+}
+
+
+function fixedScroll(){
+    var fS=new FixedScroll();
+
+    var ss = new ScrollSwipe({
+        target: document.body, // can be a div, or anything else you want to track scroll/touch events on
+        scrollSensitivity: 0, // the lower the number, the more sensitive
+        touchSensitivity: 0, // the lower the number, the more senitive,
+        scrollPreventDefault: true, // prevent default option for scroll events
+        touchPreventDefault: true, // prevent default option for touch events
+        scrollCb: fS.scrollCb(),
+        touchCb: fS.touchCb(),
+        touchMoveCb: fS.touchMoveCb()
+    });
 }
 
 export default fixedScroll;
