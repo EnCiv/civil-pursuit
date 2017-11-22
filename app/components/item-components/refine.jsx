4d51b15e6ce12751ba8ffab95a1e7b74062ebc40
David@Synaccord <david@synaccord.com>
2017-11-21 20:56:01 -0800

    RASPified refine panel

--- app/components/item-components/refine.jsx
+++ app/components/item-components/refine.jsx
@@ -9,42 +9,50 @@ import EvaluationStore from '../store/evaluation';
 import Promote from '../promote';
 import ItemStore from '../store/item';
 import Item from '../item';
+import {ReactActionStatePath, ReactActionStatePathClient} from 'react-action-state-path';
+
+
 
 
 exports.panel = class RefinePanel extends React.Component {
+    initialRASP={chosen: 'promote'};
+    render(){
+        return (
+        <ReactActionStatePath {...this.props} initialRASP={this.initialRASP}>
+            <RASPRefinePanel />
+        </ReactActionStatePath>
+        );
+    }
+}
 
-    toChild = [];
-    state={chosen: 'promote'};
-    
-    toMeFromChild(key, action) {
-        //onsole.info("RefinePanel.toMeFromChild", this.props.rasp.depth, key, action);
-        if (action.type === "SET_TO_CHILD") { // child is passing up her func
-            if(Object.keys(this.toChild).length) {
-                 this.toChild[key] = action.function;
-                 return null;
-            } else { // this is the first so notify parent
-                this.toChild[key] = action.function;
-                return this.props.rasp.toParent({ type: "SET_TO_CHILD", function: this.toMeFromParent.bind(this), name: this.constructor.name });  // notify parent of your existence after child existence known
-            }
-        } else if(action.type ==="SHOW_ITEM") {
-            this.setState({chosen: 'winner'});
+class RASPRefinePanel extends ReactActionStatePathClient {
+    constructor(props){
+        super(props,'chosen');
+        this.createDefaults();
+    }
+
+    actionToState(action,rasp,source,initialRASP) {
+        var nextRASP={}, delta={};
+        if(action.type ==="SHOW_ITEM") {
+            delta.chosen='winner';
         } else if(action.type ==="ITEM_DELVE") {
-            this.setState({chosen: 'winner'});
-            action.type="ITEM_REFINE"; // changing the action so that <Item> does not process it, and passes it to <QSortRefine> 
+            delta.chosen='winner';
+            //action.type="ITEM_REFINE"; // changing the action so that <Item> does not process it, and passes it to <QSortRefine> 
+            this.qaction(()=>this.props.rasp.toParent({type: "ITEM_REFINE"}));
         } else if(action.type==="RESET"){
-            this.setState({chosen: 'promote'});
-        }
+            delta.chosen='promote';
+        } else
+            return null;
+        Object.assign(nextRASP,rasp,delta);
+        return nextRASP;
         return this.props.rasp.toParent(action);
     }
 
-    toMeFromParent(action) {
-        //onsole.info("RefinePanel.toMeFromParent", this.props.rasp.depth, action);
-        if (action.type === "CLEAR_PATH") {  // clear the path and reset the RASP state back to what the const
-            this.setState({chosen: 'promote'});
-        }
-        if(this.toChild[this.state.chosen]) return this.toChild[this.state.chosen](action);
-        else return null;
+    segmentToState(action,initialRASP){
+        return initialRASP;
     }
+
+    mounted={};
 
     render() {
         const { item, user, style, rasp, winner, whyItemId, type, unsortedColor='#fff' } = this.props;
@@ -53,19 +61,20 @@ exports.panel = class RefinePanel extends React.Component {
         //this.chosen= winner ? 'winner' : 'promote';
         return (
             <div className="toggler promote" key={item._id + '-toggler-' + this.constructor.name}>
-                <Accordion active={this.state.chosen==='winner'}>
-                    { winner ? 
-                        <ItemStore item={winner} key={`item-${winner && winner._id || 'none'}`}>
+                <Accordion active={rasp.chosen==='winner'}>
+                    { this.mounted[winner] ? this.mounted[winner] : winner ? 
+                        (this.mounted[winner]=
+                         (<ItemStore item={winner} key={`item-${winner && winner._id || 'none'}`}>
                             <Item
                                 item={winner}
                                 user={user}
-                                rasp={{ depth: rasp.depth, shape: rasp.shape, toParent: this.toMeFromChild.bind(this, 'winner') }}
+                                rasp={this.childRASP(rasp.shape, 'winner')}
                             />
-                        </ItemStore>
+                        </ItemStore>))
                         : null
                     }
                 </Accordion>
-                <Accordion active={this.state.chosen==='promote'} style={{ backgroundColor: unsortedColor }}>
+                <Accordion active={rasp.chosen==='promote'} style={{ backgroundColor: unsortedColor }}>
                     <EvaluationStore
                         item-id={whyItemId}
                         active={active}
@@ -74,7 +83,7 @@ exports.panel = class RefinePanel extends React.Component {
                             ref="promote"
                             panel={panel}
                             user={user}
-                            rasp={{ depth: rasp.depth, shape: rasp.shape, toParent: this.toMeFromChild.bind(this, 'promote') }}
+                            rasp={this.childRASP(rasp.shape, 'promote')}
                             hideFeedback={true}
                             hideFinish={true}
                         />
