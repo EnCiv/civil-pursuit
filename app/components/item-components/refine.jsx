d100502dae43d18119e6fdf165c197d497c39287
David@Synaccord <david@synaccord.com>
2017-11-24 14:55:28 -0800

    RefinePanel uses Decendant_Unfocus to decide chosen, QSortRefine looks at show_item or Refine_item

--- app/components/item-components/refine.jsx
+++ app/components/item-components/refine.jsx
@@ -33,10 +33,9 @@ class RASPRefinePanel extends ReactActionStatePathClient {
 
     actionToState(action,rasp,source,initialRASP) {
         var nextRASP={}, delta={};
-        if(action.type ==="SHOW_ITEM") {
+        if(action.type === "DECENDANT_UNFOCUS"){
             delta.chosen='winner';
         } else if(action.type ==="ITEM_DELVE") {
-            delta.chosen='winner';
             //action.type="ITEM_REFINE"; // changing the action so that <Item> does not process it, and passes it to <QSortRefine> 
             var nextAction=Object.assign({},action,{type: "ITEM_REFINE"}); // create a new action with all the parameters of the old action
             this.qaction(()=>this.props.rasp.toParent(nextAction));
