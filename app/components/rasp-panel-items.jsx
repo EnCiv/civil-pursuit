923ceb8c90039a46a3c462c25f33998cfcc012c1
David@Synaccord <david@synaccord.com>
2017-12-01 22:41:07 -0800

    removed unused import config, Icon, and EditAndGoAgain

--- app/components/rasp-panel-items.jsx
+++ app/components/rasp-panel-items.jsx
@@ -2,10 +2,7 @@
 
 import React from 'react';
 import Accordion          from 'react-proactive-accordion';
-import Icon from './util/icon';
 import ItemStore from '../components/store/item';
-import EditAndGoAgain from './edit-and-go-again';
-import config from '../../public.json';
 import {ReactActionStatePath, ReactActionStatePathClient } from 'react-action-state-path';
 import Item from './item';
 
