f63cd61e18a2e0728f811258b0909fd24e6e5d5a
David@Synaccord <david@synaccord.com>
2017-12-01 22:10:53 -0800

    import Item prevents super typeerror, rm import ItemStore

--- app/components/panel-items.jsx
+++ app/components/panel-items.jsx
@@ -3,10 +3,10 @@
 import React from 'react';
 import Accordion          from 'react-proactive-accordion';
 import Icon from './util/icon';
-import ItemStore from '../components/store/item';
 import EditAndGoAgain from './edit-and-go-again';
 import config from '../../public.json';
 import {ReactActionStatePath, ReactActionStatePathClient } from 'react-action-state-path';
+import Item from './item';
 import PanelHead from './panel-head';
 import RASPPanelItems from './rasp-panel-items';
 import ClassNames from 'classnames';
