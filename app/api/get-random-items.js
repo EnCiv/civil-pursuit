11580c5baf3554dde320e30480d6b8f7a214d315
David@Synaccord <david@synaccord.com>
2017-12-20 12:08:59 -0800

    getrandom items also gets most important if there are any

--- app/api/get-random-items.js
+++ app/api/get-random-items.js
@@ -4,29 +4,28 @@ import Item from '../models/item';
 import QVote from '../models/qvote';
 import Mungo from 'mungo';
 
-function getRandomItems (panel, size, cb) {
+function getRandomItems(panel, size, cb) {
   try {
-    let id        =   'panel-' + panel.type._id || panel.type;
-    const query   =   { type : panel.type._id || panel.type};
+    let id = 'panel-' + panel.type._id || panel.type;
+    const query = { type: panel.type._id || panel.type };
     const userId = this.synuser ? this.synuser.id : null;
 
-
-    if ( panel.parent ) {
-      const parentId = panel.parent._id || panel.parent; 
+    if (panel.parent) {
+      const parentId = panel.parent._id || panel.parent;
       id += '-' + parentId;
       query.parent = parentId;
     }
 
-    if ( panel.skip ) {
+    if (panel.skip) {
       query.skip = panel.skip;
     }
 
-    if(panel.limit) {
+    if (panel.limit) {
       query.limit = panel.limit;
     }
 
-    if(panel.own) {
-      if(userId) {
+    if (panel.own) {
+      if (userId) {
         query.user = userId;
       } else {
         cb(panel, 0); // request to get the users's own items but no user logged in so return nothing
@@ -35,45 +34,70 @@ function getRandomItems (panel, size, cb) {
 
     QVote.aggregate([
       // items the use has voted on
-      {$match: {user: Mungo.Type.ObjectID.convert(userId)}},
+      { $match: { user: Mungo.Type.ObjectID.convert(userId) } },
       // add the itemInfo about the items the user has voted on
-      { $lookup: {
-        from: "items",
-        localField: "item",
-        foreignField: "_id",
-        as: "itemInfo"
-      }},
-      {$unwind: "$itemInfo"},
-      {$match: {"itemInfo.type":  Mungo.Type.ObjectID.convert(panel.type), "itemInfo.parent" : Mungo.Type.ObjectID.convert(panel.parent)}},
-      {$group: {_id: "nin", "nin": {$addToSet: "$itemInfo._id"}}},
-      {$project: {"nin": true, "_id":false }}
-    ]).then(results=>{
-      if(results && results.length){
-        let nin=results[0].nin;
-        if(nin.length) query._id={["$nin"]: nin};  // exclude items the users has already voted on
+      {
+        $lookup: {
+          from: "items",
+          localField: "item",
+          foreignField: "_id",
+          as: "itemInfo"
+        }
+      },
+      { $unwind: "$itemInfo" },
+      { $match: { "itemInfo.type": Mungo.Type.ObjectID.convert(panel.type), "itemInfo.parent": Mungo.Type.ObjectID.convert(panel.parent) } },
+      { $group: { _id: "$criteria", "itemIds": { $addToSet: "$itemInfo._id" } } }
+    ]).then(qsections => {
+      var nin = [];
+      var mosts=[];
+      if (qsections && qsections.length) {
+        qsections.forEach(criteria => { 
+          nin = nin.concat(criteria.itemIds); 
+          if(criteria._id==='most')
+            mosts=criteria.itemIds; 
+        });
+        if (nin.length) query._id = { ["$nin"]: nin };  // exclude items the users has already voted on
       }
-      Item
-      .getRandomItems(query, size, userId)
-      .then(
-        results => {
-          try {
-            if(!panel.items) { panel.items = []; }
-            panel.items = panel.items.concat(results.items);
-            cb(panel);
-          }
-          catch ( error ) {
-            ko(error);
-          }
+      if ((!panel.items || !panel.items.length) && mosts.length) { // if the panel is empty, but the user has perviously voted
+        Item.find({ _id: { $in: mosts } }).then(items => {
+          if (!panel.items) panel.items = [];
+          if(items && items.length) panel.items = panel.items.concat(items);
+
+          Item
+            .getRandomItems(query, size, userId)
+            .then(results => {
+              if(results && results.items) panel.items = results.items.concat(panel.items);
+              cb(panel);
+            },
+            this.error.bind(this)
+            );
         },
-        this.error.bind(this)
-      );
-    })
+          this.error.bind(this)
+        )
+      } else {
+        Item
+          .getRandomItems(query, size, userId)
+          .then( results => {
+            try {
+              if (!panel.items) { panel.items = []; }
+              panel.items = panel.items.concat(results.items);
+              cb(panel);
+            }
+            catch (error) {
+              ko(error);
+            }
+          },
+          this.error.bind(this)
+          );
+      }
+    },
+      this.error.bind(this)
+      )
   }
 
-  catch ( error ) {
+  catch (error) {
     this.error(error);
   }
-
 }
 
 export default getRandomItems;
