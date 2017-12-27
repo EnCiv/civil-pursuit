697eeda578bcd3c87be801ce0137f09268e4d374
David@Synaccord <david@synaccord.com>
2017-12-27 09:36:22 -0800

    qvote-totals renderChildren cleaned up - but shouldn't fix any problems

--- app/components/store/qvote-totals.jsx
+++ app/components/store/qvote-totals.jsx
@@ -5,75 +5,71 @@ import update from 'immutability-helper';
 
 class QVoteTotals extends React.Component {
 
-  state = { finale: [] };
+    state = { finale: [] };
 
-  constructor(props){
-      super(props);
-      if(this.props.shared.items){
-        this.props.shared.items.map((item, i)=>{ 
-                let obj={item: item._id, index: i};
-                Object.keys(this.props.shared.sections).forEach(sectionName=>{obj[sectionName]=0});
+    constructor(props) {
+        super(props);
+        if (this.props.shared.items) {
+            this.props.shared.items.map((item, i) => {
+                let obj = { item: item._id, index: i };
+                Object.keys(this.props.shared.sections).forEach(sectionName => { obj[sectionName] = 0 });
                 this.state.finale.push(obj)
             });
         }
-  }
+    }
 
 
-  componentDidMount () {
-        if(this.props.shared && this.props.shared.items){
-            var idList=[];
-            this.props.shared.items.map(item=>idList.push(item._id));
+    componentDidMount() {
+        if (this.props.shared && this.props.shared.items) {
+            var idList = [];
+            this.props.shared.items.map(item => idList.push(item._id));
             window.socket.emit('get qvote info', idList, false, this.okGetQVoteInfo.bind(this));
         }
-  }
+    }
 
     componentWillReceiveProps(newProps) { //just read in the new props and through out the old ones and fetch new votes
-    //onsole.info("qvote-totals: newProps");
-      var newFinale=[];
-      var idList=[];
-      if(newProps.shared && newProps.shared.items){
-        newProps.shared.items.map((item, i)=>{ 
-                let obj={item: item._id, index: i};
-                Object.keys(newProps.shared.sections).forEach(sectionName=>{obj[sectionName]=0});
+        //onsole.info("qvote-totals: newProps");
+        var newFinale = [];
+        var idList = [];
+        if (newProps.shared && newProps.shared.items) {
+            newProps.shared.items.map((item, i) => {
+                let obj = { item: item._id, index: i };
+                Object.keys(newProps.shared.sections).forEach(sectionName => { obj[sectionName] = 0 });
                 newFinale.push(obj);
                 idList.push(item._id);
             });
-        window.socket.emit('get qvote info', idList, false, this.okGetQVoteInfo.bind(this));
-        this.setState({finale: newFinale});
+            window.socket.emit('get qvote info', idList, false, this.okGetQVoteInfo.bind(this));
+            this.setState({ finale: newFinale });
         }
     }
 
-  okGetQVoteInfo(accumulation) {
-      var newFinale=this.state.finale.slice(0);
-      //onsole.info("QVoteTotal got qvote info length,accumulation.length");
-      if(accumulation.length){
-        accumulation.forEach(qvote => {
-            newFinale.some((qobj, i)=>{
-                if(qobj.item===qvote.item){
-                    Object.assign(newFinale[i],qvote.results);
-                    return true;
-                } else 
-                return false;
+    okGetQVoteInfo(accumulation) {
+        var newFinale = this.state.finale.slice(0);
+        //onsole.info("QVoteTotal got qvote info length,accumulation.length");
+        if (accumulation.length) {
+            accumulation.forEach(qvote => {
+                newFinale.some((qobj, i) => {
+                    if (qobj.item === qvote.item) {
+                        Object.assign(newFinale[i], qvote.results);
+                        return true;
+                    } else
+                        return false;
+                })
             })
-        })
-        newFinale.sort((a,b)=>{return (b.most - a.most);})  // sort so largest most is first in list
-        this.setState({finale: newFinale});
-      }
-  }
-
-    renderChildren(moreProps) {
-        return React.Children.map(this.props.children, (child, i) => {
-            var {children, ...newProps} = this.props;
-            Object.assign(newProps, this.state, moreProps);
-            return React.cloneElement(child, newProps, child.props.children)
-        });
+            newFinale.sort((a, b) => { return (b.most - a.most); })  // sort so largest most is first in list
+            this.setState({ finale: newFinale });
+        }
     }
 
-  render () {
-    return (
-      <section>{ this.renderChildren() }</section>
-    );
-  }
+    render() {
+        const { children, ...childProps } = this.props;
+        Object.assign(childProps, this.state);
+        return (
+            <section>
+                {React.Children.map(React.Children.only(children), child => React.cloneElement(child, childProps, child.props.children))}
+            </section>
+        );
+    }
 }
 
 export default QVoteTotals;
