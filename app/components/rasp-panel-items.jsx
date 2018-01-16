e8c4ff3d83aad4385c13b5b5dc5f3e3555292120
David@Synaccord <david@synaccord.com>
2018-01-15 16:36:18 -0800

    rasp-panel-items vM: ooview - uses shortId for focus state

--- app/components/rasp-panel-items.jsx
+++ app/components/rasp-panel-items.jsx
@@ -171,19 +171,18 @@ export default class RASPPanelItems extends ReactActionStatePathClient {
             delta.shortId=null;
             delta.decendantFocus=false;
             delta.creator=false;
-        } else if ((action.type === "FOCUS") || (action.type==="TOGGLE_FOCUS" && !rasp.focus) || (action.type === "FOCUS_STATE")) {
-          delta.focus = true;
+        } else if ((action.type === "FOCUS") || (action.type==="TOGGLE_FOCUS" && !rasp.shortId) || (action.type === "FOCUS_STATE")) {
           delta.decendantFocus=false;
-          if(rasp.shortId) this.toChild[rasp.shortId]({type: "UNFOCUS_STATE"});
-          delta.shortId=null;
+          if(rasp.shortId && (action.shortId !== rasp.shortId)) this.toChild[rasp.shortId]({type: "UNFOCUS_STATE"});
+          if(action.shortId) delta.shortId = action.shortId;
           if(action.type!=="FOCUS_STATE") {
             this.queueFocus(action);
           }
-        } else if ((action.type === "UNFOCUS") || (action.type==="TOGGLE_FOCUS" && rasp.focus) || (action.type === "UNFOCUS_STATE")) {
-          delta.focus = false;
+        } else if ((action.type === "UNFOCUS") || (action.type==="TOGGLE_FOCUS" && rasp.shortId) || (action.type === "UNFOCUS_STATE")) {
           delta.decendantFocus=false;
           delta.creator=false;
           if(rasp.shortId)this.toChild[rasp.shortId]({type: "RESET_SHAPE"});
+          delta.shortId=null;
           if(action.type!=="UNFOCUS_STATE")
             this.queueUnfocus(action);
         } else if (action.type === "ITEM_DELVE") {
