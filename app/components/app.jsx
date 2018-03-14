55ce63af89ae5930272d8a00e01a5a7d7b7aedc6
David@Synaccord <david@synaccord.com>
2018-03-14 10:57:20 -0700

    pass props to small layout

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -207,8 +207,8 @@ class App extends React.Component {
             let component=last.type.component || 'Subtype';
 
           return (
-            <SmallLayout user={ user } RASPRoot={'/i/'} setPath={this.setPath.bind(this)}>
-              <TypeComponent component={component} { ...this.props } user={ user } count = { 1 } panel={ last } />
+            <SmallLayout {...this.props} user={ user } RASPRoot={'/i/'}  setPath={this.setPath.bind(this)}>
+              <TypeComponent component={component} count = { 1 } panel={ last } />
             </SmallLayout>
           );
         }
