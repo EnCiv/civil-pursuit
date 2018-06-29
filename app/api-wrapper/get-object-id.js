d1839f7d84a2cad50d3689002dba4ea3f784aa0e
David@Synaccord <david@synaccord.com>
2018-06-29 13:16:36 -0700

    get-object-id

--- app/api-wrapper/get-object-id.js
+++ app/api-wrapper/get-object-id.js
@@ -1,12 +1,12 @@
 'use strict';
 
 import apiWrapper from '../lib/util/api-wrapper';
-import Mungo from 'mungo';
+import ObjectID from 'bson-objectid';
 
 function getObjectId(cb){
     setTimeout(()=>{
-        var _id=new Mungo.mongodb.ObjectID();
-        cb(Mungo.mongodb.ObjectID(_id));
+        var _id=new ObjectID();
+        cb(ObjectID(_id).toHexString());
     })
 }
 
