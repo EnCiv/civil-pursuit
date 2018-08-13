8911efd0eb2db03aa9d3d0642a268b59a83bd793
David@Synaccord <david@synaccord.com>
2018-08-13 13:29:12 -0700

    logup bar is like logout span

--- app/components/logup-bar.jsx
+++ app/components/logup-bar.jsx
@@ -1,93 +1,85 @@
 'use strict';
 
-import React                          from 'react';
-import ReactDOM                       from 'react-dom';
-import TopBar                         from './top-bar';
-import Footer                         from './footer';
-import Panel                          from './panel';
-import ReactScrollBar from './util/react-scrollbar';
-import EmailInput                     from './util/email-input';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import EmailInput from './util/email-input';
 import Button from './util/button';
-import Login from './login';
 import Components from "./panel-components/";
 import ListComponent from './list-component';
 
 
 class Logup extends React.Component {
-    
-    state={validationError: null, successMessage: null }
 
-    logup () {
-        let email = ReactDOM.findDOMNode(this.refs.email).value;
-        if ( email ) {
-            window.socket.emit('set user info', { email }, this.login.bind(this,email,this.props.user && this.props.user.tempid));
-        }
+    state = { error: null, info: null }
+
+    static validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
     }
-    
-    login(email, password){
-        Login.signIn(email,this.props.user.tempid)
-        .then(
-            () => {
-                this.setState({ validationError : null, successMessage : 'Welcome, sending password reset email' });
-                setTimeout(()=>{
+
+    logup() {
+        let email = ReactDOM.findDOMNode(this.refs.email).value;
+        if (!LogoutSpan.validateEmail(email)) {
+            return this.setState({ error: "email address not valid", info: null });
+        } else {
+            window.socket.emit('set user info', { email }, () => {
+                this.setState({ error: null, info: 'Welcome, sending password reset email' }, () => {
                     window.socket.emit('send password', email, window.location.pathname, response => {
-                        if ( response.error ) {
-                          let { error } = response;
-                  
-                          if ( error === 'User not found' ) {
-                            error = 'Email not found';
-                          }
-                  
-                          this.setState({ info : null, validationError : error });
+                        if (response.error) {
+                            let { error } = response;
+
+                            if (error === 'User not found') {
+                                error = 'Email not found';
+                            }
+
+                            this.setState({ info: null, error: error });
                         }
                         else {
-                          this.setState({ info : null, successMessage : 'Message sent! Please check your inbox' });
+                            this.setState({ error: null, info: 'Message sent! Please check your inbox' });
                         }
-                        setTimeout(() => location.href = window.location.pathname, 800); 
-                      });
-                },800);
-            },
-            error => {
-                this.setState({ validationError : error.message })
-            }
-        );
+                        setTimeout(() => location.href = '/sign/out', 800);
+                    });
+                });
+            })
+        }
     }
 
-    getBannerNode(){
-        return this.refs.banner;
-      }
 
-  render () {
-    const { user, rasp } = this.props;
-    const {children, ...lessProps} = this.props;
-    const iconWidth=(typeof window !== 'undefined') ? window.Synapp.fontSize*2 : 13*2;
-    const type= {
-        _id: "SmallLayout",
-        title: "Complete login",
-        instruction: "A temporary Id has been assigned so you can continue in this discussion. This Id is stored in your browser (as a cookie). When you logout, it will be lost and un retrivable and your input will be eventually be deleted. When you set your email address your registration can be retreived using the forgot password link", 
-      };
-    if(user && user.id && !user.email)
-        return (
-            <div className="logup-bar" ref="banner">
-                <div className="logup-bar-center">
-                    <span className="logup-bar-title">Complete setup</span>
-                    <div className="logup-bar-input">
-                        <EmailInput autoFocus required placeholder="Email" ref="email" name="email" />
+    getBannerNode() {
+        return this.refs.banner;
+    }
+
+    render() {
+        const { user, rasp } = this.props;
+        const { children, ...lessProps } = this.props;
+        const iconWidth = (typeof window !== 'undefined') ? window.Synapp.fontSize * 2 : 13 * 2;
+        const type = {
+            _id: "SmallLayout",
+            title: "Complete login",
+            instruction: "A temporary Id has been assigned so you can continue in this discussion. This Id is stored in your browser (as a cookie). When you logout, it will be lost and un retrivable and your input will be eventually be deleted. When you set your email address your registration can be retreived using the forgot password link",
+        };
+        if (user && user.id && !user.email)
+            return (
+                <div className="logup-bar" ref="banner">
+                    <div className="logup-bar-center">
+                        <span className="logup-bar-title">Complete setup</span>
+                        <div className="logup-bar-input">
+                            <EmailInput autoFocus required placeholder="Email" ref="email" name="email" />
+                        </div>
+                        <div className="logup-bar-button">
+                            <Button block large success radius onClick={this.logup.bind(this)}>Save</Button>
+                        </div>
+                        <ListComponent Components={Components} {...lessProps} type={type} component={'Instruction'} part={'button'} key={rasp.raspId + '-' + 'button'} position={0.5 * iconWidth} />
                     </div>
-                    <div className="logup-bar-button">
-                        <Button block large success radius onClick={this.logup.bind(this)}>Save</Button>
+                    <div>{this.state.info}{this.state.error}</div>
+                    <div className="logup-bar-instruction">
+                        <ListComponent Components={Components} {...lessProps} type={type} component={'Instruction'} part={'panel'} key={rasp.raspId + '-' + 'button'} position={0.5 * iconWidth} />
                     </div>
-                    <ListComponent Components={Components} {...lessProps} type={type} component={'Instruction'} part={'button'} key={rasp.raspId + '-' + 'button'} position={0.5*iconWidth} />
                 </div>
-                <div>{this.state.successMessage}{this.state.validationError}</div>
-                <div className="logup-bar-instruction">
-                    <ListComponent Components={Components} {...lessProps} type={type} component={'Instruction'} part={'panel'} key={rasp.raspId + '-' + 'button'} position={0.5*iconWidth} />
-                </div>
-            </div>
-        );
-    else
-        return null;
-  }
+            );
+        else
+            return null;
+    }
 }
 
 export default Logup;
