5954827f9a4056f26190c0820c119cf55e2a2fbb
David@Synaccord <david@synaccord.com>
2018-09-16 13:14:52 -0700

    Turk comments

--- app/lib/mechanical-turk-task.js
+++ app/lib/mechanical-turk-task.js
@@ -1,4 +1,5 @@
 'use strict';
+// The Mechanical Turk values will be stored in class, all components that include this class will be able to access the same TASK files.  No need to React Context
 
 class MechanicalTurkTask {
     static setFromProps(props){
