d923da30a513572f47f83c4729dc2f87d9e79205
David@Synaccord <david@synaccord.com>
2018-10-27 13:19:24 -0700

    QSortWhy first edit doesn't dobule toggle

--- app/components/type-components/qsort-why.jsx
+++ app/components/type-components/qsort-why.jsx
@@ -101,7 +101,7 @@ class RASPQSortWhy extends ReactActionStatePathClient {
             this.results.why[this.whyName][itemId]=action.item._id;
             this.setState({ 'sections': QSortToggle(this.state.sections,itemId,this.whyName,'set')});
         } else if(action.type==="DESCENDANT_FOCUS"){
-            this.setState({ 'sections': QSortToggle(this.state.sections,itemId,this.whyName)});
+            this.setState({ 'sections': QSortToggle(this.state.sections,itemId,this.buttons[0],'set')}); // - set incase of double events to unsorted
         } else if(action.type==="RESET"){
             Object.assign(this.props.shared,clone(this._defaults.that.results));
             return null;
