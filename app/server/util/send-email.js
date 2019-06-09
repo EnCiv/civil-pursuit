48f49de316c5ad4be461d0581b126bcf9b37bbbf
David@Synaccord <david@synaccord.com>
2019-06-09 11:24:53 -0700

    nodemailer, clone

--- app/server/util/send-email.js
+++ app/server/util/send-email.js
@@ -1,47 +1,35 @@
 'use strict';
 
-import nodemailer         from 'nodemailer';
-import sequencer          from 'promise-sequencer';
-import secret             from 'syn/../../secret.json';
+import nodemailer from 'nodemailer';
+import secret from 'syn/../../secret.json';
 
 const transporter = nodemailer.createTransport({
-  service : 'Zoho',
-  auth: {
-    user :  secret.email.user,
-    pass :  secret.email.password
-  }
+	service: 'Zoho',
+	auth: {
+		user: secret.email.user,
+		pass: secret.email.password
+	}
 });
 
-function sendEmail (options = {}) {
-  console.log('Sending email', options);
-
-  return new Promise((pass, fail) => {
-    if ( ! options.to ) {
-      return fail(new Error('Missing email recipient'));
-    }
-
-    if ( ! options.subject ) {
-      return fail(new Error('Missing email subject'));
-    }
-
-    if ( process.env.NODE_ENV != 'production' ) {
-      console.log('Not sending emails when not in production', process.env.NODE_ENV);
-      return pass();
-    }
-
-    sequencer(
-      ()        =>    sequencer.promisify(transporter.sendMail.bind(this), [options]),
-      results   =>    new Promise((pass, fail) => {
-        if ( results.response === '250 Message received' ) {
-          pass();
-        }
-        else {
-          console.error("sendEmail failed with:", results.response)
-          fail(new Error(results.response));
-        }
-      })
-    ).then(pass, fail);
-  });
+function sendEmail(options = {}) {
+	console.log('Sending email', options);
+	return new Promise(async (pass, fail) => {
+		if (!options.to)
+			return fail(new Error('Missing email recipient'));
+		if (!options.subject)
+			return fail(new Error('Missing email subject'));
+		if (process.env.NODE_ENV !== 'production') {
+			console.log('Not sending emails when not in production', process.env.NODE_ENV);
+			return pass();
+		}
+		let results = await transporter.sendMail(options)
+		if (results.response === '250 Message received')
+			pass();
+		else {
+			console.error("sendEmail failed with:", results.response)
+			fail(new Error(results.response));
+		}
+	});
 }
 
 export default sendEmail;
