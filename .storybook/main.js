5a7fd208e3b43b33feb759cbcb50fa0f30c0d286
David Fridley <ddfridley@yahoo.com>
2023-08-27 21:28:25 -0700

    storybook starting to work

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,12 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 
-import custom from './webpack.config.js';
+//import custom from './webpack.config.js';
+import {merge} from 'webpack-merge'
+const webpack=require("webpack");
+const path = require("path");
 
 const config = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -17,15 +20,33 @@ const config = {
   docs: {
     autodocs: 'tag',
   },
-
   webpackFinal: async (config) => {
-    return {
-      ...config,
-      module: { ...config.module, rules: [...config.module.rules, ...custom.module.rules] },
 
+    const newConfig=merge(config,{
+      module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                //include: /(.*profile.*)/, // for some reason, webpack (4.25.1) will exclude files with names containing 'profile' (or 'profile-' not sure) so I has to explicitly include them
+                loader: "babel-loader",
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            }
+        ]
+    },
       resolve: {
         extensions: ['.*','.js','.jsx'],
         fallback: {
+          "fs": false,
           "os": require.resolve("os-browserify/browser"),
           "http": require.resolve("stream-http"),
           "https": require.resolve("https-browserify"),
@@ -37,7 +58,19 @@ const config = {
 
         }
       },
-    };
-  },
+      plugins:[
+        new webpack.IgnorePlugin({resourceRegExp: /clustered|dateFile|file|fileSync|gelf|hipchat|logFacesAppender|loggly|logstashUDP|mailgun|multiprocess|slack|smtp/},/(.*log4js.*)/),  // these appenders are require()ed by log4js but not used by this app
+        new webpack.IgnorePlugin({resourceRegExp: /nodemailer/}), // not used in the client side - those should be move outside of the app directory
+  
+        // using a function because when this ran on heroku using just "../modules/client-side-model" failed
+          new webpack.NormalModuleReplacementPlugin(/.+models\/.+/, resource => {
+              resource.request = "../models/client-side-model";
+          }),
+  
+          new webpack.HotModuleReplacementPlugin()  // DO NOT use --hot in the command line - it will cause a stack overflow on the client
+      ]
+    })
+    return newConfig
+  }
 }
 export default config
