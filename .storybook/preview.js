f78e723dea56fe1fac9ff9d7e9842e3686382d2c
Boyuan Yu <yu.boyu@northeastern.edu>
2023-09-25 12:00:35 -0700

    item.stories draft first commit
    converted 70%

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,8 @@
 /** @type { import('@storybook/react').Preview } */
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';  // Adjust based on your React version
+configure({ adapter: new Adapter() });
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
