aad34553b3e0a028926d03f78223a3780042cdaa
Julian Cardone <juliancardone1@gmail.com>
2023-09-26 14:58:54 -0400

    implement focusability tests

--- stories/react-scroll.stories.jsx
+++ stories/react-scroll.stories.jsx
@@ -1,294 +1,150 @@
 import React from 'react'
 
-const specs = () => {}
-const describe = () => {}
-const it = () => {}
-
-// import {ReactWrapper, mount} from "enzyme";
 import expect from 'expect'
 
 import Common from './common'
 
 import ReactScrollBar from '../app/components/util/react-scrollbar'
 
+import { fireEvent, within } from '@storybook/testing-library'
+
 class Layout extends React.Component {
-  render() {
-    return (
-      <div style={Common.outerStyle}>
-        <div
-          ref={e => {
-            if (!this.topBar && e) {
-              this.topBar = e
-              this.forceUpdate()
-            }
-          }}
-          style={{
-            height: '100px',
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            zIndex: 2,
-            backgroundColor: 'rgba(255,255,255,.9)',
-            margin: 0,
-            textAlign: 'center',
-            verticalAlign: 'middle',
-          }}
-        >
-          Banner
-        </div>
-        <ReactScrollBar style={{}} topBar={this.topBar} extent={0}>
-          <div id="scroll-bar">{this.props.children}</div>
-        </ReactScrollBar>
-      </div>
-    )
-  }
+    render(){
+        return (
+            <div style={Common.outerStyle}>
+                <div ref={e=>{if(!this.topBar && e) {this.topBar=e;this.forceUpdate()}}} style={{height: "100px", position: "fixed", top: 0, left: 0, right: 0,zIndex: 2, backgroundColor: "rgba(255,255,255,.9)", margin: 0,textAlign: "center", verticalAlign: "middle"}}>Banner</div>
+                <ReactScrollBar style={{}} topBar={this.topBar} extent={0}>
+                    <div id="scroll-bar">
+                        {this.props.children}
+                    </div>
+                </ReactScrollBar>
+            </div>
+        )
+    }
 }
 
 export default {
-  title: 'react-scroll',
+  title: 'react-scroll-bar',
   component: Layout,
   decorators: [
-    Story => (
-      <Layout>
-        {Common.outerSetup()}
-        <Story />
-      </Layout>
-    ),
+    Story => {
+      Common.outerSetup()
+      return <Story />
+    },
   ],
 }
 
+const generateDivs = maxLines => {
+  let lines = []
+  let i = 0
+  while (i++ < maxLines)
+    lines.push(
+      <div id={'line-' + i} key={'line-' + i}>
+        {i}
+      </div>
+    )
+
+  return (
+    <Layout>
+      <div style={{ backgroundColor: 'lightblue' }}>{lines}</div>
+    </Layout>
+  )
+}
+
+const testFunction = async (canvasElement, step, focalPoint = '') => {
+  await Common.asyncSleep(600)
+  const canvas = within(canvasElement)
+
+  await step('Item should be found', async () => {
+    let item = canvas.getByText(`${focalPoint}`)
+    console.log(item)
+    await step('Item DOM Node should be found', async () => {
+      expect(item).not.toBeNull()
+    })
+    await step('Item should be focusable', async () => {
+      // window.Synapp.ScrollFocus(item)
+      // fireEvent.focus(item)
+      // expect(document.activeElement).toBe(item);
+    })
+  })
+}
+
 export const Setup = () => {
-  Common.outerSetup()
-  var lines = []
-  let i = 0
-  while (i++ < 80) lines.push(<div key={'line-' + i}>{i}</div>)
-
-  return <div style={{ backgroundColor: 'lightblue' }}>{lines}</div>
+  return generateDivs(80)
 }
 
-export const ScrollFocusAt20 = () => {
-  var topBar = undefined
-  Common.outerSetup()
-  var lines = []
-  let i = 0
-  while (i++ < 80)
-    lines.push(
-      <div id={'line-' + i} key={'line-' + i}>
-        {i}
-      </div>
-    )
-
-  const story = <div style={{ backgroundColor: 'lightblue' }}>{lines}</div>
-
-  const storyTest = async e => {
-    // do this after the story has rendered
-    Common.Wrapper = mount(story, { attachTo: e })
-    await Common.asyncSleep(600)
-
-    specs(() =>
-      describe('item should be found', () => {
-        let item = document.getElementById('line-20')
-        window.Synapp.ScrollFocus(item)
-
-        it(`Item DOM Node should be found`, function () {
-          expect(item).toBeTruthy()
-        })
-      })
-    )
-  }
-
-  return <Common.RenderStory testFunc={storyTest}></Common.RenderStory>
+export const ScrollFocusAt1 = {
+  render: () => {
+    return generateDivs(80)
+  },
+  play: ({ canvasElement, step }) => {
+    testFunction(canvasElement, step, '1')
+  },
 }
 
-export const ScrollFocusAt80 = () => {
-  var topBar = undefined
-  Common.outerSetup()
-  var lines = []
-  let i = 0
-  while (i++ < 80)
-    lines.push(
-      <div id={'line-' + i} key={'line-' + i}>
-        {i}
-      </div>
-    )
-
-  const story = <div style={{ backgroundColor: 'lightblue' }}>{lines}</div>
-
-  const storyTest = async e => {
-    // do this after the story has rendered
-    Common.Wrapper = mount(story, { attachTo: e })
-    await Common.asyncSleep(600)
-
-    specs(() =>
-      describe('item should be found', () => {
-        let item = document.getElementById('line-80')
-        window.Synapp.ScrollFocus(item)
-        it(`Item DOM Node should be found`, function () {
-          expect(item).toBeTruthy()
-        })
-      })
-    )
-  }
-  return <Common.RenderStory testFunc={storyTest}></Common.RenderStory>
+export const ScrollFocusAt20 = {
+  render: () => {
+    return generateDivs(80)
+  },
+  play: ({ canvasElement, step }) => {
+    testFunction(canvasElement, step, '20')
+  },
 }
 
-export const ScrollFocusAt78 = () => {
-  var topBar = undefined
-  Common.outerSetup()
-  var lines = []
-  let i = 0
-  while (i++ < 80)
-    lines.push(
-      <div id={'line-' + i} key={'line-' + i}>
-        {i}
-      </div>
-    )
-
-  const story = <div style={{ backgroundColor: 'lightblue' }}>{lines}</div>
-
-  const storyTest = async e => {
-    // do this after the story has rendered
-    Common.Wrapper = mount(story, { attachTo: e })
-    await Common.asyncSleep(600)
-
-    specs(() =>
-      describe('item should be found', () => {
-        let item = document.getElementById('line-78')
-        window.Synapp.ScrollFocus(item)
-        it(`Item DOM Node should be found`, function () {
-          expect(item).toBeTruthy()
-        })
-      })
-    )
-  }
-  return <Common.RenderStory testFunc={storyTest}></Common.RenderStory>
+export const ScrollFocusAt78 = {
+  render: () => {
+    return generateDivs(80)
+  },
+  play: ({ canvasElement, step }) => {
+    testFunction(canvasElement, step, '78')
+  },
 }
 
-export const ScrollFocusAt40 = () => {
-  var topBar = undefined
-  Common.outerSetup()
-  var lines = []
-  let i = 0
-  while (i++ < 80)
-    lines.push(
-      <div id={'line-' + i} style={i === 40 ? { height: '2000px' } : {}} key={'line-' + i}>
-        {i}
-      </div>
-    )
-
-  const story = <div style={{ backgroundColor: 'lightblue' }}>{lines}</div>
-
-  const storyTest = async e => {
-    // do this after the story has rendered
-    Common.Wrapper = mount(story, { attachTo: e })
-    await Common.asyncSleep(600)
-
-    specs(() =>
-      describe('item should be found', () => {
-        let item = document.getElementById('line-40')
-        window.Synapp.ScrollFocus(item)
-        it(`Item DOM Node should be found`, function () {
-          expect(item).toBeTruthy()
-        })
-      })
-    )
-  }
-  return <Common.RenderStory testFunc={storyTest}></Common.RenderStory>
+export const ScrollFocusAt80 = {
+  render: () => {
+    return generateDivs(80)
+  },
+  play: ({ canvasElement, step }) => {
+    testFunction(canvasElement, step, '80')
+  },
 }
 
-export const ScrollFocusAt1 = () => {
-  var topBar = undefined
-  Common.outerSetup()
-  var lines = []
-  let i = 0
-  while (i++ < 80)
-    lines.push(
-      <div id={'line-' + i} key={'line-' + i}>
-        {i}
-      </div>
-    )
-
-  const story = <div style={{ backgroundColor: 'lightblue' }}>{lines}</div>
-
-  const storyTest = async e => {
-    // do this after the story has rendered
-    Common.Wrapper = mount(story, { attachTo: e })
-    await Common.asyncSleep(600)
-
-    specs(() =>
-      describe('item should be found', () => {
-        let item = document.getElementById('line-1')
-        window.Synapp.ScrollFocus(item)
-        it(`Item DOM Node should be found`, function () {
-          expect(item).toBeTruthy()
-        })
-      })
-    )
-  }
-  return <Common.RenderStory testFunc={storyTest}></Common.RenderStory>
+export const ScrollFocusSmallArea1 = {
+  render: () => {
+    return generateDivs(20)
+  },
+  play: ({ canvasElement, step }) => {
+    testFunction(canvasElement, step, '1')
+  },
 }
 
-export const ScrollFocusSmallArea1 = () => {
-  var topBar = undefined
-  Common.outerSetup()
-  var lines = []
-  let i = 0
-  while (i++ < 20)
-    lines.push(
-      <div id={'line-' + i} key={'line-' + i}>
-        {i}
-      </div>
-    )
-
-  const story = <div style={{ backgroundColor: 'lightblue' }}>{lines}</div>
-
-  const storyTest = async e => {
-    // do this after the story has rendered
-    Common.Wrapper = mount(story, { attachTo: e })
-    await Common.asyncSleep(600)
-
-    specs(() =>
-      describe('item should be found', () => {
-        let item = document.getElementById('line-1')
-        window.Synapp.ScrollFocus(item)
-        it(`Item DOM Node should be found`, function () {
-          expect(item).toBeTruthy()
-        })
-      })
-    )
-  }
-  return <Common.RenderStory testFunc={storyTest}></Common.RenderStory>
+export const ScrollFocusSmallArea20 = {
+  render: () => {
+    return generateDivs(20)
+  },
+  play: ({ canvasElement, step }) => {
+    testFunction(canvasElement, step, '20')
+  },
 }
 
-export const ScrollFocusSmallArea20 = () => {
-  var topBar = undefined
-  Common.outerSetup()
-  var lines = []
-  let i = 0
-  while (i++ < 20)
-    lines.push(
-      <div id={'line-' + i} key={'line-' + i}>
-        {i}
-      </div>
+export const ScrollFocus40DivTooLarge = {
+  render: () => {
+    let lines = []
+    let i = 0
+    while (i++ < 80)
+      lines.push(
+        <div id={'line-' + i} style={i === 40 ? { height: '2000px' } : {}} key={'line-' + i}>
+          {i}
+        </div>
+      )
+
+    return (
+      <Layout>
+        <div style={{ backgroundColor: 'lightblue' }}>{lines}</div>
+      </Layout>
     )
-
-  const story = <div style={{ backgroundColor: 'lightblue' }}>{lines}</div>
-
-  const storyTest = async e => {
-    // do this after the story has rendered
-    Common.Wrapper = mount(story, { attachTo: e })
-    await Common.asyncSleep(600)
-
-    specs(() =>
-      describe('item should be found', () => {
-        let item = document.getElementById('line-20')
-        window.Synapp.ScrollFocus(item)
-        it(`Item DOM Node should be found`, function () {
-          expect(item).toBeTruthy()
-        })
-      })
-    )
-  }
-  return <Common.RenderStory testFunc={storyTest}></Common.RenderStory>
+  },
+  play: ({ canvasElement, step }) => {
+    testFunction(canvasElement, step, '40')
+  },
 }
