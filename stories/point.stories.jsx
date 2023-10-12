7018a7b2ace052ecc11044ac4848971c6a74db19
Julian Cardone <juliancardone1@gmail.com>
2023-10-12 00:15:05 -0400

    add hover  functionality for default state subject and description

--- stories/point.stories.jsx
+++ stories/point.stories.jsx
@@ -2,6 +2,17 @@ import Point from '../app/components/point'
 
 export default {
   component: Point,
+  args: {
+    subject: 'Phasellus diam sapien, placerat id sollicitudin eget',
+    description:
+      'Cras porttitor quam eros, vel auctor magna consequat vitae. Donec condimentum ac libero mollis tristique.',
+  },
 }
 
-export const Primary = { args: {} }
+export const Primary = { args: { vState: 'default' } }
+export const PrimaryMouseDown = { args: { vState: 'mouseDown' } }
+export const PrimaryDisabled = { args: { vState: 'disabled' } }
+
+// to do: implement test for clicking (mousedown)
+// group stories
+// better/more convetional names
