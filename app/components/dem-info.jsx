931767796d498444b65eba7ee3a4dbc5dae5292a
Edmund Ju <edmundju@berkeley.edu>
2023-10-20 21:37:07 -0700

    define user properties from schema

--- app/components/dem-info.jsx
+++ app/components/dem-info.jsx
@@ -1,12 +1,29 @@
 'use strict'
 import React from 'react';
+import insertSheet from 'react-jss';
 
 function DemInfo(props) {
-    const { user, className, styles, ...otherProps } = props
+    const { user, className, styles, ...otherProps } = props;
+
+    const userState = user.state || 'N/A';
+    const userAge = user.dob ? calculateAge() : 'N/A';
+    const userPoliticalParty = user.party || 'N/A';
+
 
     return (
         <span>
-            
+            {`${userPoliticalParty} | ${userAge}, ${userState}`}
         </span>
     )
 }
+
+function calculateAge() {
+
+}
+
+const demInfoStyles = {
+    
+}
+
+
+export default insertSheet(demInfoStyles)(DemInfo)
