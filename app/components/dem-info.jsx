c0db5d21e3930377df63811e682ad5e534aa1c7a
Edmund Ju <edmundju@berkeley.edu>
2023-10-20 21:12:28 -0700

    create demInfo component

---
+++ app/components/dem-info.jsx
@@ -0,0 +1,12 @@
+'use strict'
+import React from 'react';
+
+function DemInfo(props) {
+    const { user, className, styles, ...otherProps } = props
+
+    return (
+        <span>
+            
+        </span>
+    )
+}
