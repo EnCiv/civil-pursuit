4e9831638b93ebc9ce2438b2bdbc1a0d93a601a2
Edmund Ju <edmundju@berkeley.edu>
2023-10-21 21:54:39 -0700

    create dem-info story

---
+++ stories/dem-info.stories.jsx
@@ -0,0 +1,15 @@
+import DemInfo from '../app/components/dem-info';
+import React from 'react';
+
+export default {
+    component: DemInfo,
+    args: {}
+}
+
+export const Primary = { args: {
+    user: {
+        dob: new Date('1990-10-20'),
+        state: 'New York',
+        party: 'Independent'
+      }
+}}
