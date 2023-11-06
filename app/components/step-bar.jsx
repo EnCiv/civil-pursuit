dbfa5cdb706ef3b32f6f193aa63df7ea1a90d621
Julian Cardone <juliancardone1@gmail.com>
2023-11-06 18:39:26 -0500

    setup stepbar component and story

---
+++ app/components/step-bar.jsx
@@ -0,0 +1,7 @@
+'use strict'
+import React from 'react'
+import Theme from '../app/components/theme'
+
+function StepBar(props) {}
+
+export default StepBar
---
+++ stories/step-bar.stories.jsx
@@ -0,0 +1,6 @@
+import StepBar from "../app/components/step-bar";
+import React from 'react'
+
+export default {
+  component: StepBar,
+}
