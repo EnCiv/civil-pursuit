98b536d46ab833f681bd890b8726931863c05895
JojoLiu66 <jojoliu6697@gmail.com>
2023-11-14 14:14:42 -0800

    Button component non stretch goal version

--- app/components/button.jsx
+++ app/components/button.jsx
@@ -1,8 +1,9 @@
 import React, {useState, useRef} from 'react';
 import { createUseStyles }  from 'react-jss';
+import cx from 'classnames'
 
 /**
- * Button component that is styled using react-jss.
+ * Button component(without stretch goal version) that is styled using react-jss.
  * It supports various states like hover, active, and disabled, 
  * and can be configured with custom styles, titles, and callbacks.
  *
@@ -18,81 +19,36 @@ function Button(props) {
         title = "", // text to display on hover
         disabled = false, 
         disableOnClick = false, // if true, the button gets disabled after click and stays disabled - prevents resubmission
-        children
+        children,
+        ...otherProps
     } = props;
 
     const [isDisabled, setIsDisabled] = useState(disabled);
     const [parentIsHovered, setParentIsHovered] = useState(false);
-    // Stretch Goal 
-    // const [longPress, setLongPress] = useState(false);
-    // const [position, setPosition] = useState({ x: 0, y: 0 });
-    // const longPressTimer = useRef(null);
 
     const classes = buttonStyles();
-    const combinedClassName = `${classes[className]} ${className}`;
-
-    // Stretch Goal 
-    // const startLongPress = () => {
-    //     const x = event.clientX || (event.touches && event.touches[0].clientX);
-    //     const y = event.clientY || (event.touches && event.touches[0].clientY);
-        
-    //     setPosition({ x, y });
-    //     longPressTimer.current = setTimeout(() => {
-    //         setLongPress(true);
-    //     }, 1000);
-    // }
-
-    // const stopLongPress = (event) => {
-    //     clearTimeout(longPressTimer.current);
-    //     if (longPress) {
-    //         setLongPress(false);
-    //     }
-    // }
+    const combinedClassName = cx(classes.buttonBase, classes[className], className, { 'hover': parentIsHovered });
 
     return (
-        <div 
-            style={{ position: 'relative' }}
-            onMouseEnter={() => setParentIsHovered(true)}
-            onMouseLeave={() => setParentIsHovered(false)}
+        <button
+            className={combinedClassName}
+            title={title}
+            disabled={isDisabled}
+            onClick={() => {
+                if (onDone) onDone();
+                if (disableOnClick) setIsDisabled(true);
+            }}
+            {...otherProps}
         >
-            <button
-                className={`${combinedClassName} ${parentIsHovered ? 'hover' : ''}`}
-                style={style}
-                title={title}
-                disabled={isDisabled}
-                onClick={() => {
-                    if (onDone) onDone();
-                    if (disableOnClick) setIsDisabled(true);
-                }}
-
-                // onMouseDown={startLongPress}
-                // onMouseUp={stopLongPress}
-                // onMouseLeave={stopLongPress}
-                // onTouchStart={startLongPress}
-                // onTouchEnd={stopLongPress}
-            >
-                {children}
-            </button>
-            {/* Stretch Goal 
-            {longPress && 
-                <div style={{
-                    top: `${position.y}px`,
-                    left: `${position.x}px`,
-                    transform: 'translateX(-50%, -100%)',
-                    backgroundColor: 'white',
-                    border: '1px solid black',
-                    padding: '5px',
-                    borderRadius: '5px',
-                }}>
-                    {title}
-                </div>
-            } */}
-        </div>
+            {children}
+        </button>
     )
 }
 
 function ModifierButton(props) {
-    return <Button {...props} className="modifierButton" />;
+    const { className, ...otherProps } = props;
+    const classes = buttonStyles();
+    return <Button {...otherProps} className={cx(classes.modifierButton, className)} />;
 }
 
 function SecondaryButton(props) {
@@ -107,22 +63,22 @@ function TextButton(props) {
     return <Button {...props} className="textButton" />;
 }
 
-
-const commonButtonStyles = {
-    width: 'auto',
-    height: 'auto',
-    borderRadius: '0.5rem',
-    padding: '0.5rem 1.25rem',
-    fontFamily: 'Inter, sans-serif',
-    fontWeight: 600,
-    fontSize: '1rem',
-    lineHeight: '1.5rem',
-    textAlign: 'center',
-}
-
 const buttonStyles = createUseStyles(theme => ({
+    buttonBase: { // These are common styles
+        width: 'auto',
+        height: 'auto',
+        borderRadius: '0.5rem',
+        padding: '0.5rem 1.25rem',
+        fontFamily: 'Inter, sans-serif',
+        fontWeight: 600,
+        fontSize: '1rem',
+        lineHeight: '1.5rem',
+        textAlign: 'center',
+        // Add any other common styles here
+      },
+
     secondaryButton: {
-        ...commonButtonStyles,
+        extend: 'buttonBase',
         backgroundColor: theme.colors.white,
         color: theme.colors.primaryButtonBlue, 
         border: `0.125rem solid ${theme.colors.primaryButtonBlue}`,
@@ -153,7 +109,7 @@ const buttonStyles = createUseStyles(theme => ({
     },
 
     modifierButton: {
-        ...commonButtonStyles,
+        extend: 'buttonBase',
         backgroundColor: theme.colors.white,
         color: theme.colors.textBrown,
         border: `0.125rem solid ${theme.colors.encivYellow}`,
@@ -176,7 +132,7 @@ const buttonStyles = createUseStyles(theme => ({
     },
 
     primaryButton: {
-        ...commonButtonStyles,
+        extend: 'buttonBase',
         backgroundColor: theme.colors.primaryButtonBlue,
         color: theme.colors.white,
         border: `0.125rem solid ${theme.colors.primaryButtonBlue}`,
@@ -206,7 +162,7 @@ const buttonStyles = createUseStyles(theme => ({
     },
 
     textButton: {
-        ...commonButtonStyles,
+        extend: 'buttonBase',
         backgroundColor: 'transparent',
         color: theme.colors.title,
         border: 'none',
