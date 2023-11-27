794359dd358360ec66e52090c8c784bbfe4480a9
Kyle Slugg <kyle.slugg@gmail.com>
2023-11-27 10:46:19 -0500

    Copied working ranking files to clean branch

---
+++ app/components/util/ranking.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+
+export default function Ranking(rankingProps) {
+  const responseOptions = ['Most', 'Neutral', 'Least']
+  const displayPropOptions = ['block', 'small', 'medium', 'large']
+
+  //Define new and inherited classes
+  let classes = ['radial_ranking', ...(className ? className : [])]
+
+  //Isolate props and set initial state
+  const { disabled, defaultValue, className, onSelected, ...otherProps } = rankingProps
+  let [response, setResponse] = useState(responseOptions.includes(defaultValue) ? defaultValue : '')
+
+  displayPropOptions.forEach(prop => {
+    if (otherProps[prop]) {
+      classes.push(prop)
+      delete otherProps[prop]
+    }
+  })
+
+  const onSelectionChange = e => {
+    if (e.target.disabled) {
+      return
+    }
+    setResponse(e.target.value)
+    if (!onSelected) {
+      return console.warn(
+        `Unhandled rank selection: ${e.target.value}. Please pass a handler function via the onSelected prop.`
+      )
+    }
+
+    onSelected(e)
+  }
+
+  return (
+    <div data-value={response} className={classes.join(' ')} onChange={onSelectionChange} {...otherProps}>
+      {responseOptions.map(option => {
+        return (
+          <div>
+            <input
+              disabled={disabled || false}
+              checked={response === option}
+              type="radio"
+              id={`ranking${option}`}
+              value={option}
+              name="importance"
+            ></input>
+            <label htmlFor={`ranking${option}`}>{option}</label>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
