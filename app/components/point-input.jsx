9c145e2acc8c84b4d5a72275de9ffe92e23b6fea
Edmund Ju <edmundju@berkeley.edu>
2023-11-27 18:41:55 -0800

    add word and char counts for subject and description fields

--- app/components/point-input.jsx
+++ app/components/point-input.jsx
@@ -2,31 +2,44 @@
 import React, { useState } from 'react';
 import { createUseStyles } from 'react-jss';
 
+// 100 character limit for subject
+
 function PointInput(props) {
-    const { style, className, maxWordCount=30, defaultValue={subject: "", description: ""} } = props
+    const { style, className, maxWordCount=30, maxCharCount=100, defaultValue={subject: "", description: ""}, onDone=(valid, values={subject: "", description: ""})=>(valid, values)} = props
     const classes = useStyles()
     const [subject, setSubject] = useState(defaultValue.subject)
     const [description, setDescription] = useState(defaultValue.description)
-    const [wordCount, setWordCount] = useState(getWordCount(description))
+    const [descWordCount, setDescWordCount] = useState(getDescWordCount(description))
+    const [subjCharCount, setSubjCharCount] = useState(getSubjCharCount(subject))
 
-    function getWordCount(inputText) {
+    function getDescWordCount(inputText) {
         if(!inputText) return 0
         return inputText.trim().split(/\s+/).length;
     }
 
-    const isValidWordCount = inputText => {
-        const wordCount = getWordCount(inputText)
+    function getSubjCharCount(inputText) {
+        return inputText.length
+    }
+
+    const isDescValid = inputText => {
+        const wordCount = getDescWordCount(inputText)
         return wordCount > 0 && wordCount <= maxWordCount
+    }
+
+    const isSubjValid = inputText => {
+        const charCount = getSubjCharCount(inputText)
+        return charCount > 0 && charCount <= maxCharCount
     }
 
 
     const handleSubjectChange = (value) => {
         setSubject(value)
+        setSubjCharCount(getSubjCharCount(value))
     }
 
     const handleDescriptionChange = (value) => {
         setDescription(value)
-        setWordCount(getWordCount(value))
+        setDescWordCount(getDescWordCount(value))
     }
 
     return (
@@ -40,13 +53,22 @@ function PointInput(props) {
                 className={classes.subject}>
 
                 </input>
+                <span
+                    className={classes.wordCount}
+                >
+                {subjCharCount} / {maxCharCount}
+                </span>
+
                 <textarea
                     placeholder="Description"
                     value={description}
                     onChange={(e) => handleDescriptionChange(e.target.value)}
+                    onBlur={() => onDone((isSubjValid(subject) && isDescValid(description)), {subject: subject, description: description})}
                     className={classes.description}>
                 </textarea>
-                <span className={classes.wordCount}>{wordCount} / {maxWordCount}</span>
+                <span
+                    className={classes.wordCount}
+                >{descWordCount} / {maxWordCount}</span>
             </div>
         </>
     )
