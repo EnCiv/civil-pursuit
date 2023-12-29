7e8dcd5609308222a27e8c83b1a68bc5aa5c0295
Edmund Ju <edmundju@berkeley.edu>
2023-12-28 21:47:17 -0800

    add color styling and text mapping for status

--- app/components/status-badge.jsx
+++ app/components/status-badge.jsx
@@ -2,15 +2,52 @@
 
 'use strict'
 import React from 'react';
+import { createUseStyles } from 'react-jss';
+import cx from 'classnames';
 
 function StatusBadge(props) {
-    const { style={}, className="", name="", status="", number=undefined, ...otherProps} = props;
+    const { style={}, className="", name="", status="progress", number=undefined, ...otherProps} = props;
+    const classes = useStyles();
+
+    const statusMapping = {
+        progress: "Unread",
+        complete: "Complete",
+        inactive: "Group, Inactive",
+        error: "Most Important"
+    }
 
     return (
-        <span>
-            {status}
+        <span className={cx(classes.container, classes[status.toLowerCase()])}>
+            {statusMapping[status.toLowerCase()]}
         </span>
     )
 }
+
+const useStyles = createUseStyles(theme => ({
+
+    container: {
+        borderRadius: '15px',
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        padding: '0.375rem 0.625rem',
+        fontFamily: theme.font.fontFamily,
+    },
+    progress: {
+        borderColor: theme.colors.statusBadgeProgressBorder,
+        backgroundColor: theme.colors.statusBadgeProgressBackground,
+    },
+    complete: {
+        borderColor: theme.colors.statusBadgeCompletedBorder,
+        backgroundColor: theme.colors.statusBadgeCompletedBackground,
+    },
+    inactive: {
+        borderColor: theme.colors.statusBadgeInactiveBorder,
+        backgroundColor: theme.colors.statusBadgeInactiveBackground,
+    },
+    error: {
+        borderColor: theme.colors.statusBadgeErrorBorder,
+        backgroundColor: theme.colors.statusBadgeErrorBackground,
+    }
+}))
 
 export default StatusBadge;
