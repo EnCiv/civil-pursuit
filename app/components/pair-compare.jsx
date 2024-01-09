9204f3fec294ad54bd02091f0de9f01416b3eb16
Edmund Ju <edmundju@berkeley.edu>
2024-01-09 12:13:16 -0800

    add props and mainPoint elements

--- app/components/pair-compare.jsx
+++ app/components/pair-compare.jsx
@@ -1,17 +1,44 @@
 // https://github.com/EnCiv/civil-pursuit/issues/53
 
 'use strict'
-import React from 'react';
+import React, { useState } from 'react';
 import { createUseStyles } from 'react-jss';
 import cx from 'classnames';
 
 function PairCompare(props) {
+    const { pointList=[], onDone=()=>{}, mainPoint={subject: "", description: ""}, ...otherProps} = props
+    const [currIdx, setCurrIdx] = useState(0);
+    const classes = useStyles();
 
     return (
-        <span>
-
-        </span>
+        <div className={classes.container} {...otherProps}>
+            <div className={classes.mainPointContainer}>
+                <div className={classes.mainPointSubject}>{mainPoint.subject}</div>
+                <div className={classes.mainPointDescription}>{mainPoint.description}</div>
+            </div>
+            <span className={classes.statusBadge}>{`${currIdx} out of ${pointList.length}`}</span>
+        </div>
     )
 }
+
+const useStyles = createUseStyles(theme => ({
+
+    container: {
+
+    },
+    statusBadge: {
+
+    },
+    mainPointContainer: {
+
+    },
+    mainPointSubject: {
+
+    },
+    mainPointDescription: {
+
+    }
+
+}))
 
 export default PairCompare;
