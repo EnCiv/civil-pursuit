8c81b769fb4f036f7f48154d422c4af8afaedd78
zilinfang <zilinfg@gmail.com>
2024-02-14 18:40:35 -0800

    Created ranking-result components using recharts library, created ranking-result stories

---
+++ app/components/ranking-result.jsx
@@ -0,0 +1,53 @@
+// https://github.com/EnCiv/civil-pursuit/issue/81
+import React from 'react'
+import { createUseStyles } from 'react-jss'
+import cx from 'classnames'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Cell, ResponsiveContainer } from 'recharts'
+
+export default function RankingResult(props) {
+  const { resultList, className, ...otherProps } = props
+  const classes = useStylesFromThemeFunction(props)
+
+  // Check if resultList is empty and set default data if so
+  const isEmpty = Object.keys(resultList).length === 0
+  const defaultData = [
+    { name: 'Most', value: 0 },
+    { name: 'Neutral', value: 0 },
+    { name: 'Least', value: 0 },
+  ]
+
+  const dataArray = isEmpty
+    ? defaultData
+    : Object.keys(resultList).map(key => ({
+        name: key, // This will be "Most", "Neutral", or "Least"
+        value: resultList[key], // This is the corresponding number value
+      }))
+
+  return (
+    <div className={cx(classes.wrapper, className)} {...props}>
+      <ResponsiveContainer width="100%" height={250}>
+        <BarChart data={dataArray} width="100%" height="100%" layout="vertical">
+          <CartesianGrid strokeDasharray="2 3" />
+          <XAxis type="number" />
+          <YAxis type="category" dataKey="name" className={cx(classes.yAxisFont)} />
+          <Tooltip />
+          <Bar dataKey="value" barSize={30}>
+            {dataArray.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill="#038a47" />
+            ))}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
+
+const useStylesFromThemeFunction = createUseStyles(theme => ({
+  wrapper: {
+    width: '100%',
+    background: theme.colorPrimary,
+  },
+  yAxisFont: {
+    fontWeight: 'bold',
+  },
+}))
