b5c4bfe76bd7f2c46a7fcd25cd55e8e6f8727067
zilinfang <zilinfg@gmail.com>
2024-02-20 13:35:55 -0800

    added useEffect to initalize onDone and groupedPointList on mount

--- app/components/grouping-step.jsx
+++ app/components/grouping-step.jsx
@@ -1,11 +1,20 @@
 // https://github.com/EnCiv/civil-pursuit/issue/49
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createUseStyles } from 'react-jss'
 import cx from 'classnames'
 
 export default function GroupingStep(props) {
-  const { className, ...otherProps } = props
+  const { onDone, shared, className, ...otherProps } = props
   const classes = useStylesFromThemeFunction(props)
+  const { pointList, groupedPointList } = shared
+
+  // Initialize groupedPointList if it doesn't exist and manage the ready state
+  useEffect(() => {
+    if (!groupedPointList) {
+      groupedPointList = JSON.parse(JSON.stringify(pointList)) // Deep clone pointList to groupedPointList
+    }
+    onDone({ valid: true, value: groupedPointList }) // Call onDone with true initially
+  }, [shared, onDone])
 
   return (
     <div className={cx(classes.wrapper, className)} {...props}>
