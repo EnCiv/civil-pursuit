78d4b4713666e8621c605e017ee642595777c4f9
Edmund Ju <edmundju@berkeley.edu>
2024-02-27 21:27:34 -0800

    onDone successful case story passes using setTimeout

--- stories/pair-compare.stories.jsx
+++ stories/pair-compare.stories.jsx
@@ -1,18 +1,22 @@
 import PairCompare from "../app/components/pair-compare";
 import Point from "../app/components/point";
+import { onDoneDecorator, onDoneResult } from "./common";
+import { within } from '@storybook/testing-library';
+import expect from 'expect';
 import React from 'react';
 
 export default {
     component: PairCompare,
-    args: {}
+    args: {},
+    decorators: [onDoneDecorator],
 }
 
-const pointOne = <Point subject="Point 1" description="This is the first point"/>;
-const pointTwo = <Point subject="Point 2" description="This is the second point"/>;
-const pointThree = <Point subject="Point 3" description="This is the third point"/>;
-const pointFour = <Point subject="Point 4" description="This is the fourth point"/>;
-const pointFive = <Point subject="Point 5" description="This is the fifth point"/>;
-const pointSix = <Point subject="Point 6" description="This is the sixth point"/>;
+const pointOne = <Point subject="Point 1" description="This is the first point" />;
+const pointTwo = <Point subject="Point 2" description="This is the second point" />;
+const pointThree = <Point subject="Point 3" description="This is the third point" />;
+const pointFour = <Point subject="Point 4" description="This is the fourth point" />;
+const pointFive = <Point subject="Point 5" description="This is the fifth point" />;
+const pointSix = <Point subject="Point 6" description="This is the sixth point" />;
 
 
 
@@ -24,5 +28,48 @@ export const sixPoints = {
             description: "Climate change and global warning"
         },
         pointList: [pointOne, pointTwo, pointThree, pointFour, pointFive, pointSix]
+    }
+}
+
+export const onDoneTest = {
+    args: {
+        mainPoint: {
+            subject: "Global Warming",
+            description: "Climate change and global warning"
+        },
+        pointList: [pointOne, pointTwo, pointThree, pointFour]
+    },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement)
+        const Point1 = canvas.getByText("Point 1");
+        Point1.click();
+
+        setTimeout(() => { // wait for transition to occur
+            const Point3 = canvas.getByText("Point 3");
+            Point3.click();
+        }, 500)
+
+        setTimeout(() => {
+            const Point4 = canvas.getByText("Point 4");
+            Point4.click();
+        }, 1300);
+
+        setTimeout(() => {
+            console.log(onDoneResult(canvas))
+            expect(onDoneResult(canvas)).toMatchObject({
+                count: 2,
+                onDoneResult: {
+                    valid: true,
+                    value: {
+                        props: {
+                            description: "This is the fourth point",
+                            subject: "Point 4"
+                        }
+                    }
+                }
+            })
+
+        }, 1500)
+
     }
 }
