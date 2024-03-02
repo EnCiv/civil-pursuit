744fa82c775ee493e1ac01f67cfde6d518e17a07
Anh Le <anh4dle@gmail.com>
2024-03-02 14:27:27 -0500

    pass the props, conditional rendering

--- app/components/step-footer.jsx
+++ app/components/step-footer.jsx
@@ -1,23 +1,38 @@
 import React from 'react'
 import { createUseStyles } from 'react-jss'
 import { Button, TextButton } from './button.jsx'
+import cx from 'classnames'
+
 // Create your Styles. Remember, since React-JSS uses the default preset,
 // most plugins are available without further configuration needed.
 
-function StepFooter() {
-  const classes = useStyles()
+function StepFooter(props) {
+  const classes = useStylesFromThemeFunction()
+  const {
+    className = '', // may or may not be passed. Should be applied to the outer most tag, after local classNames
+    onDone = () => {}, // a function that is called when the button is clicked.  - if it exists
+    onBack = () => {},
+    disabled = false,
+    disableOnClick = false, // if true, the button gets disabled after click and stays disabled - prevents resubmission
+    children,
+    ...otherProps
+  } = props
 
-  return (
-    <div className={classes.footerContainer}>
-      <hr className={classes.line}></hr>
-      <div className={classes.footerDiv}>
-        <TextButton className={classes.back}>&lt; Back</TextButton>
-        <Button className={classes.button}>Next</Button>
+  return onDone() === undefined || onBack() === undefined ? (
+    <div></div>
+  ) : (
+    <div className={cx(classes.sharedBorderStyle, className)} {...otherProps}>
+      <div className={classes.footerContainer}>
+        <hr className={classes.line}></hr>
+        <div className={classes.footerDiv}>
+          <TextButton className={classes.back}>&lt; Back</TextButton>
+          <Button className={classes.button}>Next</Button>
+        </div>
       </div>
     </div>
   )
 }
-const useStyles = createUseStyles(theme => ({
+const useStylesFromThemeFunction = createUseStyles(theme => ({
   footerContainer: {
     [`@media (max-width: ${theme.condensedWidthBreakPoint})`]: {
       width: '4rem',
