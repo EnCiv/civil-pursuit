75b5e46d336e0ce954bd7a3b547af6b34fbba5b2
Edmund Ju <edmundju@berkeley.edu>
2024-03-11 23:36:31 -0700

    pass onDone into pointInput component

--- app/components/why-input.jsx
+++ app/components/why-input.jsx
@@ -1,21 +1,24 @@
 // https://github.com/EnCiv/civil-pursuit/issues/68
 
 'use strict'
-import React from 'react';
+import React, { useEffect } from 'react';
 import Point from './point';
 import PointInput from './point-input';
 import { createUseStyles } from 'react-jss';
 
 
 function WhyInput(props) {
-
     const { point = { subject: "", description: "" }, defaultValue = { subject: "", description: "" }, onDone = () => { } } = props;
     const classes = useStyles();
+
+    const handleOnDone = ({ valid, value }) => {
+        onDone({ valid, value })
+    }
 
     return (
         <div className={classes.container}>
             <Point {...point} />
-            <PointInput />
+            <PointInput onDone={handleOnDone} />
         </div>
     )
 
--- stories/why-input.stories.jsx
+++ stories/why-input.stories.jsx
@@ -1,11 +1,22 @@
 import WhyInput from "../app/components/why-input";
+import { onDoneDecorator } from "./common";
 
 export default {
     component: WhyInput,
-    args: {}
+    args: {},
+    decorators: [onDoneDecorator],
 }
 
 export const ExamplePoint = {
+    args: {
+        point: {
+            subject: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at bibendum sapien",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at bibendum sapien"
+        }
+    }
+}
+
+export const onDoneTest = {
     args: {
         point: {
             subject: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at bibendum sapien",
