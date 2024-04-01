85becb736606ece340c4c19e7aa62f0953667039
Dongze Li <dzli@Dongzes-MacBook-Pro.local>
2024-04-01 00:32:23 -0700

    Utilized the existing Point component within ShowDualPointList for rendering individual points in each list

--- app/components/show-dual-point-list.jsx
+++ app/components/show-dual-point-list.jsx
@@ -1,12 +1,118 @@
 // https://github.com/EnCiv/civil-pursuit/issues/57
 
-import React from 'react'
+'use strict'
+import React, { useState } from 'react'
 import { createUseStyles } from 'react-jss'
 import cx from 'classnames'
-import { Button } from './button'
+import Point from './point'
+import { ModifierButton } from './button.jsx'
 
-function ShowDualPointList(props) {
-  return <div></div>
+export default function ShowDualPointList({
+  className,
+  leftPoints,
+  leftHeader,
+  rightPoints,
+  rightHeader,
+  vState,
+  ...otherProps
+}) {
+  const classes = useStylesFromThemeFunction()
+  const [isExpanded, setIsExpanded] = useState(vState === 'expanded')
+  const toggleExpandCollapse = () => setIsExpanded(!isExpanded)
+
+  return (
+    <div className={cx(classes.sharedBorderStyle, className)} {...otherProps}>
+      <div className={classes.contentContainer}>
+        <div className={cx(classes.column, classes.leftColumn)}>
+          <div className={cx(classes.header, classes.leftHeader)}>{leftHeader}</div>
+
+          {leftPoints.map((point, index) => (
+            <Point
+              key={index}
+              {...point}
+              description={isExpanded ? point.description : ''}
+              vState={isExpanded ? 'expanded' : 'collapsed'}
+              className={cx(classes.point, index % 2 === 0 ? classes.evenRow : classes.oddRow)}
+            />
+          ))}
+        </div>
+
+        <div className={cx(classes.column, classes.rightColumn)}>
+          <div className={cx(classes.header, classes.rightHeader)}>{rightHeader}</div>
+
+          {rightPoints.map((point, index) => (
+            <Point
+              key={index}
+              {...point}
+              description={isExpanded ? point.description : ''}
+              vState={isExpanded ? 'expanded' : 'collapsed'}
+              className={cx(classes.point, index % 2 === 0 ? classes.evenRow : classes.oddRow)}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className={classes.buttonContainer}>
+        <ModifierButton onDone={toggleExpandCollapse}> {isExpanded ? 'Collapse Chart' : 'Expand Chart'}</ModifierButton>
+      </div>
+    </div>
+  )
 }
 
-export default ShowDualPointList
+const useStylesFromThemeFunction = createUseStyles(theme => ({
+  sharedBorderStyle: {
+    borderRadius: '0.9375rem',
+
+    border: '1px solid rgba(217, 217, 217, 0.40)',
+  },
+
+  contentContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    position: 'relative',
+    borderTopLeftRadius: '0.9375rem',
+    borderTopRightRadius: '0.9375rem',
+    overflow: 'hidden',
+  },
+
+  column: {
+    flex: '1 1 50%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+
+  leftColumn: {
+    marginRight: '0.3rem',
+    backgroundColor: theme.colors.lightSuccess,
+  },
+
+  rightColumn: {
+    backgroundColor: theme.colors.statusBadgeProgressBackground,
+  },
+
+  header: {
+    fontSize: '1rem',
+    marginTop: '1rem',
+    marginBottom: '1rem',
+    marginLeft: '1rem',
+  },
+
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '1rem',
+    marginBottom: '1rem',
+  },
+
+  oddRow: {
+    backgroundColor: `#EFEFEF`,
+  },
+
+  evenRow: {
+    backgroundColor: `#F8F8F8`,
+  },
+
+  point: {
+    flex: 1,
+  },
+}))
--- stories/show-dual-point-list.stories.jsx
+++ stories/show-dual-point-list.stories.jsx
@@ -1,6 +1,131 @@
+// https://github.com/EnCiv/civil-pursuit/issues/57
+
+import React from 'react'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import ShowDualPointList from '../app/components/show-dual-point-list'
 
 export default {
   component: ShowDualPointList,
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+    },
+  },
+}
+
+// Case when props are undefined:
+export const UndefinedProps = {
   args: {},
+}
+
+// Case when lists are empty:
+export const ListsEmpty = {
+  args: {
+    leftHeader: "Why It's most Important",
+    leftPoints: [],
+    rightHeader: "Why It's least Important",
+    rightPoints: [],
+  },
+}
+
+export const MobileListsEmpty = {
+  args: {
+    leftHeader: "Why It's most Important",
+    leftPoints: [],
+    rightHeader: "Why It's least Important",
+    rightPoints: [],
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: 'iphonex',
+    },
+  },
+}
+
+const point1 = {
+  subject: 'Equality is a human right',
+  description:
+    'Suspendisse eget tortor sit amet sapien facilisis dictum sed et nisl. Nam pellentesque dapibus sem id ullamcorper.',
+}
+
+const point2 = {
+  subject: 'Income equality reduction',
+  description:
+    'Proin nec metus facilisis, dignissim erat a, scelerisque leo. Quisque a posuere arcu, sed luctus mi. Sed fermentum vel ante eget consequat.',
+}
+
+const point3 = {
+  subject: 'Separation of wealth',
+  description: 'Fusce nibh quam, sollicitudin ut sodales id, molestie sit amet mi. Nunc nec augue odio.',
+}
+
+const point4 = {
+  subject: 'Not a crucial issue',
+  description: 'Fusce nibh quam, sollicitudin ut sodales id, molestie sit amet mi. Nunc nec augue odio.',
+}
+
+const point5 = {
+  subject: 'Poverty',
+  description:
+    'Suspendisse eget tortor sit amet sapien facilisis dictum sed et nisl. Nam pellentesque dapibus sem id ullamcorper.',
+}
+
+const point6 = {
+  subject: 'Poverty increasing with time',
+  description:
+    'Proin nec metus facilisis, dignissim erat a, scelerisque leo. Quisque a posuere arcu, sed luctus mi. Sed fermentum vel ante eget consequat.',
+}
+
+// Case when the chart is collapsed:
+export const CollapsedChart = {
+  args: {
+    leftHeader: "Why It's most Important",
+    leftPoints: [point1, point2, point3],
+    rightHeader: "Why It's least Important",
+    rightPoints: [point4, point5, point6],
+    vState: 'collapsed',
+  },
+}
+
+export const MobileCollapsedChart = {
+  args: {
+    leftHeader: "Why It's most Important",
+    leftPoints: [point1, point2, point3],
+    rightHeader: "Why It's least Important",
+    rightPoints: [point4, point5, point6],
+    vState: 'collapsed',
+  },
+
+  parameters: {
+    viewport: {
+      defaultViewport: 'iphonex',
+    },
+  },
+}
+
+// Case when the chart is expanded:
+export const ExpandedChart = {
+  args: {
+    leftHeader: "Why It's most Important",
+    leftPoints: [point1, point2, point3],
+    rightHeader: "Why It's least Important",
+    rightPoints: [point4, point5, point6],
+    vState: 'expanded',
+  },
+}
+
+export const MobileExpandedChart = {
+  args: {
+    leftHeader: "Why It's most Important",
+    leftPoints: [point1, point2, point3],
+    rightHeader: "Why It's least Important",
+    rightPoints: [point4, point5, point6],
+    vState: 'expanded',
+  },
+
+  parameters: {
+    viewport: {
+      defaultViewport: 'iphonex',
+    },
+  },
 }
