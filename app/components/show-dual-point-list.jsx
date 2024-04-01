1d3719d430c5c3f144863886051dfc183a2d08b6
Dongze Li <li.dgze@gmail.com>
2024-04-01 01:23:13 -0700

    Update layout using CSS grid in ShowDualPointList

--- app/components/show-dual-point-list.jsx
+++ app/components/show-dual-point-list.jsx
@@ -23,37 +23,31 @@ export default function ShowDualPointList({
   return (
     <div className={cx(classes.sharedBorderStyle, className)} {...otherProps}>
       <div className={classes.contentContainer}>
-        <div className={cx(classes.column, classes.leftColumn)}>
-          <div className={cx(classes.header, classes.leftHeader)}>{leftHeader}</div>
-
-          {leftPoints.map((point, index) => (
-            <Point
-              key={index}
-              {...point}
-              description={isExpanded ? point.description : ''}
-              vState={isExpanded ? 'expanded' : 'collapsed'}
-              className={cx(classes.point, index % 2 === 0 ? classes.evenRow : classes.oddRow)}
-            />
-          ))}
+        <div className={classes.header}>
+          <div className={classes.leftHeader}>{leftHeader}</div>
+          <div className={classes.rightHeader}>{rightHeader}</div>
         </div>
-
-        <div className={cx(classes.column, classes.rightColumn)}>
-          <div className={cx(classes.header, classes.rightHeader)}>{rightHeader}</div>
-
-          {rightPoints.map((point, index) => (
-            <Point
-              key={index}
-              {...point}
-              description={isExpanded ? point.description : ''}
-              vState={isExpanded ? 'expanded' : 'collapsed'}
-              className={cx(classes.point, index % 2 === 0 ? classes.evenRow : classes.oddRow)}
-            />
+        <div className={classes.pointGrid}>
+          {leftPoints.map((point, index) => (
+            <React.Fragment key={index}>
+              <Point
+                {...point}
+                description={isExpanded ? point.description : ''}
+                vState={isExpanded ? 'expanded' : 'collapsed'}
+                className={cx(classes.point, index % 2 === 0 ? classes.evenRow : classes.oddRow)}
+              />
+              <Point
+                {...(rightPoints[index] || {})}
+                description={isExpanded ? (rightPoints[index] || {}).description : ''}
+                vState={isExpanded ? 'expanded' : 'collapsed'}
+                className={cx(classes.point, index % 2 === 0 ? classes.evenRow : classes.oddRow)}
+              />
+            </React.Fragment>
           ))}
         </div>
       </div>
-
       <div className={classes.buttonContainer}>
-        <ModifierButton onDone={toggleExpandCollapse}> {isExpanded ? 'Collapse Chart' : 'Expand Chart'}</ModifierButton>
+        <ModifierButton onDone={toggleExpandCollapse}>{isExpanded ? 'Collapse Chart' : 'Expand Chart'}</ModifierButton>
       </div>
     </div>
   )
@@ -62,57 +56,51 @@ export default function ShowDualPointList({
 const useStylesFromThemeFunction = createUseStyles(theme => ({
   sharedBorderStyle: {
     borderRadius: '0.9375rem',
-
     border: '1px solid rgba(217, 217, 217, 0.40)',
   },
-
   contentContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
     position: 'relative',
     borderTopLeftRadius: '0.9375rem',
     borderTopRightRadius: '0.9375rem',
     overflow: 'hidden',
   },
-
-  column: {
-    flex: '1 1 50%',
-    display: 'flex',
-    flexDirection: 'column',
+  header: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    borderBottom: '1px solid rgba(217, 217, 217, 0.40)',
+    columnGap: '0.3rem',
   },
-
-  leftColumn: {
-    marginRight: '0.3rem',
+  leftHeader: {
+    padding: '1rem',
+    paddingLeft: '1.5rem',
+    fontSize: '1.25rem',
     backgroundColor: theme.colors.lightSuccess,
   },
-
-  rightColumn: {
+  rightHeader: {
+    padding: '1rem',
+    paddingLeft: '1.5rem',
+    fontSize: '1.25rem',
     backgroundColor: theme.colors.statusBadgeProgressBackground,
   },
-
-  header: {
-    fontSize: '1rem',
-    marginTop: '1rem',
-    marginBottom: '1rem',
-    marginLeft: '1rem',
+  pointGrid: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(2, 1fr)',
+    gridRowGap: '0',
+    columnGap: '0.3rem',
   },
-
+  point: {
+    flex: 1,
+  },
+  oddRow: {
+    backgroundColor: `#EFEFEF`,
+  },
+  evenRow: {
+    backgroundColor: `#F8F8F8`,
+  },
   buttonContainer: {
     display: 'flex',
     justifyContent: 'center',
     marginTop: '1rem',
     marginBottom: '1rem',
-  },
-
-  oddRow: {
-    backgroundColor: `#EFEFEF`,
-  },
-
-  evenRow: {
-    backgroundColor: `#F8F8F8`,
-  },
-
-  point: {
-    flex: 1,
   },
 }))
