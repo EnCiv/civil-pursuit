8d24b6ff7feac05d95441fa7905dd328f74d41bf
Andy <awilliamscomposing@gmail.com>
2024-05-14 13:42:36 -0700

    Reworking JsonForm

--- app/components/more-details.jsx
+++ app/components/more-details.jsx
@@ -3,15 +3,47 @@
 'use strict'
 import React, { useState } from 'react'
 import Column from './util/column'
-import Select from './util/select'
 import Submit from './util/submit'
 import cx from 'classnames'
 import { createUseStyles } from 'react-jss'
 import { JsonForms } from '@jsonforms/react'
 import { vanillaCells, vanillaRenderers } from '@jsonforms/vanilla-renderers'
 
-const schema = {}
-const uischema = {}
+const schema = {
+  type: 'object',
+  properties: {
+    householdIncome: {
+      type: 'string',
+      enum: ['Option 1', 'Option 2', 'Option 3'],
+    },
+    housing: {
+      type: 'string',
+      enum: ['Option 1', 'Option 2', 'Option 3'],
+    },
+    numberOfSiblings: {
+      type: 'string',
+      enum: ['Option 1', 'Option 2', 'Option 3'],
+    },
+  },
+}
+const uischema = {
+  type: 'VerticalLayout',
+  elements: [
+    {
+      type: 'Control',
+      scope: '#/properties/householdIncome',
+    },
+
+    {
+      type: 'Control',
+      scope: '#/properties/housing',
+    },
+    {
+      type: 'Control',
+      scope: '#/properties/numberOfSiblings',
+    },
+  ],
+}
 
 const MoreDetails = props => {
   const {
@@ -19,7 +51,6 @@ const MoreDetails = props => {
     defaultValue = { subject: '', description: '' },
     onDone = () => {},
     className,
-    ...otherProps
   } = props
   const [data, setData] = useState(initialData)
   const classes = useStyles()
@@ -31,51 +62,28 @@ const MoreDetails = props => {
   }
 
   return (
-    <div className={cx(classes.container, className)} {...otherProps}>
-      <h2>We just need a few more details about you to get started.</h2>
-      <JsonForms
-        schema={schema}
-        uischema={uischema}
-        data={data}
-        renderers={vanillaRenderers}
-        // cells={vanillaCells}
-        // onChange={({ data, _errors }) => setData(data)}
-      />
-      <Column>
-        <div>Household Income</div>
-        <Select
-          block
-          medium
-          // ref=""
-          // defaultValue={}
-          // onChange={}
-        >
-          <option value="">Select household range</option>
-        </Select>
-        <div>Housing</div>
-        <Select
-          block
-          medium
-          // ref=""
-          // defaultValue={}
-          // onChange={}
-        >
-          <option value="">Select housing type</option>
-        </Select>
-        <div>Number of Siblings</div>
-        <Select
-          block
-          medium
-          // ref=""
-          // defaultValue={}
-          // onChange={}
-        >
-          <option value="">Choose one</option>
-        </Select>
-        <Submit medium success radius>
-          Submit
-        </Submit>
-      </Column>
+    <div
+      className={cx(classes.container, className)}
+    >
+      <h3>We just need a few more details about you to get started.</h3>
+
+      <div className={classes.formContainer}>
+        <Column>
+          <JsonForms
+            schema={schema}
+            uischema={uischema}
+            data={data}
+            renderers={vanillaRenderers}
+            cells={vanillaCells}
+            onChange={({ data, _errors }) => setData(data)}
+          />
+          <Submit
+            className={classes.submitButton}
+          >
+            Submit
+          </Submit>
+        </Column>
+      </div>
     </div>
   )
 }
@@ -85,7 +93,13 @@ const useStyles = createUseStyles(theme => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    fontFamily: 'Inter',
+    border: 'medium dashed green',
   },
+  formContainer: { margin: '1.25rem', border: 'small solid red' },
+  selectList: {},
+  selectTitle: { fontWeight: 'bold' },
+  submitButton: { backgroundColor: '#06335C', color: '#fff', borderRadius: '10px', width: '100%' },
 }))
 
 export default MoreDetails
