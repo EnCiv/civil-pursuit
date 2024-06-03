2f8c041b35acf873b3203f7f94d4bfdd50dd1431
Chenxuan Liu <chenxuanliu@ChenxuandeMacBook-Pro.local>
2024-06-03 14:16:11 -0500

    installing jest and testing for upsert point api

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    setupFilesAfterEnv: ['<rootDir>/setupTests.js', '<rootDir>/node_modules/jest-enzyme/lib/index.js'],
+    preset: '@shelf/jest-mongodb',
+    watchPathIgnorePatterns: ['<rootDir>/tmp/', '<rootDir>/node_modules/'],
+    roots: ['app'],
+}
