877c4cad7cdb1d8de9429e120724694f24253813
David Fridley <ddfridley@yahoo.com>
2024-06-06 11:35:19 -0700

    uses onDone, className, no margin

--- stories/how-do-you-feel.stories.jsx
+++ stories/how-do-you-feel.stories.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import HowDoYouFeel from '../app/components/util/how-do-you-feel'
+import HowDoYouFeel from '../app/components/how-do-you-feel'
 import common, { onDoneDecorator, onDoneResult } from './common'
 
 export default {
-    component: HowDoYouFeel,
-    parameters: { layout: 'centered' },
-    decorators: [onDoneDecorator],
+  component: HowDoYouFeel,
+  parameters: { layout: 'centered' },
+  decorators: [onDoneDecorator],
 }
 
 export const Empty = {
-    args: {
-        title: 'Press me',
-        disabled: false,
-    },
+  args: {
+    title: 'Press me',
+    disabled: false,
+  },
 }
