dd3bced436edf08aeacb75be72f505e278849425
Chenxuan Liu <chenxuanliu@ChenxuandeMacBook-Pro.local>
2024-06-17 05:28:45 -0500

    change the name of the setupTests.js to jest-test-setup.js

--- jest-db.config.js
+++ jest-db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     testTimeout: 5000000,
-    setupFilesAfterEnv: ['<rootDir>/setupTests.js', '<rootDir>/node_modules/jest-enzyme/lib/index.js'],
+    setupFilesAfterEnv: ['<rootDir>/jest-test-setup.js', '<rootDir>/node_modules/jest-enzyme/lib/index.js'],
     preset: '@shelf/jest-mongodb',
 }
