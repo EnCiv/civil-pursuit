f289ef974ebdd9ba1050138383b7ada87d74c208
MingyiXu11 <XMY1005594341@126.com>
2024-06-23 12:10:57 -0500

    issue 100 update

--- app/components/question-box.jsx
+++ app/components/question-box.jsx
@@ -1,98 +1,89 @@
 // https://github.com/EnCiv/civil-pursuit/issues/100
 
-import React from 'react';
-import StatusBadge from './status-badge';
+import React from 'react'
+import { createUseStyles } from 'react-jss'
+import StatusBadge from './status-badge'
+import Markdown from 'markdown-to-jsx'
 
-const QuestionBox = ({ subject, description, participants }) => {
-  const styles = {
-    container: {
-        width: '1129px',
-        height: '610px',
-        borderRadius: '30px',
-        border: '1px solid #D9D9D9',
-        backgroundColor: 'rgba(235, 235, 235, 0.4)',
-        margin: 'auto',
-        position: 'relative',
-        boxSizing: 'border-box',
-    },
-    topic: {
-        width: '812px',
-        height: '396px',
-        textAlign: 'center',
-        position: 'absolute',
-        top: '58px',
-        left: '158px',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        gap: '24px',
 
-    },
-
-    fixedText: {
-        width: '101px',
-        height: '25px',
-        fontFamily: 'Inter',
-        fontWeight: 600,
-        fontSize: '17px',
-        lineHeight: '25px',
-        letterSpacing: '0.01em',
-        color: '#06335C',
-        textAlign: 'center',
-        whiteSpace: 'nowrap',
-    },
-    subject: {
-        width: '812px',
-        height: '264px',
-        fontFamily: 'Inter',
-        fontWeight: 700,
-        fontSize: '60px',
-        lineHeight: '66px',
-        letterSpacing: '-0.03em',
-        color: '#06335C',
-        margin: '0 auto',
-        textAlign: 'center'
-      },
-    description: {
-        width: '605px',
-        height: '59px',
-        fontFamily: 'Inter',
-        fontWeight: 400,
-        fontSize: '16px',
-        lineHeight: '24px',
-        color: '#06335C',
-        margin: '0 auto',
-        textAlign: 'center',
-    },
-    underlinedText: {
-        textDecoration: 'underline'
-    },
-    participantsContainer: {
-      position: 'absolute',
-      top: '487px',
-      left: '498px',
-      display: 'flex',
-      alignItems: 'center',
-      gap: '0.5rem',
-    }
-  };
+const QuestionBox = (props) => {
+  const { subject, description, participants } = props;
+  const classes = useStyles();
   const badgeName = `${participants} participants`;
+
   return (
-    <div style={styles.container}>
-      <div style={styles.topic}>
-        <div style={styles.fixedText}>Civil Pursuit</div>
-        <div style={styles.subject}>{subject}</div>
-        <div style={styles.description}>
-          {description?.text}
-          <span style={styles.underlinedText}>{description?.underlinedText}</span>
-        </div>
+<div className={classes.container}>
+    <div className = {classes.topic}>
+      <div className={classes.fixedText}>Civil Pursuit</div>
+      <div className={classes.subject}>{subject}</div>
+      <div className = {classes.description}>
+        <Markdown>{description}</Markdown>
       </div>
-        <div style={styles.participantsContainer}>
-        <StatusBadge name={badgeName} status="" />
-        </div>
-        
+      
     </div>
-  ); 
+    <div className={classes.participants}>
+      <StatusBadge name={badgeName} status="" />
+      </div>
+    </div>
+  );
 };
 
 export default QuestionBox;
+
+const useStyles = createUseStyles({
+  container: {
+    borderRadius: '1.875rem',
+    border: '0.0625rem solid #D9D9D9',
+    backgroundColor: 'rgba(235, 235, 235, 0.4)',
+    boxSizing: 'border-box',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '3.625rem 9.875rem',
+  },
+  
+  topic: {
+    textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: '1.5rem',
+  },
+
+  fixedText: {
+    fontFamily: 'Inter',
+    fontWeight: 600,
+    fontSize: '1.0625rem',
+    lineHeight: '1.5625rem',
+    letterSpacing: '0.01em',
+    color: '#06335C',
+    textAlign: 'center',
+    whiteSpace: 'nowrap',
+  },
+  subject: {
+    fontFamily: 'Inter',
+    fontWeight: 700,
+    fontSize: '3.75rem',
+    lineHeight: '4.125rem',
+    letterSpacing: '-0.03em',
+    color: '#06335C',
+    textAlign: 'center',
+  },
+  description: {
+    fontFamily: 'Inter',
+    fontWeight: 400,
+    fontSize: '1rem',
+    lineHeight: '1.5rem',
+    color: '#06335C',
+    textAlign: 'center',
+  },
+  participants: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingTop: '1.6875rem'
+  },
+});
+
+
--- stories/question-box.stories.jsx
+++ stories/question-box.stories.jsx
@@ -1,23 +1,18 @@
-import React from 'react';
-import QuestionBox from '../app/components/question-box'; 
-
+import React from 'react'
+import QuestionBox from '../app/components/question-box'
 
 export default {
   component: QuestionBox,
-};
+}
 
-const Template = (args) => <QuestionBox {...args} />;
+const Template = args => <QuestionBox {...args} />
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
 Default.args = {
-  subject: "What one issue should ‘We the People’ unite and solve first to make our country even better?",
-  description: {
-    text: "This is a large-scale online discussion with the purpose of starting unbiased, and thoughtful conversations. ",
-    underlinedText: "We’re asking about concerns, not solutions."
-  },
+  subject: 'What one issue should ‘We the People’ unite and solve first to make our country even better?',
+  description: 'This is a large-scale online discussion with the purpose of starting unbiased, and thoughtful conversations. We’re asking about concerns, not solutions.',
   participants: 1009,
-};
+}
 
-export const Empty = Template.bind({});
-Empty.args = {};
-
+export const Empty = Template.bind({})
+Empty.args = {}
