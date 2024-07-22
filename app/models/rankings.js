0eee70665af1c0dbd22c5e2f78c24cbf5f27a09a
Chenxuan Liu <chenxuanliu@ChenxuandeMacBook-Pro.local>
2024-07-22 02:00:07 -0500

    change the ranking using schema

--- app/models/rankings.js
+++ app/models/rankings.js
@@ -1,23 +1,34 @@
 // https://github.com/EnCiv/civil-pursuit/issues/136
 
-const { Collection } = require('@enciv/mongo-collections')
+const { Mongo, Collection } = require('@enciv/mongo-collections')
 
 class Rankings extends Collection {
   static collectionName = 'rankings'
 
-  static collectionOptions = {}
+  static collectionOptions = {
+    validator: {
+      $jsonSchema: {
+        bsonType: 'object',
+        title: 'Rankings Object Validation',
+        required: ['parentId', 'userId'],
+        properties: {
+          parentId: {
+            bsonType: 'string',
+            description: "'parentId' must be a string and is required",
+          },
+          userId: {
+            bsonType: 'string',
+            description: "'userId' must be a string and is required",
+          },
+        },
+      },
+    },
+  }
 
   static collectionIndexes = [
     { key: { parentId: 1 }, name: 'parentId_index' },
     { key: { userId: 1 }, name: 'userId_index' },
   ]
-
-  static validate(doc) {
-    if (!doc.parentId || !doc.userId) {
-      return { error: 'parentId and userId are required' }
-    }
-    return { result: doc }
-  }
 }
 
 Rankings.setCollectionProps()
