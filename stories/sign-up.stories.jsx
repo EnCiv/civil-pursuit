618474e49fa861be27443160ec1dbdfb10e27cf2
Evan Mao <evanzmao@gmail.com>
2024-07-23 11:29:18 -0700

    3rd pull request changes

--- stories/sign-up.stories.jsx
+++ stories/sign-up.stories.jsx
@@ -9,40 +9,31 @@ export default {
   args: {},
   decorators: [
     Story => (
-        <div>
-            success@email.com / 'password' will succeed
-            <br />
-            all other combinations will fail
-            <Story />
-            </div>
-        ),
-    ],
+      <div>
+        success@email.com / 'password' will succeed
+        <br />
+        all other combinations will fail
+        <Story />
+      </div>
+    ),
+  ],
   parameters: {
     viewport: {
-        viewports: INITIAL_VIEWPORTS
-    }
-  }
+      viewports: INITIAL_VIEWPORTS,
+    },
+  },
 }
 
 export const sign_up_page = {
-    args: {
-        startTab: 'Sign Up'
-    }
+  args: {
+    startTab: 'Sign Up',
+  },
 }
 
 export const Log_In_Page = {
-    args: {
-        startTab: 'Log In'
-    }
-}
-
-export const Mobile_View = {
-    parameters: {
-        viewport: {
-            defaultViewport: 'iphonex',
-        }
-    },
-    args: {}
+  args: {
+    startTab: 'Log In',
+  },
 }
 
 // export const DestinationHomeString = mC({ destination: '/?path=/story/app--home' })
