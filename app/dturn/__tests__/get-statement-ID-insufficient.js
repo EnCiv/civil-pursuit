af055f0ffbb9a49ddeaa5c0294037b117b34675d
Chenxuan Liu <chenxuanliu@ChenxuandeMacBook-Pro.local>
2024-07-29 04:29:23 -0500

    update the previous tests and adding new test for get insufficient statment ID

---
+++ app/dturn/__tests__/get-statement-ID-insufficient.js
@@ -0,0 +1,21 @@
+const { Mongo } = require('@enciv/mongo-collections')
+const { initDiscussion, getStatementIds } = require('../dturn')
+
+const DISCUSSION_ID = 'testDiscussion'
+const USER1 = 'user1'
+
+beforeAll(async () => {
+  await Mongo.connect(global.__MONGO_URI__, { useUnifiedTopology: true })
+  await initDiscussion(DISCUSSION_ID)
+})
+
+afterAll(() => {
+  Mongo.disconnect()
+})
+
+describe('Get Statements', () => {
+  test('Cannot get statements from a discussion that does not have enough statements', async () => {
+    const statements = await getStatementIds(DISCUSSION_ID, 0, USER1)
+    expect(statements).toBeUndefined()
+  })
+})
--- app/dturn/__tests__/init-discussion.js
+++ app/dturn/__tests__/init-discussion.js
@@ -1,9 +1,24 @@
-const { initDiscussion, Discussions } = require('../dturn')
+const { Mongo } = require('@enciv/mongo-collections')
+const { initDiscussion } = require('../dturn')
 
-describe('initDiscussion', () => {
+const DISCUSSION_ID = 'testDiscussion'
+
+beforeAll(async () => {
+  await Mongo.connect(global.__MONGO_URI__, { useUnifiedTopology: true })
+})
+
+afterAll(() => {
+  Mongo.disconnect()
+})
+
+describe('Initialize Discussion', () => {
   test('Can initialize an empty discussion', async () => {
-    await initDiscussion('testDiscussion')
-    expect(Discussions['testDiscussion']).toBeDefined()
-    expect(Discussions['testDiscussion'].ShownStatements).toEqual([])
+    await initDiscussion(DISCUSSION_ID)
+    const discussion = require('../dturn').Discussions[DISCUSSION_ID]
+    expect(discussion).toBeDefined()
+    expect(discussion.ShownStatements).toEqual([])
+    expect(discussion.ShownGroups).toEqual([])
+    expect(discussion.Gitems).toEqual([])
+    expect(discussion.Uitems).toEqual({})
   })
 })
