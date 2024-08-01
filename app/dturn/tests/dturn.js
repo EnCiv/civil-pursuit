0800bef383a86e7b42f0527ee76594ec5e7455ba
Justin B. Yee <jbyee2015@gmail.com>
2024-08-01 14:30:14 -0700

    Refactored discussion init options into a dict for reusability.

--- app/dturn/tests/dturn.js
+++ app/dturn/tests/dturn.js
@@ -1,13 +1,13 @@
 // https://github.com/EnCiv/civil-pursuit/issues/171
 
-const { initDiscussion, getDiscussionInitOptions } = require('../dturn')
+const { initDiscussion } = require('../dturn')
 
 import { Mongo } from '@enciv/mongo-collections'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 
 // Config
 const DISCUSSION_ID = 1
-const INIT_OPTIONS = getDiscussionInitOptions()
+const nonexistentOptionErrorRegex = /'a_nonexistent_option' is not an option for initDiscussion()/
 
 let MemoryServer
 
@@ -24,23 +24,19 @@ afterAll(async () => {
 
 // Tests
 test('Succeed if all options are valid.', async () => {
-  initDiscussion(DISCUSSION_ID, { group_size: 10 })
+  initDiscussion(DISCUSSION_ID, { group_size: 15 })
 })
 
 test('Fail if all options are not valid.', async () => {
   const func = async () => {
     await initDiscussion(DISCUSSION_ID, { a_nonexistent_option: 12345 })
   }
-  expect(func()).rejects.toThrow(
-    `'a_nonexistent_option' is not an option for initDiscussion() - valid options are: ${INIT_OPTIONS}`
-  )
+  expect(func()).rejects.toThrow(nonexistentOptionErrorRegex)
 })
 
 test('Fail if at least 1 option is not valid.', async () => {
   const func = async () => {
     await initDiscussion(DISCUSSION_ID, { group_size: 10, a_nonexistent_option: 12345 })
   }
-  expect(func()).rejects.toThrow(
-    `'a_nonexistent_option' is not an option for initDiscussion() - valid options are: ${INIT_OPTIONS}`
-  )
+  expect(func()).rejects.toThrow(nonexistentOptionErrorRegex)
 })
