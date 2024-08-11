c2d1d167c2e357c04a64e5fbb7b0cabfae7580bb
Justin B. Yee <jbyee2015@gmail.com>
2024-08-10 17:37:12 -0700

    Progress toward rendering children and info in step-slider.

--- app/components/tournament.js
+++ app/components/tournament.js
@@ -1,6 +1,6 @@
 // https://github.com/EnCiv/civil-pursuit/issues/151
 
-import React from 'react'
+import React, { useReducer } from 'react'
 import { createUseStyles } from 'react-jss'
 import cx from 'classnames'
 
@@ -17,29 +17,74 @@ const WebComponents = {
   StepBar: StepBar,
   RoundTracker: RoundTracker,
   StepSlider: StepSlider,
-  // TODO: Import answer step component here.,
+  Answer: undefined, // TODO: Import answer step component here.,
   GroupingStep: GroupingStep,
   RankStep: RankStep,
-  ReviewPointList: ReviewPointList,
+  Review: ReviewPointList,
   WhyStep: WhyStep,
   CompareReasons: CompareReasons,
-  // TODO: Import Intermission component here.
+  Intermission: undefined, // TODO: Import Intermission component here.
 }
 
-export default function Tournament(props) {
+function buildChildren(steps, round) {
+  return steps.map(step => {
+    const { webComponent, ...props } = step
+
+    const LookupResult = WebComponents[webComponent]
+    if (LookupResult) {
+      // Pass all props from step obj except WebComponent
+      return <LookupResult {...props} round={round} />
+    } else {
+      console.error(`Couldn't render step - component '${webComponent}' was not found in WebComponents.`)
+      return <div>Couldn't render step - component '{webComponent}' was not found in WebComponents.</div>
+    }
+  })
+}
+
+function Tournament(props) {
   const { className, steps = [], ...otherProps } = props
   const classes = useStylesFromThemeFunction(props)
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'incrementRound':
+        // Rebuild steps when going to next round
+        const newRound = state.currentRound + 1
+
+        return {
+          currentRound: newRound,
+          stepComponents: buildChildren(steps, newRound),
+        }
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, { currentRound: 1, stepComponents: buildChildren(steps, 1) })
+
+  const stepInfo = steps.map(step => {
+    return {
+      name: step.stepName,
+      title: step.stepIntro.description,
+    }
+  })
 
   return (
     <div className={cx(classes.wrapper, className)} {...otherProps}>
       <RoundTracker />
+      <StepSlider
+        steps={stepInfo}
+        children={state.stepComponents}
+        onDone={valid => {
+          if (valid) dispatch({ type: 'incrementRound' })
+        }}
+      />
     </div>
   )
 }
 
 const useStylesFromThemeFunction = createUseStyles(theme => ({
   wrapper: {
-    background: theme.colorPrimary,
-    padding: '1rem',
+    width: '100%',
   },
 }))
+
+export default Tournament
