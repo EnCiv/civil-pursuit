a9f5e7e57d1240a0a9a6dbe71b89e02f859b8a06
David Fridley <ddfridley@yahoo.com>
2024-08-12 21:56:07 -0700

    getStatementIds will not  fail if users statement gotten at random

--- app/dturn/__tests__/add-retrieve-10-statements.js
+++ app/dturn/__tests__/add-retrieve-10-statements.js
@@ -1,8 +1,5 @@
 // https://github.com/EnCiv/civil-pursuit/issues/154
-const { Mongo } = require('@enciv/mongo-collections')
 const { initDiscussion, insertStatementId, getStatementIds, Discussions } = require('../dturn')
-const fs = require('fs')
-const path = require('path')
 
 const DISCUSSION_ID = 'testDiscussion'
 const USER_ID = 'user1'
@@ -16,33 +13,37 @@ const OPTIONS = {
   getAllUInfo: async () => [],
 }
 
-const logFilePath = path.join(__dirname, 'test-log.txt')
-
-function logToFile(content) {
-  fs.writeFileSync(logFilePath, content)
-}
-
-beforeAll(async () => {
-  await Mongo.connect(global.__MONGO_URI__, { useUnifiedTopology: true })
-  await initDiscussion(DISCUSSION_ID, OPTIONS)
-  for (let i = 0; i < 20; i++) {
-    await insertStatementId(DISCUSSION_ID, `user${i}`, `statement${i}`)
-  }
-  logToFile('Discussion state after insertion:\n' + JSON.stringify(Discussions[DISCUSSION_ID], null, 2))
-})
-
-afterAll(() => {
-  Mongo.disconnect()
-})
-
 describe('Add and Retrieve 10 Statements', () => {
   test('Can add 10 statements to a discussion, with group size of 10', async () => {
-    const discussion = Discussions[DISCUSSION_ID]
-    logToFile('Discussion state before retrieval:\n' + JSON.stringify(discussion, null, 2))
+    await initDiscussion(DISCUSSION_ID, OPTIONS)
+    const props = []
+    for (let i = 0; i < 20; i++) {
+      props.push([DISCUSSION_ID, `user${i}`, `statement${i}`])
+    }
+    for await (const args of props) {
+      await insertStatementId(...args)
+    }
 
     const statements = await getStatementIds(DISCUSSION_ID, 0, USER_ID)
-    console.log('Retrieved statements:', statements)
-    logToFile('Retrieved statements after first retrieval:\n' + JSON.stringify(statements, null, 2))
+
+    expect(statements).toBeDefined()
+    expect(statements.length).toBe(10)
+  })
+})
+
+describe('Add and Retrieve 10 Statements not randomly', () => {
+  test('Can add 10 statements to a discussion, with group size of 10', async () => {
+    await initDiscussion(DISCUSSION_ID, OPTIONS)
+    const props = []
+    for (let i = 0; i < 20; i++) {
+      props.push([DISCUSSION_ID, `user${i}`, `statement${i}`])
+    }
+    for await (const args of props) {
+      await insertStatementId(...args)
+    }
+    process.env.JEST_TEST_ENV = 'true'
+
+    const statements = await getStatementIds(DISCUSSION_ID, 0, USER_ID)
 
     expect(statements).toBeDefined()
     expect(statements.length).toBe(10)
