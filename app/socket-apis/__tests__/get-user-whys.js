1d8c9887a8f1db534ecf918380b4f5ed77325637
MingyiXu11 <XMY1005594341@126.com>
2024-10-07 08:53:14 -0500

    getUserWhys API and jest test

---
+++ app/socket-apis/__tests__/get-user-whys.js
@@ -0,0 +1,93 @@
+const { MongoMemoryServer } = require('mongodb-memory-server')
+const { MongoClient } = require('mongodb')
+const getUserWhys = require('../get-user-whys')
+const Points = require('../../models/points')
+
+let memoryServer
+let connection
+let db
+const userId = '12345678abcdefgh'
+const synuser = { synuser: { id: userId } }
+
+beforeAll(async () => {
+  memoryServer = await MongoMemoryServer.create()
+  const uri = memoryServer.getUri()
+  connection = await MongoClient.connect(uri, { useNewUrlParser: true })
+  db = connection.db()
+  Points.find = jest.fn().mockImplementation(query => ({
+    toArray: jest.fn().mockResolvedValueOnce(db.collection('points').find(query).toArray()),
+  }))
+})
+
+afterAll(async () => {
+  await connection.close()
+  await memoryServer.stop()
+})
+
+beforeEach(() => {
+  jest.spyOn(console, 'error').mockImplementation(() => {})
+})
+
+afterEach(() => {
+  console.error.mockRestore()
+})
+
+// Test 1: User not logged in
+test('Return undefined if user is not logged in.', async () => {
+  const cb = jest.fn()
+  await getUserWhys.call({}, ['id1', 'id2'], cb)
+
+  expect(cb).toHaveBeenCalledTimes(1)
+  expect(cb).toHaveBeenCalledWith(undefined)
+  expect(console.error).toHaveBeenCalledWith('Cannot retrieve whys - user is not logged in.')
+})
+
+// Test 2: Return empty array if no data found
+test('Return empty array if no data found.', async () => {
+  const cb = jest.fn()
+
+  // Mock the find function to return an empty array
+  Points.find.mockImplementationOnce(() => ({
+    toArray: jest.fn().mockResolvedValueOnce([]),
+  }))
+
+  await getUserWhys.call(synuser, ['nonexistentId'], cb)
+
+  expect(cb).toHaveBeenCalledTimes(1)
+  expect(cb).toHaveBeenCalledWith([])
+  expect(console.error).not.toHaveBeenCalled()
+})
+
+// Test 3: Return list of 5 items
+test('Return list of items matching parentId in ids.', async () => {
+  const cb = jest.fn()
+
+  // Mock the find function to return items where parentId is in ids
+  const mockPoints = [
+    { _id: '1', title: 'Point1', userId, parentId: 'id1' },
+    { _id: '2', title: 'Point2', userId, parentId: 'id1' },
+    { _id: '3', title: 'Point3', userId, parentId: 'id2' },
+    { _id: '4', title: 'Point4', userId, parentId: 'id2' },
+    { _id: '5', title: 'Point5', userId, parentId: 'id1' },
+    // These items should not be returned because their parentId is not in ['id1', 'id2']
+    { _id: '6', title: 'Point6', userId, parentId: 'id3' },
+    { _id: '7', title: 'Point7', userId, parentId: 'id4' },
+    // These items should not be returned because their userId is not equals to current userId
+    { _id: '8', title: 'Point8', userId: userId + '0', parentId: 'id1' },
+    { _id: '9', title: 'Point9', userId: userId + '0', parentId: 'id2' },
+  ]
+
+  // Filter only the points where parentId is 'id1' or 'id2' and userId matches
+  const expectedPoints = mockPoints.filter(point => ['id1', 'id2'].includes(point.parentId) && point.userId === userId)
+
+  Points.find.mockImplementationOnce(() => ({
+    toArray: jest.fn().mockResolvedValueOnce(expectedPoints),
+  }))
+
+  const result = await getUserWhys.call(synuser, ['id1', 'id2'], cb)
+
+  expect(cb).toHaveBeenCalledTimes(1)
+  expect(cb).toHaveBeenCalledWith(expectedPoints)
+  expect(result).toEqual(expectedPoints)
+  expect(result.length).toBe(5) // This checks that the result contains 5 items
+})
---
+++ app/socket-apis/get-user-whys.js
@@ -0,0 +1,39 @@
+// https://github.com/EnCiv/civil-pursuit/issues/206
+
+const Points = require('../models/points')
+async function getUserWhys(ids, cb) {
+  const cbFailure = errorMsg => {
+    if (errorMsg) console.error(errorMsg)
+    if (cb) cb(undefined)
+  }
+
+  // Check if user is logged in
+  if (!this.synuser || !this.synuser.id) {
+    return cbFailure('Cannot retrieve whys - user is not logged in.')
+  }
+
+  // Verify that ids is provided and is a valid array
+  if (!Array.isArray(ids) || ids.length === 0) {
+    return cbFailure('Invalid argument provided to getUserWhys(ids: Array, cb: Function).')
+  }
+
+  // Fetch points from the Points collection where parentId matches any of the ids
+  let pointsList
+  try {
+    pointsList = await Points.find({ parentId: { $in: ids }, userId: this.synuser.id }).toArray()
+  } catch (error) {
+    return cbFailure('Failed to retrieve points - Points.find failed.')
+  }
+
+  // If no points found, return an empty array
+  if (!pointsList || pointsList.length === 0) {
+    if (cb) cb([])
+    return []
+  }
+
+  // Return the points list
+  if (cb) cb(pointsList)
+  return pointsList
+}
+
+module.exports = getUserWhys
