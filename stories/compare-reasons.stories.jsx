b56584db2407cc6a7f112ce75e43b5849612e177
David Fridley <ddfridley@yahoo.com>
2024-11-03 14:04:59 -0800

    width 240

--- stories/compare-reasons.stories.jsx
+++ stories/compare-reasons.stories.jsx
@@ -5,211 +5,47 @@ import { onDoneDecorator, onDoneResult } from './common'
 import { within, userEvent } from '@storybook/test'
 import expect from 'expect'
 
-export default {
-  component: CompareReasons,
-  args: {},
-  decorators: [onDoneDecorator],
-}
+export default { component: CompareReasons, args: {}, decorators: [onDoneDecorator] }
 
 const pointWithWhyRankListList = [
   {
     point: { _id: '1', subject: 'subject 1', description: 'describe 1' },
     whyRankList: [
-      {
-        why: {
-          _id: '2',
-          subject: '1 is less than 2',
-          description: '2 is why because',
-          parentId: '1',
-          category: 'most',
-        },
-      },
-      {
-        why: {
-          _id: '3',
-          subject: '1 is less than 3',
-          description: '3 is why because',
-          parentId: '1',
-          category: 'most',
-        },
-      },
-      {
-        why: {
-          _id: '4',
-          subject: '1 is less than 4',
-          description: '4 is why because',
-          parentId: '1',
-          category: 'most',
-        },
-      },
-      {
-        why: {
-          _id: '5',
-          subject: '1 is less than 5',
-          description: '5 is why because',
-          parentId: '1',
-          category: 'most',
-        },
-      },
-      {
-        why: {
-          _id: '6',
-          subject: '1 is less than 6',
-          description: '6 is why because',
-          parentId: '1',
-          category: 'most',
-        },
-      },
+      { why: { _id: '2', subject: '1 is less than 2', description: '2 is why because', parentId: '1', category: 'most' } },
+      { why: { _id: '3', subject: '1 is less than 3', description: '3 is why because', parentId: '1', category: 'most' } },
+      { why: { _id: '4', subject: '1 is less than 4', description: '4 is why because', parentId: '1', category: 'most' } },
+      { why: { _id: '5', subject: '1 is less than 5', description: '5 is why because', parentId: '1', category: 'most' } },
+      { why: { _id: '6', subject: '1 is less than 6', description: '6 is why because', parentId: '1', category: 'most' } },
     ],
   },
   {
     point: { _id: '21', subject: 'subject 20', description: 'describe 20' },
     whyRankList: [
-      {
-        why: {
-          _id: '22',
-          subject: '21 is less than 2',
-          description: '2 is why because',
-          parentId: '21',
-          category: 'most',
-        },
-      },
-      {
-        why: {
-          _id: '23',
-          subject: '21 is less than 3',
-          description: '3 is why because',
-          parentId: '21',
-          category: 'most',
-        },
-      },
-      {
-        why: {
-          _id: '24',
-          subject: '21 is less than 4',
-          description: '4 is why because',
-          parentId: '21',
-          category: 'most',
-        },
-      },
-      {
-        why: {
-          _id: '25',
-          subject: '21 is less than 5',
-          description: '5 is why because',
-          parentId: '21',
-          category: 'most',
-        },
-      },
-      {
-        why: {
-          _id: '26',
-          subject: '21 is less than 6',
-          description: '6 is why because',
-          parentId: '21',
-          category: 'most',
-        },
-      },
+      { why: { _id: '22', subject: '21 is less than 2', description: '2 is why because', parentId: '21', category: 'most' } },
+      { why: { _id: '23', subject: '21 is less than 3', description: '3 is why because', parentId: '21', category: 'most' } },
+      { why: { _id: '24', subject: '21 is less than 4', description: '4 is why because', parentId: '21', category: 'most' } },
+      { why: { _id: '25', subject: '21 is less than 5', description: '5 is why because', parentId: '21', category: 'most' } },
+      { why: { _id: '26', subject: '21 is less than 6', description: '6 is why because', parentId: '21', category: 'most' } },
     ],
   },
   {
     point: { _id: '31', subject: 'subject 30', description: 'describe 30' },
     whyRankList: [
-      {
-        why: {
-          _id: '32',
-          subject: '21 is less than 2',
-          description: '2 is why because',
-          parentId: '31',
-          category: 'most',
-        },
-      },
-      {
-        why: {
-          _id: '33',
-          subject: '21 is less than 3',
-          description: '3 is why because',
-          parentId: '31',
-          category: 'most',
-        },
-      },
-      {
-        why: {
-          _id: '34',
-          subject: '21 is less than 4',
-          description: '4 is why because',
-          parentId: '31',
-          category: 'most',
-        },
-      },
-      {
-        why: {
-          _id: '35',
-          subject: '21 is less than 5',
-          description: '5 is why because',
-          parentId: '31',
-          category: 'most',
-        },
-      },
-      {
-        why: {
-          _id: '36',
-          subject: '21 is less than 6',
-          description: '6 is why because',
-          parentId: '31',
-          category: 'most',
-        },
-      },
+      { why: { _id: '32', subject: '21 is less than 2', description: '2 is why because', parentId: '31', category: 'most' } },
+      { why: { _id: '33', subject: '21 is less than 3', description: '3 is why because', parentId: '31', category: 'most' } },
+      { why: { _id: '34', subject: '21 is less than 4', description: '4 is why because', parentId: '31', category: 'most' } },
+      { why: { _id: '35', subject: '21 is less than 5', description: '5 is why because', parentId: '31', category: 'most' } },
+      { why: { _id: '36', subject: '21 is less than 6', description: '6 is why because', parentId: '31', category: 'most' } },
     ],
   },
   {
     point: { _id: '41', subject: 'subject 40', description: 'describe 40' },
     whyRankList: [
-      {
-        why: {
-          _id: '42',
-          subject: '21 is less than 2',
-          description: '2 is why because',
-          parentId: '41',
-          category: 'least',
-        },
-      },
-      {
-        why: {
-          _id: '43',
-          subject: '21 is less than 3',
-          description: '3 is why because',
-          parentId: '41',
-          category: 'least',
-        },
-      },
-      {
-        why: {
-          _id: '44',
-          subject: '21 is less than 4',
-          description: '4 is why because',
-          parentId: '41',
-          category: 'least',
-        },
-      },
-      {
-        why: {
-          _id: '45',
-          subject: '21 is less than 5',
-          description: '5 is why because',
-          parentId: '41',
-          category: 'least',
-        },
-      },
-      {
-        why: {
-          _id: '46',
-          subject: '21 is less than 6',
-          description: '6 is why because',
-          parentId: '41',
-          category: 'least',
-        },
-      },
+      { why: { _id: '42', subject: '21 is less than 2', description: '2 is why because', parentId: '41', category: 'least' } },
+      { why: { _id: '43', subject: '21 is less than 3', description: '3 is why because', parentId: '41', category: 'least' } },
+      { why: { _id: '44', subject: '21 is less than 4', description: '4 is why because', parentId: '41', category: 'least' } },
+      { why: { _id: '45', subject: '21 is less than 5', description: '5 is why because', parentId: '41', category: 'least' } },
+      { why: { _id: '46', subject: '21 is less than 6', description: '6 is why because', parentId: '41', category: 'least' } },
     ],
   },
 ]
@@ -238,51 +74,20 @@ const pointList = [
     subject: 'Headline Issue #1',
     description: 'Description for Headline Issue #1',
     pointWithWhyRankListList,
-    reasonPoints: {
-      most: [pointOne, pointTwo, pointThree, pointFour, pointFive],
-      least: [pointSix, pointSeven, pointEight, pointNine, pointTen],
-    },
+    reasonPoints: { most: [pointOne, pointTwo, pointThree, pointFour, pointFive], least: [pointSix, pointSeven, pointEight, pointNine, pointTen] },
   },
-  {
-    subject: 'Headline Issue #2',
-    description: 'Description for Headline Issue #2',
-    reasonPoints: {
-      most: [pointEleven, pointTwelve],
-      least: [pointThirteen, pointFourteen],
-    },
-  },
-  {
-    subject: 'Headline Issue #3',
-    description: 'Description for Headline Issue #3',
-    reasonPoints: {
-      most: [pointFifteen, pointSixteen],
-      least: [pointSeventeen, pointEighteen],
-    },
-  },
+  { subject: 'Headline Issue #2', description: 'Description for Headline Issue #2', reasonPoints: { most: [pointEleven, pointTwelve], least: [pointThirteen, pointFourteen] } },
+  { subject: 'Headline Issue #3', description: 'Description for Headline Issue #3', reasonPoints: { most: [pointFifteen, pointSixteen], least: [pointSeventeen, pointEighteen] } },
 ]
 
-export const threePointLists = {
-  args: {
-    pointWithWhyRankListList,
-    side: 'most',
-  },
-}
+export const threePointLists = { args: { pointWithWhyRankListList, side: 'most' } }
 
-export const emptyPointList = {
-  args: {
-    pointList: [],
-  },
-}
+export const emptyPointList = { args: { pointList: [] } }
 
-export const emptyArgs = {
-  args: {},
-}
+export const emptyArgs = { args: {} }
 
 export const twoPointListsPlayThrough = {
-  args: {
-    pointList: pointList.slice(1, 3),
-    side: 'least',
-  },
+  args: { pointList: pointList.slice(1, 3), side: 'least' },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement)
     const pointThirteen = canvas.getByText('Point 13')
@@ -290,12 +95,6 @@ export const twoPointListsPlayThrough = {
 
     await userEvent.click(pointThirteen)
     await userEvent.click(pointSeventeen)
-    expect(onDoneResult(canvas)).toMatchObject({
-      count: 5,
-      onDoneResult: {
-        valid: true,
-        value: 100,
-      },
-    })
+    expect(onDoneResult(canvas)).toMatchObject({ count: 5, onDoneResult: { valid: true, value: 100 } })
   },
 }
