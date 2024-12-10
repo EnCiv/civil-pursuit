7d876bc171b6e4e5459a6ef3f574de8905a64446
David Fridley <ddfridley@yahoo.com>
2024-12-10 12:14:23 -0800

    subobjects that change are new objects

--- app/lib/__tests__/set-or-delete-by-mutate-path.js
+++ app/lib/__tests__/set-or-delete-by-mutate-path.js
@@ -1,17 +1,158 @@
 import setOrDeleteByMutatePath from '../set-or-delete-by-mutate-path'
 
 describe('test setOrDeleteByMutatePath', () => {
+  test('a simple object', () => {
+    const messages = []
+    const dst = { a: 1 }
+    const result = setOrDeleteByMutatePath(dst, { a: 2 }, messages)
+    expect(result).toEqual({ a: 2 })
+    expect(result).not.toBe(dst)
+    expect(messages).toMatchObject([
+      //
+      'a: changing 1 to 2',
+      'returning mutated copy',
+    ])
+  })
   test('set an object', () => {
+    const messages = []
+    const dst = {
+      a: {
+        b: 1, // don't pretty this into one line
+      },
+    }
+    const result = setOrDeleteByMutatePath(dst, { a: { b: 2 } }, messages)
+    expect(result).toMatchObject({ a: { b: 2 } })
+    expect(result).not.toBe(dst)
+    expect(result.a).not.toBe(dst.a)
+    expect(messages).toMatchObject([
+      //
+      'a.b: changing 1 to 2',
+      'mutating a',
+      'returning mutated copy',
+    ])
+  })
+  test("set doesn't really change anything", () => {
+    const messages = []
+    const dst = {
+      a: {
+        b: 1, // don't pretty this into one line
+      },
+    }
+    const result = setOrDeleteByMutatePath(dst, { a: { b: 1 } }, messages)
+    expect(result).toMatchObject({ a: { b: 1 } })
+    expect(result).toBe(dst)
+    expect(result.a).toBe(dst.a)
+    expect(messages).toMatchObject([])
+  })
+  test('set some stuff but leave others alone', () => {
+    const messages = []
     const dst = {
       a: {
         b: 1, // don't pretty this into one line
         c: 2,
+        d: 3,
+      },
+      f: {
+        g: 4,
       },
     }
-    const result = setOrDeleteByMutatePath(dst, { a: { b: 2 } })
-    expect(result).toMatchObject({ a: { b: 2, c: 2 } })
+    const result = setOrDeleteByMutatePath(dst, { a: { b: 2 } }, messages)
+    expect(result).toMatchObject({ a: { b: 2, c: 2, d: 3 }, f: { g: 4 } })
     expect(result).not.toBe(dst)
-    expect(result.a.c).toBe(dst.a.c)
     expect(result.a).not.toBe(dst.a)
+    expect(result.f).toBe(dst.f)
+    expect(result.f.g).toBe(dst.f.g)
+    expect(messages).toMatchObject([
+      //
+      'a.b: changing 1 to 2',
+      'mutating a',
+      'returning mutated copy',
+    ])
+  })
+  test('can set and delete things', () => {
+    const messages = []
+    const dst = {
+      a: {
+        b: 1, // don't pretty this into one line
+        c: 2,
+        d: 3,
+      },
+      f: {
+        g: 4,
+      },
+    }
+    const result = setOrDeleteByMutatePath(dst, { a: { b: 2, c: undefined }, f: undefined }, messages)
+    expect(result).toMatchObject({ a: { b: 2, d: 3 } })
+    expect(result).not.toBe(dst)
+    expect(result.a).not.toBe(dst.a)
+    expect(messages).toMatchObject([
+      //
+      'a.b: changing 1 to 2',
+      'deleting a.c: 2',
+      'mutating a',
+      'deleting f: {"g":4}',
+      'returning mutated copy',
+    ])
+  })
+  test('can set and delete things deeply', () => {
+    const messages = []
+    const dst = {
+      z: {
+        a: {
+          b: 1, // don't pretty this into one line
+          c: 2,
+          d: 3,
+        },
+        f: {
+          g: 4,
+        },
+      },
+    }
+    const result = setOrDeleteByMutatePath(dst, { z: { a: { b: 2, c: undefined }, f: undefined } }, messages)
+    expect(result).toMatchObject({ z: { a: { b: 2, d: 3 } } })
+    expect(result).not.toBe(dst)
+    expect(result.z).not.toBe(dst.z)
+    expect(result.z.a).not.toBe(dst.z.a)
+    expect(messages).toMatchObject([
+      //
+      'z.a.b: changing 1 to 2',
+      'deleting z.a.c: 2',
+      'mutating z.a',
+      'deleting z.f: {"g":4}',
+      'mutating z',
+      'returning mutated copy',
+    ])
+  })
+  test('it can change arrays', () => {
+    const messages = []
+    const dst = ['1', '2', '3', '4']
+    const result = setOrDeleteByMutatePath(dst, ['0'], messages)
+    expect(result).toMatchObject(['0'])
+    expect(result).not.toBe(dst)
+    expect(messages).toMatchObject([
+      //
+      '[0]: changing "1" to "0"',
+      'deleting [1]: "2"',
+      'deleting [2]: "3"',
+      'deleting [3]: "4"',
+      'returning mutated copy',
+    ])
+  })
+  test('it can change deep in arrays', () => {
+    const messages = []
+    const dst = [{ a: 1 }, { b: 2 }, { c: { d: 7 } }]
+    const result = setOrDeleteByMutatePath(dst, [{ a: 1 }, { b: 2 }, { c: { f: 8 } }], messages)
+    expect(result).toMatchObject([{ a: 1 }, { b: 2 }, { c: { d: 7, f: 8 } }])
+    expect(result).not.toBe(dst)
+    expect(result[0]).toBe(dst[0])
+    expect(result[1]).toBe(dst[1])
+    expect(result[2]).not.toBe(dst[2])
+    expect(messages).toMatchObject([
+      //don't pretty this into one line
+      '[2].c.f: changing not-present to 8',
+      'mutating [2].c',
+      'mutating [2]',
+      'returning mutated copy',
+    ])
   })
 })
--- app/lib/set-or-delete-by-mutate-path.js
+++ app/lib/set-or-delete-by-mutate-path.js
@@ -1,5 +1,36 @@
 const cloneDeep = require('lodash/cloneDeep')
 
+/**
+setOrDeleteByMutatePath(dst, src, messages, keyPath)
+
+ dst: the object that src will be upserted into
+ src: the object that is to be upserted into dst
+
+optional: 
+ messages: and array of strings detailing the changes. If undefined, no messages are generated. Mostly messages are for debugging and inspecting.
+ keyPath: the object path in messages will be prefixed with keyPath, which could be the name of the dst variable for example
+
+returns 
+  dst if no real changes, or a new object, if changes were made
+
+This function is applied recursively, so any sub-object that has changes in it will also be a new object, 
+but sub-objects with no changes will keep the same object ref.
+
+This is userful when react is rendering from a complex object that is handled by multiple react components,
+only the components where there have been changes will see new props and rerender. Components that render
+sub-objects that have not changed, will not rerender. For example if react is rendering a list, and only one
+item in the list has chanaged, only the one item should rerender.
+
+This works with plain objects and arrays. Has not been tested with maps or sets.
+
+Also, if a property in src is present and set to undefined, like {a: {b: undefined}}, then the property (like a.b) in the dst will be deleted
+
+Sub-objects in src will be cloned so nothing in src will be referced after use
+
+Sub-objects of dst will be referenced directly if they have not changed, 
+
+ */
+
 function sameKeys(dstKeys, srcKeys) {
   // keys are from an array so they are in order
   if (dstKeys.length !== srcKeys.length) return false
@@ -15,65 +46,79 @@ function stringify(obj, key) {
   const value = obj[key]
   if (typeof value === 'undefined' && !Object.keys(obj).includes(key)) return 'not-present'
   if (typeof value === 'function') return (value.name || 'unnamed') + '()'
-  return JSON.stringify(value, null, 2)
+  // ??? it would be better to use util.inspect but that doesn't work on browser. Tried loupe but have trouble with require/import in node 16
+  return JSON.stringify(value)
 }
 
 function setOrDeleteKeyWithMessages(dst, src, messages, keyPath, key) {
   if (typeof dst[key] === 'object' && typeof src[key] === 'object' && dst[key] !== null && src[key] !== null) {
-    const updated = setOrDeleteWithMessages(dst[key], src[key], messages, keyPath)
-    if (updated) dst[key] = { ...dst[key] }
-    return updated
+    const wasUpdated = setOrDeleteByMutatePath(dst[key], src[key], messages, keyPath)
+    if (wasUpdated !== dst[key]) {
+      return wasUpdated
+    }
   } else {
     if (typeof src[key] === 'object' && src[key] !== null) {
       if (messages) messages.push(`${keyPath}: changing ${stringify(dst, key)} to ${stringify(src, key)}`)
-      dst[key] = cloneDeep(src[key])
-      return 1
+      return cloneDeep(src[key])
     } else if (dst[key] !== src[key]) {
       if (messages) messages.push(`${keyPath}: changing ${stringify(dst, key)} to ${stringify(src, key)}`)
-      dst[key] = src[key]
-      return 1
+      return src[key]
     }
   }
-  return 0
+  return dst[key]
 }
 
-function setOrDeleteWithMessages(dst, src, messages, keyPath) {
+export default function setOrDeleteByMutatePath(dst, src, messages, keyPath) {
   let updated = 0
   if (typeof dst === 'object' && typeof src === 'object' && dst !== null && src !== null) {
     const dstKeys = Object.keys(dst)
     const srcKeys = Object.keys(src)
     if (Array.isArray(dst) && Array.isArray(src)) {
-      for (const key of srcKeys) updated += setOrDeleteKeyWithMessages(dst, src, messages, `${keyPath ? keyPath : ''}[${key}]`, key)
+      const newDst = []
+      for (const key of srcKeys) {
+        newDst[key] = setOrDeleteKeyWithMessages(dst, src, messages, `${keyPath ? keyPath : ''}[${key}]`, key)
+        if (newDst[key] !== dst[key]) ++updated
+      }
       if (!sameKeys(dstKeys, srcKeys)) {
-        for (const key of dstKeys.reverse()) {
-          // reverse because splice changes order each time
+        for (const key of dstKeys) {
           if (srcKeys.includes(key)) continue
           else {
-            if (messages) messages.push(`deleting ${keyPath ? keyPath : ''}[${key}]: ${dst[key]}`)
-            dst.splice(key, 1)
+            if (messages) messages.push(`deleting ${keyPath ? keyPath : ''}[${key}]: ${stringify(dst, key)}`)
             ++updated
           }
         }
-        return updated
+      }
+      if (updated) {
+        if (messages) messages.push(`${keyPath ? 'mutating ' + keyPath : 'returning mutated copy'}`)
+        return newDst
       }
     } else {
+      const newDst = {}
       for (const key of srcKeys) {
         if (typeof src[key] === 'undefined') {
-          if (messages) messages.push(`deleting ${keyPath ? keyPath + '.' : ''}${key}: ${dst[key]}`)
-          delete dst[key]
+          if (messages) messages.push(`deleting ${keyPath ? keyPath + '.' : ''}${key}: ${stringify(dst, key)}`)
+          //delete dst[key]
           ++updated
-        } else updated += setOrDeleteKeyWithMessages(dst, src, messages, (keyPath ? keyPath + '.' : '') + key, key)
+        } else {
+          newDst[key] = setOrDeleteKeyWithMessages(dst, src, messages, (keyPath ? keyPath + '.' : '') + key, key)
+          if (newDst[key] !== dst[key]) ++updated
+        }
+      }
+      if (!sameKeys(dstKeys, srcKeys)) {
+        for (const key of dstKeys) {
+          if (srcKeys.includes(key)) continue
+          else {
+            newDst[key] = dst[key]
+          }
+        }
+      }
+      if (updated) {
+        if (messages) messages.push(`${keyPath ? 'mutating ' + keyPath : 'returning mutated copy'}`)
+        return newDst
       }
     }
   } else {
     if (messages) messages.push(`${keyPath ? keyPath + ': c' : 'C'}an not change type ${typeof dst} to ${typeof src}`)
   }
-  return updated
-}
-
-// returns a top level copy of dst, with mutated changes along the src path(s)
-// or returns dst if there were no changes
-export default function setOrDeleteByMutatePath(dst, src, messages, keyPath) {
-  if (setOrDeleteWithMessages(dst, src, messages, keyPath)) return { ...dst }
-  else return dst
+  return dst
 }
