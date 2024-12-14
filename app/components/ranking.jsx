77ec38e66cc65ddf5d3f22b9a13cd9d6f98813dc
David Fridley <ddfridley@yahoo.com>
2024-12-13 22:31:57 -0800

    no onDone if initally empty

--- app/components/ranking.jsx
+++ app/components/ranking.jsx
@@ -14,22 +14,19 @@ const selectedOption = (
 const unselectedOption = <rect x="1" y="1" width="22" height="22" rx="11" stroke="#06335C" strokeWidth="2" />
 
 export default function Ranking(props) {
-  //Isolate props and set initial state
   const { disabled, defaultValue, className, onDone, ...otherProps } = props
   let [response, setResponse] = useState(responseOptions.includes(defaultValue) ? defaultValue : '')
   useEffect(() => {
-    if (defaultValue) {
-      if (!responseOptions.includes(defaultValue)) {
-        setResponse(undefined)
-        onDone && onDone({ valid: false, value: '' })
-      } else {
-        setResponse(defaultValue)
-        onDone && onDone({ valid: true, value: defaultValue })
-      }
+    if (!defaultValue && !response) return // do not call onDone if initally empty, or if change to empty from above, but it's already empty
+    if (responseOptions.includes(defaultValue)) {
+      setResponse(defaultValue)
+      onDone && onDone({ valid: true, value: defaultValue })
+    } else {
+      setResponse('')
+      onDone && onDone({ valid: false, value: '' })
     }
   }, [defaultValue])
 
-  //Introduce component styling
   const styleClasses = rankingStyleClasses(props)
 
   const onSelectionChange = e => {
@@ -38,23 +35,16 @@ export default function Ranking(props) {
     }
     setResponse(e.target.value)
     if (!onDone) {
-      return console.warn(
-        `Unhandled rank selection: ${e.target.value}. Please pass a handler function via the onDone prop.`
-      )
+      return console.warn(`Unhandled rank selection: ${e.target.value}. Please pass a handler function via the onDone prop.`)
     } else onDone({ valid: true, value: e.target.value })
-   
   }
   return (
-    <div
-      data-value={response}
-      className={cx(className, styleClasses.group, disabled && styleClasses.disabled)}
-      {...otherProps}
-    >
+    <div data-value={response} className={cx(className, styleClasses.group, disabled && styleClasses.disabled)} {...otherProps}>
       {responseOptions.map(option => {
         return (
           <label key={option}>
             <input
-              disabled={disabled || false}
+              disabled={disabled || false} /**/
               checked={response === option}
               type="radio"
               value={option}
@@ -63,12 +53,7 @@ export default function Ranking(props) {
               onChange={onSelectionChange}
             ></input>
             <span className={cx(styleClasses.option)}>
-              <svg
-                className={cx(styleClasses.optionIcon)}
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+              <svg className={cx(styleClasses.optionIcon)} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 {response === option ? selectedOption : unselectedOption}
               </svg>
               {option}
