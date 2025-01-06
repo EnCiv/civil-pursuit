6cee6446053d7fb04f1bcc825bc24f155e0bc4b0
aneelabanda <aneela2106@gmail.com>
2025-01-06 17:41:55 -0600

    changes done color from theme.js and use strict removed

--- app/components/status-badge.jsx
+++ app/components/status-badge.jsx
@@ -1,7 +1,6 @@
 // https://github.com/EnCiv/civil-pursuit/issues/47
 // https://github.com/EnCiv/civil-pursuit/issues/261
 
-https: 'use strict'
 import React from 'react'
 import { createUseStyles } from 'react-jss'
 import cx from 'classnames'
@@ -38,13 +37,13 @@ const useStyles = createUseStyles(theme => ({
   progress: {
     borderColor: theme.colors.statusBadgeProgressBorder,
     backgroundColor: theme.colors.statusBadgeProgressBackground,
-    color: '#342A00',
+    color: theme.colors.statusBadgeProgressTextColor,
     fontWeight: '100',
   },
   complete: {
     borderColor: theme.colors.statusBadgeCompletedBorder,
     backgroundColor: theme.colors.statusBadgeCompletedBackground,
-    color: '#005621',
+    color: theme.colors.statusBadgeCompletedTextColor,
     fontWeight: '100',
   },
   inactive: {
@@ -54,7 +53,7 @@ const useStyles = createUseStyles(theme => ({
   error: {
     borderColor: theme.colors.statusBadgeErrorBorder,
     backgroundColor: theme.colors.statusBadgeErrorBackground,
-    color: '#BF1300',
+    color: theme.colors.statusBadgeErrorTextColor,
     fontWeight: '100',
   },
 }))
