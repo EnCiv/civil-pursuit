da64f8939cd16b3073cfd8b8e0e82bae83216883
Yi Ying Li <yiyinl4@andrew.cmu.edu>
2025-02-14 14:53:08 -0500

    restore removed comment

--- app/components/ranking.jsx
+++ app/components/ranking.jsx
@@ -19,7 +19,7 @@ export default function Ranking(props) {
   let [response, setResponse] = useState(responseOptions.includes(defaultValue) ? defaultValue : '')
 
   useEffect(() => {
-    if (!defaultValue && !response) return
+    if (!defaultValue && !response) return // do not call onDone if initally empty, or if change to empty from above when it's already empty
     if (responseOptions.includes(defaultValue)) {
       setResponse(defaultValue)
       onDone && onDone({ valid: true, value: defaultValue })
